932b9d0a18aa97a9f8567100d7bc8dbdbdae7352
evill33t <ronny.stiftel@gmail.com>
2006-12-17 22:18:44 +0000

    ...
    git-svn-id: http://svn.l2jfree.com/l2jfree@198 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/L2_GameServer_c5/src/main/java/net/sf/l2j/gameserver/model/L2Character.java
+++ trunk/L2_GameServer_c5/src/main/java/net/sf/l2j/gameserver/model/L2Character.java
@@ -1885,13 +1885,6 @@ public abstract class L2Character extends L2Object
 
        if(newEffect == null) return;
 
-       // Make sure there's no same effect previously  
-       for (int i=0; i<_effects.size(); i++)   
-       {   
-           if (_effects.get(i).getSkill().getId() == newEffect.getSkill().getId())   
-               return;  
-       } 
-
        synchronized (this)
        {
            if (_effects == null)
