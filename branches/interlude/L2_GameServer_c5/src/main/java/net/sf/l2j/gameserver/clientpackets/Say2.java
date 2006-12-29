3d94497a606821f072fa851adcad0ceb6469e90f
evill33t <ronny.stiftel@gmail.com>
2006-12-29 23:50:28 +0000

    branch   interlude     lil fix for say2
    git-svn-id: http://svn.l2jfree.com/l2jfree@262 6ff00ba9-25cd-4355-987e-099c62bc2628

--- branches/interlude/L2_GameServer_c5/src/main/java/net/sf/l2j/gameserver/clientpackets/Say2.java
+++ branches/interlude/L2_GameServer_c5/src/main/java/net/sf/l2j/gameserver/clientpackets/Say2.java
@@ -43,88 +43,88 @@ import org.apache.commons.logging.LogFactory;
  */
 public class Say2 extends ClientBasePacket
 {
-	private static final String _C__38_SAY2 = "[C] 38 Say2";
-	private final static Log _log = LogFactory.getLog(Say2.class.getName());
-	private static Log _logChat = LogFactory.getLog("chat");
+    private static final String _C__38_SAY2 = "[C] 38 Say2";
+    private final static Log _log = LogFactory.getLog(Say2.class.getName());
+    private static Log _logChat = LogFactory.getLog("chat");
 
-	public final static int ALL = 0;
-	public final static int SHOUT = 1; //!
-	public final static int TELL = 2;
-	public final static int PARTY = 3; //#
-	public final static int CLAN = 4;  //@
-	public final static int GM = 5;    
-	public final static int PETITION_PLAYER = 6; // used for petition
-	public final static int PETITION_GM = 7; //* used for petition
-	public final static int TRADE = 8; //+
-	public final static int ALLIANCE = 9; //$
-	public final static int ANNOUNCEMENT = 10;
-	public final static int PARTYROOM_ALL = 15; //(yellow)
-	public final static int PARTYROOM_COMMANDER = 16; //(blue)
-	public final static int HERO_VOICE = 17;
+    public final static int ALL = 0;
+    public final static int SHOUT = 1; //!
+    public final static int TELL = 2;
+    public final static int PARTY = 3; //#
+    public final static int CLAN = 4;  //@
+    public final static int GM = 5;    
+    public final static int PETITION_PLAYER = 6; // used for petition
+    public final static int PETITION_GM = 7; //* used for petition
+    public final static int TRADE = 8; //+
+    public final static int ALLIANCE = 9; //$
+    public final static int ANNOUNCEMENT = 10;
+    public final static int PARTYROOM_ALL = 15; //(yellow)
+    public final static int PARTYROOM_COMMANDER = 16; //(blue)
+    public final static int HERO_VOICE = 17;
     
-	public final static String[] chatNames = {
-		"ALL  ",
-		"SHOUT",
-		"TELL ",
-		"PARTY",
-		"CLAN ",
-		"GM   ",
-		"PETITION_PLAYER",
-		"PETITION_GM",
-		"TRADE",
-		"ALLIANCE",
-		"ANNOUNCEMENT", //10
-		"WILLCRASHCLIENT:)",
-		"FAKEALL?",
-		"FAKEALL?",
-		"FAKEALL?",
-		"PARTYROOM_ALL",
-		"PARTYROOM_COMMANDER",
-		"HERO_VOICE"
-	};
+    public final static String[] chatNames = {
+        "ALL  ",
+        "SHOUT",
+        "TELL ",
+        "PARTY",
+        "CLAN ",
+        "GM   ",
+        "PETITION_PLAYER",
+        "PETITION_GM",
+        "TRADE",
+        "ALLIANCE",
+        "ANNOUNCEMENT", //10
+        "WILLCRASHCLIENT:)",
+        "FAKEALL?",
+        "FAKEALL?",
+        "FAKEALL?",
+        "PARTYROOM_ALL",
+        "PARTYROOM_COMMANDER",
+        "HERO_VOICE"
+    };
     
     private String _text;
-	private int _type;
-	private String _target;
-	/**
-	 * packet type id 0x38
-	 * format:		cSd (S)
-	 * @param decrypt
-	 */
-	public Say2(ByteBuffer buf, ClientThread client)
-	{
-		super(buf, client);
-		_text = readS();
-		_type = readD();
-		_target = (_type == TELL) ? readS() : null;  
-	}
+    private int _type;
+    private String _target;
+    /**
+     * packet type id 0x38
+     * format:      cSd (S)
+     * @param decrypt
+     */
+    public Say2(ByteBuffer buf, ClientThread client)
+    {
+        super(buf, client);
+        _text = readS();
+        _type = readD();
+        _target = (_type == TELL) ? readS() : null;  
+    }
 
-	void runImpl()
-	{
-		if (_log.isDebugEnabled()) 
-			_log.info("Say2: Msg Type = '" + _type + "' Text = '" + _text + "'.");
-		
-		if(_type >= chatNames.length)
-		{
-			_log.warn("Say2: Invalid type: "+_type);
-			return;
-		}
-		
-		L2PcInstance activeChar = getClient().getActiveChar();
-				
-		if (activeChar == null)
-		{
-			_log.warn("[Say2.java] Active Character is null.");
-			return;
-		}
-		
-		if (activeChar.isChatBanned())
-		{
-			{
-				activeChar.sendMessage("You may not chat while a chat ban is in effect.");
-				return;
-			}
-		}
+    void runImpl()
+    {
+        if (_log.isDebugEnabled()) 
+            _log.info("Say2: Msg Type = '" + _type + "' Text = '" + _text + "'.");
+        
+        if(_type >= chatNames.length)
+        {
+            _log.warn("Say2: Invalid type: "+_type);
+            return;
+        }
+        
+        L2PcInstance activeChar = getClient().getActiveChar();
+                
+        if (activeChar == null)
+        {
+            _log.warn("[Say2.java] Active Character is null.");
+            return;
+        }
+        
+        if (activeChar.isChatBanned())
+        {
+            {
+                activeChar.sendMessage("You may not chat while a chat ban is in effect.");
+                return;
+            }
+        }
         
         if (activeChar.isInJail() && Config.JAIL_DISABLE_CHAT)
         {
@@ -134,57 +134,57 @@ public class Say2 extends ClientBasePacket
                 return;
             }
         }
-		
-		if (_type == PETITION_PLAYER && activeChar.isGM()) 
-			_type = PETITION_GM;
-					
+        
+        if (_type == PETITION_PLAYER && activeChar.isGM()) 
+            _type = PETITION_GM;
+                    
 
-		if (Config.LOG_CHAT) 
-		{
-			if (_type == TELL)
+        if (Config.LOG_CHAT) 
+        {
+            if (_type == TELL)
                 _logChat.info( chatNames[_type] + "[" + activeChar.getName() + " to "+_target+"] " + _text);
-			else
+            else
                 _logChat.info( chatNames[_type] + "[" + activeChar.getName() + "] " + _text);
-		}
+        }
 
-		if(Config.USE_SAY_FILTER) 
-		{
-			for(String pattern : Config.FILTER_LIST)
-			{
-					_text = _text.replaceAll(pattern,"^_^");
-	  		}
-		}		
-		
-		CreatureSay cs = new CreatureSay(activeChar.getObjectId(), _type, activeChar.getName(), _text);
-	
-				
-		switch (_type)
-		{
-			case TELL:
-				L2PcInstance receiver = L2World.getInstance().getPlayer(_target);
-				
-				if (receiver != null && 
-						!BlockList.isBlocked(receiver, activeChar))
-				{	
-					if (!receiver.getMessageRefusal())
-					{
-						receiver.sendPacket(cs);
-						activeChar.sendPacket(new CreatureSay(activeChar.getObjectId(),  _type, "->" + receiver.getName(), _text));
-					}
-					else
-					{
-						activeChar.sendPacket(new SystemMessage(SystemMessage.THE_PERSON_IS_IN_MESSAGE_REFUSAL_MODE));
-					}
-		}
-				else
-				{
-					SystemMessage sm = new SystemMessage(SystemMessage.S1_IS_NOT_ONLINE);
-					sm.addString(_target);		  
-					activeChar.sendPacket(sm);
+        if(Config.USE_SAY_FILTER) 
+        {
+            for(String pattern : Config.FILTER_LIST)
+            {
+                    _text = _text.replaceAll(pattern,"^_^");
+            }
+        }       
+        
+        CreatureSay cs = new CreatureSay(activeChar.getObjectId(), _type, activeChar.getName(), _text);
+    
+                
+        switch (_type)
+        {
+            case TELL:
+                L2PcInstance receiver = L2World.getInstance().getPlayer(_target);
+                
+                if (receiver != null && 
+                        !BlockList.isBlocked(receiver, activeChar))
+                {   
+                    if (!receiver.getMessageRefusal())
+                    {
+                        receiver.sendPacket(cs);
+                        activeChar.sendPacket(new CreatureSay(activeChar.getObjectId(),  _type, "->" + receiver.getName(), _text));
+                    }
+                    else
+                    {
+                        activeChar.sendPacket(new SystemMessage(SystemMessage.THE_PERSON_IS_IN_MESSAGE_REFUSAL_MODE));
+                    }
+        }
+                else
+                {
+                    SystemMessage sm = new SystemMessage(SystemMessage.S1_IS_NOT_ONLINE);
+                    sm.addString(_target);        
+                    activeChar.sendPacket(sm);
                     sm = null;
-				}
-			break;
-		case SHOUT:
+                }
+            break;
+        case SHOUT:
                 if (Config.DEFAULT_GLOBAL_CHAT.equalsIgnoreCase("on") ||
                         (Config.DEFAULT_GLOBAL_CHAT.equalsIgnoreCase("gm") && activeChar.isGM()))
                 {
@@ -203,15 +203,15 @@ public class Say2 extends ClientBasePacket
                     }
                 }                
                 break;
-		case TRADE: 
-				if (Config.DEFAULT_TRADE_CHAT.equalsIgnoreCase("on") ||
-						(Config.DEFAULT_TRADE_CHAT.equalsIgnoreCase("gm") && activeChar.isGM()))
-			{
-				for (L2PcInstance player : L2World.getInstance().getAllPlayers())
-				{
-						player.sendPacket(cs);
-				}
-				} else if (Config.DEFAULT_TRADE_CHAT.equalsIgnoreCase("limited"))
+        case TRADE: 
+                if (Config.DEFAULT_TRADE_CHAT.equalsIgnoreCase("on") ||
+                        (Config.DEFAULT_TRADE_CHAT.equalsIgnoreCase("gm") && activeChar.isGM()))
+            {
+                for (L2PcInstance player : L2World.getInstance().getAllPlayers())
+                {
+                        player.sendPacket(cs);
+                }
+                } else if (Config.DEFAULT_TRADE_CHAT.equalsIgnoreCase("limited"))
                 {
                     int region = MapRegionTable.getInstance().getMapRegion(activeChar.getX(), activeChar.getY());
                     for (L2PcInstance player : L2World.getInstance().getAllPlayers())
@@ -221,34 +221,36 @@ public class Say2 extends ClientBasePacket
                     }
                 }
              break;
-		case ALL:
-			if (_text.startsWith(".")) 
-			{
-				StringTokenizer st = new StringTokenizer(_text);
+        case ALL:
+            if (_text.startsWith(".")) 
+            {
+                StringTokenizer st = new StringTokenizer(_text);
 
                 String command;
                 String params;
                 
                 if (st.countTokens()>=2)
-				{
-                    command = st.nextToken().substring(1);
-                    params = st.nextToken();             
-				}
-                else
                 {
                     command = st.nextToken().substring(1);
+                    params = st.nextToken();             
+                }
+                else
+                {
+                    command = _text.substring(1).trim();
                     params = "";             
                 }
-                IVoicedCommandHandler vch = VoicedCommandHandler.getInstance().getVoicedCommandHandler(command);
-                
-                if (vch != null) 
-                    vch.useVoicedCommand(command, activeChar, params);
-                else
-                    _log.warn("No handler registered for voice command '"+command+"'");
-                
+                if (command.length()>0)
+                {
+                    IVoicedCommandHandler vch = VoicedCommandHandler.getInstance().getVoicedCommandHandler(command);
+                    
+                    if (vch != null) 
+                        vch.useVoicedCommand(command, activeChar, params);
+                    else
+                        _log.warn("No handler registered for voice command '"+command+"'");
+                }                
                 command=null;
                 params=null;
-			}
+            }
             else
             {
                 for (L2PcInstance player : activeChar.getKnownList().getKnownPlayers())
@@ -259,48 +261,48 @@ public class Say2 extends ClientBasePacket
                 activeChar.sendPacket(cs);
             }   
             break;
-		case CLAN:
-			if (activeChar.getClan() != null)
-				activeChar.getClan().broadcastToOnlineMembers(cs);
-			break;	
-		case ALLIANCE:
-			if (activeChar.getClan() != null)
-				activeChar.getClan().broadcastToOnlineAllyMembers(cs);
-			break;
-		case PARTY:
-			if (activeChar.isInParty())
-				activeChar.getParty().broadcastToPartyMembers(cs);
-			break;
-		case PETITION_PLAYER:
-		case PETITION_GM:
-			if (!PetitionManager.getInstance().isPlayerInConsultation(activeChar))
-			{
-				activeChar.sendPacket(new SystemMessage(745));
-				break;
-			}
-			
-			PetitionManager.getInstance().sendActivePetitionMessage(activeChar, _text);
-			break;
-		case PARTYROOM_ALL:
-		case PARTYROOM_COMMANDER:
-			//PartyCommandManager.getInstance().sendChannelMessage(activeChar, _text);
-			break;
-		case HERO_VOICE:
-			if (activeChar.isHero())
-			{
-				for (L2PcInstance player : L2World.getInstance().getAllPlayers())
-					if (!BlockList.isBlocked(player, activeChar))
-						player.sendPacket(cs);
-			}
-			break;
-		}			
-	}
+        case CLAN:
+            if (activeChar.getClan() != null)
+                activeChar.getClan().broadcastToOnlineMembers(cs);
+            break;  
+        case ALLIANCE:
+            if (activeChar.getClan() != null)
+                activeChar.getClan().broadcastToOnlineAllyMembers(cs);
+            break;
+        case PARTY:
+            if (activeChar.isInParty())
+                activeChar.getParty().broadcastToPartyMembers(cs);
+            break;
+        case PETITION_PLAYER:
+        case PETITION_GM:
+            if (!PetitionManager.getInstance().isPlayerInConsultation(activeChar))
+            {
+                activeChar.sendPacket(new SystemMessage(745));
+                break;
+            }
+            
+            PetitionManager.getInstance().sendActivePetitionMessage(activeChar, _text);
+            break;
+        case PARTYROOM_ALL:
+        case PARTYROOM_COMMANDER:
+            //PartyCommandManager.getInstance().sendChannelMessage(activeChar, _text);
+            break;
+        case HERO_VOICE:
+            if (activeChar.isHero())
+            {
+                for (L2PcInstance player : L2World.getInstance().getAllPlayers())
+                    if (!BlockList.isBlocked(player, activeChar))
+                        player.sendPacket(cs);
+            }
+            break;
+        }           
+    }
 
-	/* (non-Javadoc)
-	 * @see net.sf.l2j.gameserver.clientpackets.ClientBasePacket#getType()
-	 */
-	public String getType()
-	{
-		return _C__38_SAY2;
-	}
+    /* (non-Javadoc)
+     * @see net.sf.l2j.gameserver.clientpackets.ClientBasePacket#getType()
+     */
+    public String getType()
+    {
+        return _C__38_SAY2;
+    }
 }
