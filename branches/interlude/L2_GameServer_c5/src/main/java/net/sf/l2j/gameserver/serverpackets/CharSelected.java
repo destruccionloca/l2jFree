915748b3cd14ccc53ea7eecc159a6b454616f4bb
evill33t <ronny.stiftel@gmail.com>
2007-01-06 01:17:37 +0000

    branch   interlude     possible fix for 0x0d exploit
    git-svn-id: http://svn.l2jfree.com/l2jfree@283 6ff00ba9-25cd-4355-987e-099c62bc2628

--- branches/interlude/L2_GameServer_c5/src/main/java/net/sf/l2j/gameserver/serverpackets/CharSelected.java
+++ branches/interlude/L2_GameServer_c5/src/main/java/net/sf/l2j/gameserver/serverpackets/CharSelected.java
@@ -21,6 +21,9 @@ package net.sf.l2j.gameserver.serverpackets;
 import net.sf.l2j.gameserver.GameTimeController;
 import net.sf.l2j.gameserver.model.actor.instance.L2PcInstance;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
  * This class ...
  * 
@@ -33,7 +36,9 @@ public class CharSelected extends ServerBasePacket
     private static final String _S__21_CHARSELECTED = "[S] 15 CharSelected";
     private L2PcInstance _cha;
     private int _sessionId;
+    private final static Log _log = LogFactory.getLog(CharSelected.class.getName());
 
+    
     /**
      * @param _characters
      */
@@ -51,6 +56,12 @@ public class CharSelected extends ServerBasePacket
     
     final void writeImpl()
     {
+        if(_cha.getAccountName()!=getClient().getLoginName())
+        {
+            _log.fatal("HACKER: Account" + getClient().getLoginName() + " tried to login with char "+_cha.getName()+" of account "+_cha.getAccountName());
+            getClient().getConnection().close();
+        }
+        
         writeC(0x15);
         
         writeS(_cha.getName());
