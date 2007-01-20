18436fa1307e9be798c7bb435593f9ddb6032952
DiezelMax <nonexistent@l2jfree.com>
2007-01-20 19:01:14 +0000

    branch interlude DP: fix itemsonground
    git-svn-id: http://svn.l2jfree.com/l2jfree@387 6ff00ba9-25cd-4355-987e-099c62bc2628

--- branches/interlude/L2_Datapack_Interlude/sql/items_on_ground.sql
+++ branches/interlude/L2_Datapack_Interlude/sql/items_on_ground.sql
@@ -1,7 +1,7 @@
 -- ----------------------------
 -- Table structure for itemsonground
 -- ----------------------------
-CREATE TABLE IF NOT EXISTS `ItemsOnGround` (
+CREATE TABLE IF NOT EXISTS `itemsonground` (
   `object_id` int(11) NOT NULL default '0',
   `item_id` int(11) default NULL,
   `count` int(11) default NULL,
@@ -9,6 +9,7 @@ CREATE TABLE IF NOT EXISTS `ItemsOnGround` (
   `x` int(11) NOT NULL default '0',
   `y` int(11) NOT NULL default '0',
   `z` int(11) NOT NULL default '0',
-  `drop_time` int(11) NOT NULL default '0',
+  `drop_time` decimal(20,0) NOT NULL default '0',
+  `equipable` int(1) default '0',
   PRIMARY KEY  (`object_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+);
