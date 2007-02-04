a4e2a38150fdc34363fa6ca33b4e2fb45c4ef9dd
evill33t <ronny.stiftel@gmail.com>
2007-02-04 21:40:02 +0000

    trunk 	gameserver 		sync to l2j rev520
    git-svn-id: http://svn.l2jfree.com/l2jfree@507 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/L2_GameServer_c5/src/main/java/net/sf/l2j/gameserver/model/L2Object.java
+++ trunk/L2_GameServer_c5/src/main/java/net/sf/l2j/gameserver/model/L2Object.java
@@ -178,6 +178,7 @@ public abstract class L2Object
         // synchronized, to avoid deadlocks
         // Remove the L2Object from the world
         L2World.getInstance().removeVisibleObject(this, reg);
+        L2World.getInstance().removeObject(this);
     }
 
     /**
--- trunk/L2_GameServer_c5/src/main/java/net/sf/l2j/gameserver/pathfinding/Node.java
+++ trunk/L2_GameServer_c5/src/main/java/net/sf/l2j/gameserver/pathfinding/Node.java
@@ -27,6 +27,7 @@ public class Node
     private final int _Neighbors_idx;
     private Node[] _Neighbors;
     private Node _Parent;
+	private short _Cost;
     
     
     public Node(AbstractNodeLoc Loc, int Neighbors_idx)
@@ -38,6 +39,11 @@ public class Node
     public void setParent(Node p)
     {
         _Parent = p;
+	}
+	
+	public void setCost(int cost)
+	{
+		_Cost = (short)cost;
     }
     
     public void attacheNeighbors()
@@ -59,6 +65,11 @@ public class Node
     {
         return _Loc;
     }
+	
+	public short getCost()
+	{
+		return _Cost;
+	}
 
     /**
      * @see java.lang.Object#equals(java.lang.Object)
