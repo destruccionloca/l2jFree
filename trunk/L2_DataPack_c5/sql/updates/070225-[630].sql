46b3e6733d35b19f3ff867a4fc42a9b3804ed430
evill33t <ronny.stiftel@gmail.com>
2007-02-25 20:11:38 +0000

    trunk   datapack     commits for last [630]
    git-svn-id: http://svn.l2jfree.com/l2jfree@631 6ff00ba9-25cd-4355-987e-099c62bc2628

---
+++ trunk/L2_DataPack_c5/sql/updates/070225-[630].sql
@@ -0,0 +1,7 @@
+ALTER TABLE characters DROP apprentice;
+ALTER TABLE characters DROP varka;
+ALTER TABLE characters drop ketra;
+ALTER TABLE characters ADD COLUMN last_recom_date decimal(20,0) NOT NULL DEFAULT 0 AFTER subpledge;
+ALTER TABLE characters ADD COLUMN apprentice int(1) NOT NULL DEFAULT 0 AFTER pledge_rank;
+ALTER TABLE characters ADD COLUMN sponsor int(1) NOT NULL DEFAULT 0 AFTER apprentice;
+ALTER TABLE characters ADD COLUMN varka_ketra_ally int(1) NOT NULL DEFAULT 0 AFTER sponsor;
