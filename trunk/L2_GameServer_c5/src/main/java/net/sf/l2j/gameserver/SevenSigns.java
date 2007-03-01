5d93cc669d5894e935626f1fd8650b4f1818abb6
SarutrA <nonexistent@l2jfree.com>
2007-03-01 13:41:41 +0000

    TRUNK : GS correct config sevensigns.properties file
    git-svn-id: http://svn.l2jfree.com/l2jfree@680 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/L2_GameServer_c5/src/main/java/net/sf/l2j/gameserver/SevenSigns.java
+++ trunk/L2_GameServer_c5/src/main/java/net/sf/l2j/gameserver/SevenSigns.java
@@ -37,7 +37,7 @@ public class SevenSigns
     private static SevenSigns _instance;
 
     // Basic Seven Signs Constants \\
-    public static final String SEVEN_SIGNS_DATA_FILE = "config/signs.properties";
+    public static final String SEVEN_SIGNS_DATA_FILE = "config/sevensigns.properties";
     public static final String SEVEN_SIGNS_HTML_PATH = "data/html/seven_signs/";
     
     public static final int CABAL_NULL = 0;
