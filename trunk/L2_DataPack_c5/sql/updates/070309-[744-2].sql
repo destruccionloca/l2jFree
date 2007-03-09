17e6dece74b6f8cb63fd1677cefaf721b9edcab3
SarutrA <nonexistent@l2jfree.com>
2007-03-09 16:19:25 +0000

    git-svn-id: http://svn.l2jfree.com/l2jfree@752 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/L2_DataPack_c5/sql/updates/070309-[744-2].sql
+++ trunk/L2_DataPack_c5/sql/updates/070309-[744-2].sql
@@ -1,1 +1,1 @@
-ALTER TABLE characters ADD lvl_joined_academy INT( 11 ) NOT NULL DEFAULT '0' AFTER pledge_rank ; 
+ALTER TABLE characters ADD lvl_joined_academy INT( 4 ) NOT NULL DEFAULT '0' AFTER pledge_rank ; 
