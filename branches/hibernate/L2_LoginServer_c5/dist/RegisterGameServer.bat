348dbf5354e31127f164f415948bdf84f3017049
corwin <nonexistent@l2jfree.com>
2007-03-21 20:08:13 +0000

    Delete Hibernate branches
    git-svn-id: http://svn.l2jfree.com/l2jfree@874 6ff00ba9-25cd-4355-987e-099c62bc2628

--- branches/hibernate/L2_LoginServer_c5/dist/RegisterGameServer.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-SET OLDCLASSPATH=%CLASSPATH%
-call setenv.bat
-
-@java -Djava.util.logging.config.file=console.cfg net.sf.l2j.gsregistering.GameServerRegister
-
-SET CLASSPATH=%OLDCLASSPATH%
-@pause
