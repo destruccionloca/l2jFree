9b7aae20737dc8b3f3e11919ede8a59e932eb960
evill33t <ronny.stiftel@gmail.com>
2007-03-24 20:14:48 +0000

    fix for champion hp multi
    git-svn-id: http://svn.l2jfree.com/l2jfree@893 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/L2_GameServer_c5/src/main/java/net/sf/l2j/gameserver/model/actor/stat/NpcStat.java
+++ trunk/L2_GameServer_c5/src/main/java/net/sf/l2j/gameserver/model/actor/stat/NpcStat.java
@@ -28,5 +28,5 @@ public class NpcStat extends CharStat
     // Property - Public
     public L2NpcInstance getActiveChar() { return (L2NpcInstance)super.getActiveChar(); }
 
-    public final int getMaxHp() { return (int)calcStat(Stats.MAX_HP, getActiveChar().getTemplate().baseHpMax , null, null); }
+    public final int getMaxHp() { return (int)calcStat(Stats.MAX_HP, getActiveChar().getTemplate().baseHpMax * (getActiveChar().isChampion()?Config.CHAMPION_HP:1) , null, null); }
 }
