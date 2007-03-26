798a97ba5fe004c34cb2061db767672303461326
G1ta0 <l2jdev@gmail.com>
2007-03-26 20:09:48 +0000

    - sync to l2jdp rev.2814
    git-svn-id: http://svn.l2jfree.com/l2jfree@920 6ff00ba9-25cd-4355-987e-099c62bc2628

---
+++ trunk/L2_DataPack_c5/data/jscript/custom/6050_KetraOrcSupport/__init__.py
@@ -0,0 +1,42 @@
+# Created by Emperorc
+import sys
+from net.sf.l2j.gameserver.model.quest import State
+from net.sf.l2j.gameserver.model.quest import QuestState
+from net.sf.l2j.gameserver.model.quest.jython import QuestJython as JQuest
+
+qn = "6050_KetraOrcSupport"
+
+class Quest (JQuest) :
+
+ def __init__(self,id,name,descr): JQuest.__init__(self,id,name,descr)
+ 
+ def onEvent (self,event,st):
+    return
+ 
+ def onTalk (self,npc,player):
+     htmltext = "<html><head><body>I have nothing to say you</body></html>"
+     st = player.getQuestState(qn)
+     if not st : return htmltext
+
+     npcId = npc.getNpcId()
+     id = st.getState()
+     Alevel = st.getPlayer().getAllianceWithVarkaKetra()
+     if Alevel == 2 :
+        htmltext = "1.htm"
+     elif Alevel == 3 or Alevel == 4 :
+         htmltext = "2.htm"
+     elif Alevel == 5 :
+         htmltext = "3.htm"
+     else :
+         htmltext = "no.htm"
+     return htmltext
+
+QUEST       = Quest(6050, qn, "custom")
+CREATED     = State('Start', QUEST)
+COMPLETED   = State('Completed',   QUEST)
+
+QUEST.setInitialState(CREATED)
+QUEST.addStartNpc(31375)
+QUEST.addTalkId(31375)
+
+print "importing quests: 6050: Ketra Orc Support"
