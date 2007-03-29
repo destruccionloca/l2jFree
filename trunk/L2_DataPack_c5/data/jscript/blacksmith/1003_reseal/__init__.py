f3f6d098af6d9cb41bf147a53e811b3b5b7d51cf
Wolverine <nonexistent@l2jfree.com>
2007-03-29 19:00:33 +0000

    TRUNK DP:
    - sync to l2jdb rev 2845
    
    git-svn-id: http://svn.l2jfree.com/l2jfree@962 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/L2_DataPack_c5/data/jscript/blacksmith/1003_reseal/__init__.py
+++ trunk/L2_DataPack_c5/data/jscript/blacksmith/1003_reseal/__init__.py
@@ -232,21 +232,16 @@ class Quest (JQuest) :
       htmltext = "Trade has been canceled."
     
     if htmltext != event:
-      st.setState(COMPLETED)
       st.exitQuest(1)
-
     return htmltext
 
- def onTalk (Self,npc,st):
-
-   npcId = npc.getNpcId()
-   st.getQuestState(qn)
-   htmltext = "<html><head><body>I have nothing to say to you.</body></html>"
-   st.set("cond","0")
+ def onTalk (self,npc,player):
+   st = player.getQuestState(qn)
+   if not st: return 
    st.setState(STARTED)
    return "1.htm"
 
-QUEST       = Quest(1003,"1003_reseal","Blacksmith")
+QUEST       = Quest(1003,qn,"Blacksmith")
 CREATED     = State('Start',     QUEST)
 STARTED     = State('Started',   QUEST)
 COMPLETED   = State('Completed', QUEST)
