c36ac8e48e8f26ff9ffc6e7e3bd79330ae50ecf6
Wolverine <nonexistent@l2jfree.com>
2007-04-09 17:04:45 +0000

    TRUNK DP:
    - sync to l2jdb rev 2899
    
    git-svn-id: http://svn.l2jfree.com/l2jfree@1062 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/L2_DataPack_c5/data/jscript/__init__.py
+++ trunk/L2_DataPack_c5/data/jscript/__init__.py
@@ -1,7 +1,6 @@
 __all__ = [
 'quests',
 #'luxury_shop',
-'blacksmith',
 'custom',
 'teleports',
 'village_master',
@@ -9,7 +8,6 @@ __all__ = [
 ]
 import quests
 #import luxury_shop
-import blacksmith
 import custom
 import teleports
 import village_master
