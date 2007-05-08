516eb063b0643c5d8bb0e03c42ee963b633462f1
Wolverine <nonexistent@l2jfree.com>
2007-05-08 22:49:58 +0000

    TRUNK DP IL:
    - correction... (thx inkblot)
    
    git-svn-id: http://svn.l2jfree.com/l2jfree@1326 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/L2_DataPack_IL/sql/etcitem.sql
+++ trunk/L2_DataPack_IL/sql/etcitem.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS `etcitem` (
   `item_id` decimal(11,0) NOT NULL default '0',
   `name` varchar(100) default NULL,
   `crystallizable` varchar(5) default NULL,
-  `item_type` varchar(12) default NULL,
+  `item_type` varchar(14) default NULL,
   `weight` decimal(4,0) default NULL,
   `consume_type` varchar(9) default NULL,
   `material` varchar(11) default NULL,
