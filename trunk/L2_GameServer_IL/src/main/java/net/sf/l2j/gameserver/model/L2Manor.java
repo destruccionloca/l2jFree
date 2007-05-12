2773ece1cc6bfcae0caea911649e8e80307d88b4
DiezelMax <nonexistent@l2jfree.com>
2007-05-12 12:09:07 +0000

    trunk GS IL: L2Manor.java cosmetic changes
    git-svn-id: http://svn.l2jfree.com/l2jfree@1334 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/L2_GameServer_IL/src/main/java/net/sf/l2j/gameserver/model/L2Manor.java
+++ trunk/L2_GameServer_IL/src/main/java/net/sf/l2j/gameserver/model/L2Manor.java
@@ -23,35 +23,12 @@ public class L2Manor
 {
     private final static Log _log = LogFactory.getLog(L2Manor.class.getName());
     private static final L2Manor INstance = new L2Manor();
+    private static FastMap<Integer,SeedData> _seeds;
     
     private L2Manor()
     {
         _seeds = new FastMap<Integer,SeedData>();
         parseData();
-        
-        //Seeds data, moved to datapack...
-/*
-        _seeds.put(5016,new SeedData(10,5073)); // Seed: Dark Coda, Type1: stem,100 Type2: braided hemp,500 Manufactured: Gludio,Oren
-        _seeds.put(5017,new SeedData(13,5068)); // Seed: Red Coda,  Type1: Type2: Manufactured:
-        _seeds.put(5018,new SeedData(16,5065)); // Seed: Chilly Coda,  Type1: Type2: Manufactured:
-        _seeds.put(5019,new SeedData(19,5067)); // Seed: Blue Coda,  Type1: Type2: Manufactured:
-        _seeds.put(5020,new SeedData(22,5069)); // Seed: Golden Coda,  Type1: Type2: Manufactured:
-        _seeds.put(5021,new SeedData(25,5071)); // Seed: Lute Coda,  Type1: Type2: Manufactured:
-        _seeds.put(5022,new SeedData(28,5070)); // Seed: Desert Coda,  Type1: Type2: Manufactured:
-        _seeds.put(5028,new SeedData(31,5078)); // Seed: Red Cobol,  Type1: Type2: Manufactured:
-        _seeds.put(5034,new SeedData(34,5075)); // Seed: Chilly Cobol,  Type1: Type2: Manufactured:
-        _seeds.put(5023,new SeedData(37,5077)); // Seed: Blue Cobol,  Type1: Type2: Manufactured:
-        _seeds.put(5036,new SeedData(40,5082)); // Seed: Thorn Cobol,  Type1: Type2: Manufactured:
-        _seeds.put(5037,new SeedData(43,5079)); // Seed: Golden Cobol,  Type1: Type2: Manufactured:
-        _seeds.put(5038,new SeedData(46,5084)); // Seed: Great Cobol,  Type1: Type2: Manufactured:
-        _seeds.put(5056,new SeedData(49,5088)); // Seed: Red Codran,  Type1: Type2: Manufactured:
-        _seeds.put(5049,new SeedData(50,5091)); // Seed: Sea Codran,  Type1: Type2: Manufactured:
-        _seeds.put(5057,new SeedData(52,5085)); // Seed: Chilly Codran,  Type1: Type2: Manufactured:
-        _seeds.put(5058,new SeedData(55,5087)); // Seed: Blue Codran,  Type1: Type2: Manufactured:
-        _seeds.put(5059,new SeedData(58,5092)); // Seed: Twin Codran,  Type1: Type2: Manufactured:
-        _seeds.put(5060,new SeedData(61,5094)); // Seed: Great Codran,  Type1: Type2: Manufactured:
-        _seeds.put(5061,new SeedData(64,5090)); // Seed: Desert Codran,  Type1: Type2: Manufactured:
-*/ 
     }
     
     public static L2Manor getInstance()
@@ -167,7 +144,7 @@ public class L2Manor
                     if(seed.RU == 1)  // Runne Castle by L2Emu team
                         crops.add(seed.crop);
                     break;
-                case 9:
+                case 9:		// Schuttgart castle
                     if(seed.SCH == 1)
                         crops.add(seed.crop);
                     break;
@@ -289,7 +266,7 @@ public class L2Manor
                     continue;
                 }
                 SeedData seed = parseList(line);
-                _seeds.put(seed.id, seed);             
+                _seeds.put(seed.id, seed);
             }
 
             _log.info("ManorManager: Loaded " + _seeds.size() + " seeds");
@@ -301,6 +278,7 @@ public class L2Manor
         catch (Exception e)
         {
             _log.warn("error while loading seeds: " + e);
+            e.printStackTrace();
         }
         finally
         {
@@ -316,7 +294,6 @@ public class L2Manor
 
     private SeedData parseList(String line)
     {
-        
         StringTokenizer st = new StringTokenizer(line, ";");
 
         int seedId = Integer.parseInt(st.nextToken());  // seed id
@@ -340,6 +317,4 @@ public class L2Manor
         
         return seed;
     }
-    
-    private static FastMap<Integer,SeedData> _seeds;
 }
