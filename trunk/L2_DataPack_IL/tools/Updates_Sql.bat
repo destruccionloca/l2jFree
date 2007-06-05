dc1f57d3e39ac692b603b25c559047bf18bb9978
SarutrA <nonexistent@l2jfree.com>
2007-06-05 13:25:46 +0000

    TRUNK DP: DataPack update for TvT autoevent         - changeset [1496]
    git-svn-id: http://svn.l2jfree.com/l2jfree@1497 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/L2_DataPack_IL/tools/Updates_Sql.bat
+++ trunk/L2_DataPack_IL/tools/Updates_Sql.bat
@@ -63,6 +63,7 @@ echo UPDATE
 %mysqlPath% -h %gshost% -u %gsuser% --password=%gspass% -D %gsdb% < ../sql/updates/070409-[1060].sql
 %mysqlPath% -h %gshost% -u %gsuser% --password=%gspass% -D %gsdb% < ../sql/updates/070504-[1287].sql
 %mysqlPath% -h %gshost% -u %gsuser% --password=%gspass% -D %gsdb% < ../sql/updates/070519-[1387].sql
+%mysqlPath% -h %gshost% -u %gsuser% --password=%gspass% -D %gsdb% < ../sql/updates/070605-[1497].sql
 
 :end
 echo Script complete.Duplicte Errors on line 1 occure when u already have the lines at your db.
