c5fc82a6ed57c1cc8942abecef661361ff60fedd
Respawner <respawneral@gmail.com>
2007-07-03 16:22:46 +0000

    Trunk LS & GS IL : GameServer_loop.sh and LoginServer_loop.sh changes for logs records
    git-svn-id: http://svn.l2jfree.com/l2jfree@1677 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/L2_GameServer_IL/dist/GameServer_loop.sh
+++ trunk/L2_GameServer_IL/dist/GameServer_loop.sh
@@ -5,8 +5,8 @@ err=1
 until [ $err == 0 ]; 
 do
 	. ./setenv.sh
-	mv log/java0.log.0 "log/`date +%Y-%m-%d_%H-%M-%S`_java.log"
-	mv log/stdout.log "log/`date +%Y-%m-%d_%H-%M-%S`_stdout.log"
+	[ -f log/java0.log.0 ] && mv log/java0.log.0 "log/`date +%Y-%m-%d_%H-%M-%S`_java.log"
+	[ -f log/stdout.log ] && mv log/stdout.log "log/`date +%Y-%m-%d_%H-%M-%S`_stdout.log"
 	java -Dfile.encoding=UTF-8 -Xmx1024m net.sf.l2j.gameserver.GameServer > log/stdout.log 2>&1
 	err=$?
 #	/etc/init.d/mysql restart
