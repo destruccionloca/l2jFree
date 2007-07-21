81d1e5469538fdb0421a662ad090995affc0b8ff
evill33t <ronny.stiftel@gmail.com>
2007-07-21 16:43:13 +0000

    interlude l2world fix by trexx http://forum.l2jserver.com/thread.php?threadid=25109
    git-svn-id: http://svn.l2jfree.com/l2jfree@1756 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/L2_GameServer_IL/src/main/java/net/sf/l2j/gameserver/model/L2World.java
+++ trunk/L2_GameServer_IL/src/main/java/net/sf/l2j/gameserver/model/L2World.java
@@ -727,7 +727,7 @@ public final class L2World implements L2WorldMBean
      */
     private boolean validRegion(int x, int y)
     {
-        return (x >= 0 && x < REGIONS_X && y >= 0 && y < REGIONS_Y);
+    	return (x >= 0 && x < REGIONS_X && y >= 0 && y < REGIONS_Y);
     }
     
     /**
@@ -754,9 +754,9 @@ public final class L2World implements L2WorldMBean
             }
         }
         
-        for (int x=0; x < REGIONS_X; x++)
+        for (int x=0; x <= REGIONS_X; x++)
         {
-            for (int y=0; y < REGIONS_Y; y++)
+        	for (int y=0; y <= REGIONS_Y; y++)
             {
                 for (int a = -1; a <= 1; a++)
                 {
