e3fcebbdf17683bc5cb229dcf6be3ea01df26b5f
Respawner <respawneral@gmail.com>
2007-08-08 11:07:28 +0000

    Trunk DP IL : Sync to L2JDP 3262, 3263, 3264, 3265, 3267, 3268, 3269, 3270 	* XML: first lot of Augmentation Active & Passive Skills [big thx to desti for the contrib - thx to ratonofx & eXtremalas for testing/feedback] 	  IMPORTANT NOTE: many values (where unknown) are custom 	  - Please test and comment on any problems you find - there are likely still some which were overlooked 	  - I'll fix the comments one by one as the skills get cleaned 	  - Still lots of skills to go.. some of which can be copy & pasted 	  (by HeXa) 	* XML: Armors Sets Passive Skills NG [thx MELERIX] (by HeXa) 	* XML: Armors Sets Passive Skills D Grade [thx MELERIX] - I added the shield bonuses in as well (by HeXa) 	* XML: Armors Sets Passive Skills C Grade [thx MELERIX] - I added the shield bonuses in as well (by HeXa) 	* XML: added reduce evasion effect to Clan Hall: Berserker Spirit [thx _redruM_] & cleaned up some XML [thx eXtremalas] (by HeXa) 	* SQL: added random spawns for Rooney (Blacksmith of Wind) with 20 minute delays [thx gigiikun] 	  - NOTE: we need to check the delay values... table has a mix of seconds and millisecond values for some reason 	  (by HeXa) 	* XML: Skill fixes (by Aikimaniac) 	* Sin Eater Addition. Some notes: 	  Most stats should be VERY accurate. HP, MP, XP, and Food Consumption should be extremely accurate (off by 1 point, at most). 	  Attack stats (PAtk, MAtk, Atkspd, Casting Speed, crit, etc) were taken from babypets. Custom but should be fairly accurate based on our info. 	  Defence stats (PDef, MDef, evasion, speed, etc) were taken from wolves. Custom but should be fairly accurate based on our info. 	  (by Fulminus)
    git-svn-id: http://svn.l2jfree.com/l2jfree@1809 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/L2_DataPack_IL/sql/auction.sql
+++ trunk/L2_DataPack_IL/sql/auction.sql
@@ -1,27 +1,22 @@
--- 
+-- ---------------------------
 -- Table structure for table `auction`
--- 
-
+-- ---------------------------
 CREATE TABLE IF NOT EXISTS `auction` (
-  id int(11) NOT NULL default '0',
-  sellerId int(11) NOT NULL default '0',
-  sellerName varchar(50) NOT NULL default 'NPC',
-  sellerClanName varchar(50) NOT NULL default '',
-  itemType varchar(25) NOT NULL default '',
-  itemId int(11) NOT NULL default '0',
-  itemObjectId int(11) NOT NULL default '0',
-  itemName varchar(40) NOT NULL default '',
-  itemQuantity int(11) NOT NULL default '0',
-  startingBid int(11) NOT NULL default '0',
-  currentBid int(11) NOT NULL default '0',
-  endDate decimal(20,0) NOT NULL default '0',
+  `id` int(11) NOT NULL default '0',
+  `sellerId` int(11) NOT NULL default '0',
+  `sellerName` varchar(50) NOT NULL default 'NPC',
+  `sellerClanName` varchar(50) NOT NULL default '',
+  `itemType` varchar(25) NOT NULL default '',
+  `itemId` int(11) NOT NULL default '0',
+  `itemObjectId` int(11) NOT NULL default '0',
+  `itemName` varchar(40) NOT NULL default '',
+  `itemQuantity` int(11) NOT NULL default '0',
+  `startingBid` int(11) NOT NULL default '0',
+  `currentBid` int(11) NOT NULL default '0',
+  `endDate` decimal(20,0) NOT NULL default '0',
   PRIMARY KEY  (`itemType`,`itemId`,`itemObjectId`),
   KEY `id` (`id`)
 ) DEFAULT CHARSET=utf8;
-
--- 
--- Dumping data for table `auction`
--- 
 
 INSERT IGNORE INTO `auction` VALUES 
 (22, 0, 'NPC', 'NPC Clan', 'ClanHall', 22, 0, 'Moonstone Hall', 1, 20000000, 0, 1164841200000),
