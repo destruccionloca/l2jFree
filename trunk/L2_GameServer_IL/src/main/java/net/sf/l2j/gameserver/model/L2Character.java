ec567f633482220d3ab6b0915aaeba49693ede8b
crion <medinos@gmx.de>
2007-09-06 11:48:18 +0000

    sorry
    git-svn-id: http://svn.l2jfree.com/l2jfree@1953 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/L2_GameServer_IL/src/main/java/net/sf/l2j/gameserver/model/L2Character.java
+++ trunk/L2_GameServer_IL/src/main/java/net/sf/l2j/gameserver/model/L2Character.java
@@ -2840,12 +2840,12 @@ public abstract class L2Character extends L2Object
         // Send the packets if needed
         if (mi != null)
             sendPacket(mi);
-        // summon info only needs to go to the owner, not to the whole party
-        // player info: if in party, send to all party members except one's self.
-        // if not in party, send to self.
-        if (player.isInParty() && summon == null)
+        if (ps != null && player != null)
         {
-            if (player.isInParty())
+            // summon info only needs to go to the owner, not to the whole party
+            // player info: if in party, send to all party members except one's self.
+            // if not in party, send to self.
+            if (player.isInParty() && summon == null)
                 player.getParty().broadcastToPartyMembers(player, ps);
             else
                 player.sendPacket(ps);
