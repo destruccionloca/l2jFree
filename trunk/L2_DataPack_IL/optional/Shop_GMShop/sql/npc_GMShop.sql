015744930cffc4b17c9cae4f011e4c906345a904
Michiru <nonexistent@l2jfree.com>
2007-09-08 17:00:31 +0000

    TRUNK DP: updated NPC´s in optional folder
    git-svn-id: http://svn.l2jfree.com/l2jfree@1963 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/L2_DataPack_IL/optional/Shop_GMShop/sql/npc_GMShop.sql
+++ trunk/L2_DataPack_IL/optional/Shop_GMShop/sql/npc_GMShop.sql
@@ -1,3 +1,3 @@
 -- Use INSERT INTO `npc` if you don't have L2JFree server
 INSERT INTO `custom_npc` VALUES
-(7099,30837,'GM Shop',1,'{Your NPC Title}',1,'NPC.a_traderA_MHuman',10.00,24.00,70,'male','L2Merchant',40,3862,1493,11.85,2.78,40,43,30,21,20,10,0,0,1314,470,780,382,278,0,333,0,0,0,55,132,NULL,0,1,0);
+(7099,30837,'GM Shop',1,'{Your NPC Title}',1,'NPC.a_traderA_MHuman',10.00,24.00,70,'male','L2Merchant',40,3862,1493,11.85,2.78,40,43,30,21,20,10,0,0,1314,470,780,382,278,0,333,0,0,0,55,132,NULL,0,1,0,'LAST_HIT');
--- trunk/L2_DataPack_IL/optional/Teleporter_LuxuryGK/sql/npc_LuxuryGK.sql
+++ trunk/L2_DataPack_IL/optional/Teleporter_LuxuryGK/sql/npc_LuxuryGK.sql
@@ -2,4 +2,4 @@
 DELETE FROM `npc` WHERE `id`='7077';
 
 INSERT INTO `npc` VALUES
-(7077, 31275, 'Tinkerbell', 1, 'Luxury Gatekeeper', 1, 'NPC.a_teleporter_FHuman', 8.00, 25.00, 70, 'female', 'L2Teleporter', 40, 3862, 1494, NULL, NULL, 40, 43, 30, 21, 20, 10, 5879, 590, 1444, 514, 760, 381, 253, 0, 253, 0, 0, 0, 80, 120, NULL, NULL, 0, 0);
+(7077, 31275, 'Tinkerbell', 1, 'Luxury Gatekeeper', 1, 'NPC.a_teleporter_FHuman', 8.00, 25.00, 70, 'female', 'L2Teleporter', 40, 3862, 1494, NULL, NULL, 40, 43, 30, 21, 20, 10, 5879, 590, 1444, 514, 760, 381, 253, 0, 253, 0, 0, 0, 80, 120, NULL, NULL, 0, 0,'LAST_HIT');
