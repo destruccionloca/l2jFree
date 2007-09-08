5027e4e35dad1e704ee2a3f33c1e9e71d7829479
Respawner <respawneral@gmail.com>
2007-09-08 18:52:24 +0000

    Trunk GS IL : Sync to L2J 1138 & 1139 	* Pet Info Packet change: It should show how many shots the servitor uses PER HIT, not running count. Side-effect fix: Sending the unnecessary pet-info packets from the beast shot handlers wiped the PartySpelled info. This info is on longer wiped now. TODO: servitor template id and level should dictate the use of more than 1 shot. For now, all servitors continue to only use 1 shot per attack. 	* Resending servitor's list of buffs after each petInfo packet (required, or list of buffs disappears) 	(by Fulminus)
    git-svn-id: http://svn.l2jfree.com/l2jfree@1964 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/L2_GameServer_IL/src/main/java/net/sf/l2j/gameserver/handler/itemhandlers/BeastSoulShot.java
+++ trunk/L2_GameServer_IL/src/main/java/net/sf/l2j/gameserver/handler/itemhandlers/BeastSoulShot.java
@@ -28,7 +28,6 @@ import net.sf.l2j.gameserver.model.actor.instance.L2PlayableInstance;
 import net.sf.l2j.gameserver.network.SystemMessageId;
 import net.sf.l2j.gameserver.serverpackets.ExAutoSoulShot;
 import net.sf.l2j.gameserver.serverpackets.MagicSkillUser;
-import net.sf.l2j.gameserver.serverpackets.PetInfo;
 import net.sf.l2j.gameserver.serverpackets.SystemMessage;
 import net.sf.l2j.gameserver.templates.L2Weapon;
 import net.sf.l2j.gameserver.util.Broadcast;
@@ -139,10 +138,6 @@ public class BeastSoulShot implements IItemHandler
             activeOwner.sendPacket(new SystemMessage(SystemMessageId.NOT_ENOUGH_SOULSHOTS));
             return;     
         }
-
-        // Update used soulshot count and send a Server->Client update.
-        activePet.increaseUsedSoulShots(1);
-        activeOwner.sendPacket(new PetInfo(activePet));
         
         // Pet uses the power of spirit.
         activeOwner.sendPacket(new SystemMessage(SystemMessageId.PET_USE_THE_POWER_OF_SPIRIT));
