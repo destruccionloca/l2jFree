8bda634aa37391107b7b6c8172a38e0c477ed1d0
SarutrA <nonexistent@l2jfree.com>
2007-11-10 07:38:39 +0000

    TRUNK DP IL: removing dublicate table creation
    git-svn-id: http://svn.l2jfree.com/l2jfree@2351 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/L2_DataPack_IL/sql/npcskills.sql
+++ trunk/L2_DataPack_IL/sql/npcskills.sql
@@ -10,17 +10,6 @@ CREATE TABLE npcskills (
 ) DEFAULT CHARSET=utf8;
 
 -- 
--- Table structure for table `npcskills`
--- 
-DROP TABLE IF EXISTS npcskills;
-CREATE TABLE npcskills (
-  npcid int(11) NOT NULL default '0',
-  skillid int(11) NOT NULL default '0',
-  level int(11) NOT NULL default '0',
-  PRIMARY KEY  (npcid,skillid,level)
-) TYPE=MyISAM;
-
--- 
 -- Dumping data for table `npcskills`
 -- 
 
