f4cadd424572d1e60efb463d143a199a8f7ef662
crion <medinos@gmx.de>
2007-12-19 20:05:46 +0000

    Trunk DP: Another correction
    git-svn-id: http://svn.l2jfree.com/l2jfree@2591 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/L2_DataPack_IL/data/jscript/quests/118_ToLeadAndBeLed/__init__.py
+++ trunk/L2_DataPack_IL/data/jscript/quests/118_ToLeadAndBeLed/__init__.py
@@ -87,7 +87,7 @@ class Quest (JQuest) :
    if player.getClan() == None :
      htmltext = "30517-00.htm"
      st.exitQuest(1)
-   elif player.getPledgeRank() == -1 :
+   elif player.getSubPledgeType() == -1 :
      if id==COMPLETED:
        htmltext = "<html><body>This quest has already been completed.</body></html>" 
      elif player.getLevel() < 19 or not player.getSponsor() :
