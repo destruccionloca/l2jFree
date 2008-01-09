9d03613b9c7fba829112b74929d10549a9e56f69
evill33t <ronny.stiftel@gmail.com>
2008-01-09 18:43:55 +0000

    trunk ct1 dp autotransform support/temporary respawn zone for kamael island
    git-svn-id: http://svn.l2jfree.com/l2jfree@2778 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/L2_DataPack_CT1/data/scripts/transformations/Akamanah.java
+++ trunk/L2_DataPack_CT1/data/scripts/transformations/Akamanah.java
@@ -11,9 +11,6 @@ import net.sf.l2j.gameserver.instancemanager.TransformationManager;
  */
 public class Akamanah extends L2Transformation
 {
-	private String _realName;
-	private String _realTitle;
-
 	public Akamanah()
 	{
 		// id, duration (secs), colRadius, colHeight
@@ -23,18 +20,12 @@ public class Akamanah extends L2Transformation
 	public void onTransform()
 	{
 		// Store real values
-		_realName = this.getPlayer().getName();
-		_realTitle = this.getPlayer().getTitle();
-
-		// remove title and name
-		this.getPlayer().setTitle("");
-		this.getPlayer().setName("");
+		this.getPlayer().getAppearance().setDisplayName(false);
 	}
 
 	public void onUntransform()
 	{
-		this.getPlayer().setTitle(_realTitle);
-		this.getPlayer().setName(_realName);
+		this.getPlayer().getAppearance().setDisplayName(true);
 	}
 
 	public static void main(String[] args)
