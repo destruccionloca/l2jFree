cc2378ff1ef6ffec01d959d9f7a0ff75de6443b8
Wolverine <nonexistent@l2jfree.com>
2008-01-12 14:06:34 +0000

    TRUNK DP CT1:
    - sync to L2J_JP Rev 941 by sandman
      * updated baium ai
    
    git-svn-id: http://svn.l2jfree.com/l2jfree@2815 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/L2_DataPack_CT1/data/scripts/ai/individual/baium.py
+++ trunk/L2_DataPack_CT1/data/scripts/ai/individual/baium.py
@@ -15,7 +15,7 @@ class baium (JQuest):
 
   def onTalk (self,npc,player):
     st = player.getQuestState("baium")
-    if not st : return "<html><body>You are either not carrying out your quest or don't meet the criteria.</body></html>"
+    if not st : return "<html><head><body>You are either not carrying out your quest or don't meet the criteria.</body></html>"
     npcId = npc.getNpcId()
     if npcId == 29025 :
       if st.getInt("ok"):
@@ -27,27 +27,37 @@ class baium (JQuest):
         st.exitQuest(1)
         return "Conditions are not right to wake up Baium"
     elif npcId == 31862 :
-      if st.getQuestItemsCount(4295) : # bloody fabric
-        st.takeItems(4295,1)
-        player.teleToLocation(113100,14500,10077)
-        BaiumManager.getInstance().addPlayerToLair(player)
-        st.set("ok","1")
+      if BaiumManager.getInstance().isEnableEnterToLair() :
+        if player.isFlying() :
+          return "<html><body>Angelic Vortex:<br>You may not enter while flying a wyvern</body></html>"
+        if st.getQuestItemsCount(4295) : # bloody fabric
+          st.takeItems(4295,1)
+          player.teleToLocation(113100,14500,10077)
+          BaiumManager.getInstance().addPlayerToLair(player)
+          st.set("ok","1")
+        else :
+          return "<html><head><body>Angelic Vortex:<br>You do not have enough items</body></html>"
       else :
-        return '<html><body>Angelic Vortex:<br>You do not have enough items</body></html>'
+        return "<html><body>Angelic Vortex:<br>You may not enter at this time</body></html>"
     return
 
-  def onKill(self,npc,player,isPet):
+  def onKill (self,npc,player,isPet):
     st = player.getQuestState("baium")
-    BaiumManager.getInstance().setCubeSpawn()
     if not st: return
+    BaiumManager.getInstance().setCubeSpawn()
     st.exitQuest(1)
 
-# Quest class and state definition
-QUEST       = baium(-1, "baium", "ai")
+  def onAttack(self,npc,player,damage,isPet):
+    st = player.getQuestState("baium")
+    if not st: return
+    BaiumManager.getInstance().setLastAttackTime()
 
+# Quest class and state definition
+QUEST = baium(-1, "baium", "ai")
 # Quest NPC starter initialization
 QUEST.addStartNpc(29025)
 QUEST.addStartNpc(31862)
 QUEST.addTalkId(29025)
 QUEST.addTalkId(31862)
 QUEST.addKillId(29020)
+QUEST.addAttackId(29020)
