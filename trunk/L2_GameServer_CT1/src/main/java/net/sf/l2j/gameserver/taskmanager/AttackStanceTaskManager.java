c256e1fe89d554bd79d1a6d87cdc4bb7266f8299
crion <medinos@gmx.de>
2008-01-14 19:51:53 +0000

    fix for [2827]
    git-svn-id: http://svn.l2jfree.com/l2jfree@2830 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/L2_GameServer_CT1/src/main/java/net/sf/l2j/gameserver/taskmanager/AttackStanceTaskManager.java
+++ trunk/L2_GameServer_CT1/src/main/java/net/sf/l2j/gameserver/taskmanager/AttackStanceTaskManager.java
@@ -11,7 +11,6 @@
  * 
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>.
- */ttp://www.gnu.org/copyleft/gpl.html
  */
 package net.sf.l2j.gameserver.taskmanager;
 
