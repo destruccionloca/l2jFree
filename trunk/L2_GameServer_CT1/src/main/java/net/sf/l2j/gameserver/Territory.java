fac5d45e325d74c6191044b3b38300197cc801c6
Wolverine <nonexistent@l2jfree.com>
2008-01-14 19:16:43 +0000

    TRUNK GS CT1:
    - GPL V3
    
    git-svn-id: http://svn.l2jfree.com/l2jfree@2827 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/L2_GameServer_CT1/src/main/java/net/sf/l2j/gameserver/Territory.java
+++ trunk/L2_GameServer_CT1/src/main/java/net/sf/l2j/gameserver/Territory.java
@@ -1,8 +1,8 @@
 /*
-/*
- * This program is free software; you can redistribute it and/or modify it under
+ * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2, or (at your option) any later version.
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  * 
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
@@ -10,17 +10,8 @@
  * details.
  * 
  * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
- * Place - Suite 330, Boston, MA 02111-1307, USA.
- * 
- * http://www.gnu.org/copyleft/gpl.html
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  */
-/*
- * coded by Balancer ported to L2JRU by Mr balancer@balancer.ru http://balancer.ru
- * 
- * version 0.1.1, 2005-06-07 version 0.1, 2005-03-16
- */
-
 package net.sf.l2j.gameserver;
 
 import javolution.util.FastMap;
