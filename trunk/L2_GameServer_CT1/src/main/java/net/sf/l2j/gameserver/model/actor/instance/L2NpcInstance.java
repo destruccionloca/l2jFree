08b771ca28f3f53924fa10dd06fb0526ca17852b
crion <medinos@gmx.de>
2008-01-15 16:38:07 +0000

    Trunk GS CT1: Fix for missing Priest of Dawn/Dusk Priestess
    git-svn-id: http://svn.l2jfree.com/l2jfree@2832 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/L2_GameServer_CT1/src/main/java/net/sf/l2j/gameserver/model/actor/instance/L2NpcInstance.java
+++ trunk/L2_GameServer_CT1/src/main/java/net/sf/l2j/gameserver/model/actor/instance/L2NpcInstance.java
@@ -2187,7 +2187,8 @@ public class L2NpcInstance extends L2Character
                 case 31084:
                 case 31168:
                 case 31692:
-                case 31694:                
+                case 31694:
+                case 31997:
                 switch (playerCabal) 
                 {
                     case SevenSigns.CABAL_DAWN:
@@ -2228,7 +2229,8 @@ public class L2NpcInstance extends L2Character
                 case 31091:
                 case 31169:
                 case 31693:
-                case 31695:                 
+                case 31695:
+                case 31998:
                 switch (playerCabal) 
                 {
                     case SevenSigns.CABAL_DUSK:
