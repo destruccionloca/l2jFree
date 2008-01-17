1515eeef448a2f3f06193fb64599ccdbea34b731
crion <medinos@gmx.de>
2008-01-17 22:40:22 +0000

    Mercenary fix, thx Wolverine
    git-svn-id: http://svn.l2jfree.com/l2jfree@2859 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/L2_GameServer_CT1/src/main/java/net/sf/l2j/gameserver/model/L2ItemInstance.java
+++ trunk/L2_GameServer_CT1/src/main/java/net/sf/l2j/gameserver/model/L2ItemInstance.java
@@ -1323,7 +1323,7 @@ public final class L2ItemInstance extends L2Object
 		if (Config.ASSERT)
 			assert getPosition().getWorldRegion() == null;
 
-		if (Config.GEODATA)
+		if (Config.GEODATA && dropper != null)
 		{
 			Location dropDest = GeoData.getInstance().moveCheck(dropper.getX(), dropper.getY(), dropper.getZ(), x, y, z);
 			x = dropDest.getX();
