ae16ca20024d9cddb2a985c2498cdccb31f7ab14
evill33t <ronny.stiftel@gmail.com>
2008-01-26 00:29:08 +0000

    l2j-commons updates, moved unnecessary stuff out of core
    git-svn-id: http://svn.l2jfree.com/l2jfree@2915 6ff00ba9-25cd-4355-987e-099c62bc2628

---
+++ trunk/l2j-commons/src/main/java/net/sf/l2j/config/L2Config.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package net.sf.l2j.config;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * @author evill33t
+ * 
+ */
+public class L2Config
+{
+	private final static Log _log = LogFactory.getLog(L2Config.class.getName());
+	
+	
+	private L2Config()
+	{
+	}
+}
