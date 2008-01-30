5b2b005c5e60c0f0beb44b7f1d063439c600cb85
Respawner <respawneral@gmail.com>
2008-01-30 17:04:14 +0000

    Trunk DP CT1 : Sync to L2JDP 3981, 3982, 3983, 3984, 3985, 3986, 3988, 3990 	* JYTHON: Fix for quest 172 error reported on forums, thx zhyla. 	* Circlet, ubiquitous and dream armor updates (izzy) 	* Emerald boar (izzy) 	* Hellbound link (izzy) 	* Some Rune and Schuttgart guard-related fixes (lanabel) 	* Valhalla Magic Regen SA (rodo) 	* Item skill: mana gain (zhyla) 	* Adding few CUSTOM AND TEMPORARY spawns for Divine Emissary. This will allow players to at least complete their quests until we get the real spawn info... by Fulminus 	* XML: Shield of Faith. by melerix 	* XML: pReuse for Super Haste. by melerix 	* lil html updates and stats corrections for NPC: 27323 (Divine Emissary) by Kerberos 	* Quest 283: The Few The Proud The Brave. Little fix. Thanks Vashts6583 	* Npc stats by Kerberos
    git-svn-id: http://svn.l2jfree.com/l2jfree@2978 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/L2_DataPack_CT1/data/scripts/quests/172_NewHorizons/__init__.py
+++ trunk/L2_DataPack_CT1/data/scripts/quests/172_NewHorizons/__init__.py
@@ -1,6 +1,5 @@
 # Contributed by t0rm3nt0r (tormentor2000@mail.ru) to the Official L2J Datapack Project
 # Visit http://forum.l2jdp.com for more details.
-
 import sys
 from net.sf.l2j.gameserver.model.quest import State
 from net.sf.l2j.gameserver.model.quest import QuestState
@@ -10,18 +9,16 @@ from net.sf.l2j.gameserver.model.quest.jython import QuestJython as JQuest
 qn = "172_NewHorizons"
 
 #NPC'S
-ZENYA = 32140 
+ZENYA = 32140
 RAGARA = 32163
 
 #ITEM'S
-SCROLL_OF_ESCAPE_GIRAN = 7559 
-MARK_OF_TRAVELER       = 7570 
+SCROLL_OF_ESCAPE_GIRAN = 7559
+MARK_OF_TRAVELER       = 7570
  
 class Quest (JQuest) :
 
- def __init__(self,id,name,descr):
-     JQuest.__init__(self,id,name,descr)
-     self.questItemIds = [MARK_OF_TRAVELER]
+ def __init__(self,id,name,descr): JQuest.__init__(self,id,name,descr)
 
  def onEvent (self,event,st) :
      htmltext = event
@@ -51,21 +48,19 @@ class Quest (JQuest) :
          htmltext = "32140-01.htm"
        else :
          htmltext = "32140-02.htm"
-         st.exitQuest(1)       
+         st.exitQuest(1)
      elif id == State.STARTED :
-       if npcId == ZENYA : 
+       if npcId == ZENYA :
          if cond == 1 :
            htmltext = "32140-04.htm"
        elif npcId == RAGARA :
          if cond == 1 :
            htmltext = "32163-01.htm"
      return htmltext
- 
-QUEST     = Quest(172,qn,"New Horizons") 
- 
-QUEST.addStartNpc(ZENYA) 
 
-QUEST.addTalkId(ZENYA) 
-QUEST.addTalkId(RAGARA) 
- 
+QUEST = Quest(172,qn,"New Horizons")
 
+QUEST.addStartNpc(ZENYA)
+
+QUEST.addTalkId(ZENYA)
+QUEST.addTalkId(RAGARA)
