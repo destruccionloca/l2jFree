180ac7eec412b161f26bd90e6c541768d913d2c6
SarutrA <nonexistent@l2jfree.com>
2008-02-03 13:00:08 +0000

    TRUNK DP CT1: litle correction for dirrectory
    git-svn-id: http://svn.l2jfree.com/l2jfree@3004 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/L2_DataPack_CT1/data/scripts/quests/65_CertifiedSoulBreaker/__init__.py
+++ trunk/L2_DataPack_CT1/data/scripts/quests/65_CertifiedSoulBreaker/__init__.py
@@ -6,8 +6,8 @@ from net.sf.l2j.gameserver.ai                 import CtrlIntention
 from net.sf.l2j.gameserver.model.quest        import State
 from net.sf.l2j.gameserver.model.quest        import QuestState
 from net.sf.l2j.gameserver.model.quest.jython import QuestJython as JQuest
-from net.sf.l2j.gameserver.serverpackets      import CreatureSay
-from net.sf.l2j.gameserver.serverpackets      import SocialAction
+from net.sf.l2j.gameserver.network.serverpackets      import CreatureSay
+from net.sf.l2j.gameserver.network.serverpackets      import SocialAction
 
 qn = "65_CertifiedSoulBreaker"
 
