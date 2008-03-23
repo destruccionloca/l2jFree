55dd9115ec60a38043dbf8e56ee36c5467a7d0b4
Respawner <respawneral@gmail.com>
2008-03-23 14:58:33 +0000

    Trunk GS CT1 : Fix for my last commit
    git-svn-id: http://svn.l2jfree.com/l2jfree@3268 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/L2_GameServer_CT1/src/main/java/net/sf/l2j/gameserver/handler/itemhandlers/EnchantScrolls.java
+++ trunk/L2_GameServer_CT1/src/main/java/net/sf/l2j/gameserver/handler/itemhandlers/EnchantScrolls.java
@@ -57,9 +57,9 @@ public class EnchantScrolls implements IItemHandler
 				(itemId == 957 || itemId == 958 || itemId == 953 || itemId == 954 ||	// Crystal scrolls D and C Grades
 				 itemId == 949 || itemId == 950 || itemId == 931 || itemId == 732 ||	// Crystal scrolls B and A Grades
 				 itemId == 961 || itemId == 962))										// Crystal scrolls S Grade
-			activeChar.sendPacket(new ChooseInventoryItem(itemId));
+			activeChar.sendPacket(new ChooseInventoryItem(itemId - 2));
 		else
-			activeChar.sendPacket(new ChooseInventoryItem(item.getItemId()));
+			activeChar.sendPacket(new ChooseInventoryItem(itemId));
 
 		return;
 	}
