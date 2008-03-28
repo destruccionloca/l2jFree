e97ca07cd525855bca799a94bf5100f0940997af
hanwik <nonexistent@l2jfree.com>
2008-03-28 20:57:09 +0000

    Trunk GS CT1: - Fixes not being able to create character
    git-svn-id: http://svn.l2jfree.com/l2jfree@3317 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/L2_GameServer_CT1/src/main/java/net/sf/l2j/gameserver/model/actor/instance/L2PcInstance.java
+++ trunk/L2_GameServer_CT1/src/main/java/net/sf/l2j/gameserver/model/actor/instance/L2PcInstance.java
@@ -9315,7 +9315,7 @@ public final class L2PcInstance extends L2PlayableInstance
 
     public boolean isSubClassActive()
     {
-        return getBaseClass() != getClassId().getId();
+        return _classIndex > 0;
     }
 
     public Map<Integer, SubClass> getSubClasses()
