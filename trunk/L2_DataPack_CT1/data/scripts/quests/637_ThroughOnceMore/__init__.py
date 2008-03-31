8b69246b59625ca7ae4cf7dca91e05e4a9330a83
Respawner <respawneral@gmail.com>
2008-03-31 16:22:41 +0000

    Trunk DP CT1 : Sync to L2JDP 4343, 4344, 4345, 4346, 4347, 4348, 4349, 4350, 	* Nobles shop update by Kerberos 	* One missing book html used in quest 23 by Kerberos 	* Update for quest 617 - Gather The Flames: added starter NPC Blacksmith Hilda by Kerberos 	* Added some missing npcs into fishing shots quest by Kerberos 	* JYTHON: Small typo fix in 648...need the int, not a string by Emperorc 	* New quest 23 - Lidias Heart, tnx Skeleton and Eyerobot 	TP fixes in/to Pagan Temple 	C5 updates for quests 621 & 622 (updated reward NPCs) 	Update for quest 637 Through the Gate Once More 	Corrected few minions in Forrest of Dead location 	Added missing import in quest 21 Hidden Thruth 	by Kerberos 	* npc.sql cleanup by Kerberos 	* JYTHON: Erasing dummy recipe from reward in quest 647 (thx Sandro) 	* HTML: Few typos (thx Loup_Solitaire, Torvitas) 	* Updated nobless ports in kamael village by Kerberos 	* XML: Fix for skill 4091, requested by kerberos_20 	* npc html updates by Kerberos 	* XML: Skill Chant of Protection (thx nbd) (Need core sync!)
    git-svn-id: http://svn.l2jfree.com/l2jfree@3346 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/L2_DataPack_CT1/data/scripts/quests/637_ThroughOnceMore/__init__.py
+++ trunk/L2_DataPack_CT1/data/scripts/quests/637_ThroughOnceMore/__init__.py
@@ -1,7 +1,7 @@
 # Made by BiTi! v0.2
 # v0.2.1 by DrLecter
 import sys
-
+from net.sf.l2j import Config
 from net.sf.l2j.gameserver.model.quest import State
 from net.sf.l2j.gameserver.model.quest import QuestState
 from net.sf.l2j.gameserver.model.quest.jython import QuestJython as JQuest
@@ -9,46 +9,41 @@ from net.sf.l2j.gameserver.model.quest.jython import QuestJython as JQuest
 qn = "637_ThroughOnceMore"
 
 #Drop rate
-CHANCE=40
+DROP_CHANCE=90
 #Npc
 FLAURON = 32010
 #Items
-VISITORSMARK,NECROHEART,MARK,ANTKEY = 8065,8066,8067,8273
-Quest_Mobs = [ 21565, 21566, 21567, 21568 ]
+VISITOR_MARK,FADEDMARK,NECROHEART,MARK = 8064,8065,8066,8067
+
 class Quest (JQuest) :
 
 
  def __init__(self,id,name,descr):
      JQuest.__init__(self,id,name,descr)
+     self.questItemIds = [NECROHEART]
 
  def onEvent (self,event,st) :
-    htmltext = "<html><body>You are either not on a quest that involves this NPC, or you don't meet this NPC's minimum quest requirements.</body></html>"
-    if event == "2" :
-       return "32010-02.htm"
-    if event == "3" :
-       return "32010-03.htm"
-    if event == "6" :
-       return "32010-06.htm"
-    if event == "7" :
-       return "32010-07.htm"
-    if event == "4" :
+    htmltext = event
+    if htmltext == "32010-03.htm" :
        st.set("cond","1")
        st.setState(State.STARTED)
-       st.takeItems(VISITORSMARK,1)
+       st.takeItems(FADEDMARK,1)
        st.playSound("ItemSound.quest_accept")
-       htmltext = "32010-04.htm"
     return htmltext
 
  def onTalk (self, npc, player):
-   htmltext = "<html><body>You are either not carrying out your quest or don't meet the criteria.</body></html>"
+   htmltext = "<html><body>You are either not on a quest that involves this NPC, or you don't meet this NPC's minimum quest requirements.</body></html>"
    st = player.getQuestState(qn)
    if st :
      id = st.getState()
      cond = st.getInt("cond")
-     if id == State.CREATED:
-        if player.getLevel()>72 and st.getQuestItemsCount(VISITORSMARK) :
+     if id == State.CREATED :
+        if player.getLevel()>72 and st.getQuestItemsCount(FADEDMARK) :
            htmltext = "32010-02.htm"
-        if player.getLevel()<73 or not st.getQuestItemsCount(VISITORSMARK) :
+        elif player.getLevel()>72 and st.getQuestItemsCount(VISITOR_MARK) :
+           htmltext = "32010-01a.htm"
+           st.exitQuest(1)
+        else:
            htmltext = "32010-01.htm"
            st.exitQuest(1)
      elif id == State.STARTED :
@@ -56,8 +51,8 @@ class Quest (JQuest) :
           htmltext = "32010-05.htm"
           st.takeItems(NECROHEART,10)
           st.giveItems(MARK,1)
-          st.giveItems(ANTKEY,10)
-          st.exitQuest(False) 
+          st.giveItems(8273,10)
+          st.exitQuest(False)
           st.playSound("ItemSound.quest_finish")
        else :
           htmltext = "32010-04.htm"
@@ -67,16 +62,20 @@ class Quest (JQuest) :
    st = player.getQuestState(qn)
    if st :
      if st.getState() == State.STARTED :
-       npcId = npc.getNpcId()
        count = st.getQuestItemsCount(NECROHEART)
-       if npcId in Quest_Mobs:
-          if st.getRandom(100)<CHANCE and count<10 :
-             st.giveItems(NECROHEART,1)
-             if count == 9 :
+       if st.getInt("cond") == 1 and count < 10 :
+          chance = DROP_CHANCE * Config.RATE_DROP_QUEST
+          numItems, chance = divmod(int(chance),100)
+          if st.getRandom(100) < chance : 
+             numItems += 1
+          if numItems :
+             if count + numItems >= 10 :
+                numItems = 10 - count
                 st.playSound("ItemSound.quest_middle")
                 st.set("cond","2")
              else:
                 st.playSound("ItemSound.quest_itemget")
+             st.giveItems(NECROHEART,int(numItems))
    return
 
 QUEST       = Quest(637,qn,"Through the Gate Once More")
@@ -85,5 +84,5 @@ QUEST.addStartNpc(FLAURON)
 
 QUEST.addTalkId(FLAURON)
 
-for mobId in Quest_Mobs :
-  QUEST.addKillId(mobId)
+for mob in range(21565,21568):
+    QUEST.addKillId(mob)
