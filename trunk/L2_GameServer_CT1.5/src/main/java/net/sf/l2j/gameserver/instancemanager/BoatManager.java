b4f93354879b15d8ff30ca63e7db496f40b0d241
evill33t <ronny.stiftel@gmail.com>
2008-05-08 22:57:43 +0000

    boat fix
    git-svn-id: http://svn.l2jfree.com/l2jfree@3756 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/L2_GameServer_CT1.5/src/main/java/net/sf/l2j/gameserver/instancemanager/BoatManager.java
+++ trunk/L2_GameServer_CT1.5/src/main/java/net/sf/l2j/gameserver/instancemanager/BoatManager.java
@@ -153,6 +153,8 @@ public class BoatManager
 		//npcDat.set("name", "");
 		npcDat.set("collision_radius", 0);
 		npcDat.set("collision_height", 0);
+		npcDat.set("fcollision_radius", 0);
+		npcDat.set("fcollision_height", 0);
 		npcDat.set("sex", "male");
 		npcDat.set("type", "");
 		npcDat.set("baseAtkRange", 0);
