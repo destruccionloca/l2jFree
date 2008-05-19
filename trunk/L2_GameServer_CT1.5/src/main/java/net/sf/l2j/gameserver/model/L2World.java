ddc32f03df648c44e86251b0c19fcacb36c1ebd1
Michiru <nonexistent@l2jfree.com>
2008-05-19 07:30:07 +0000

    TRUNK GS: deleted L2WorldMBean for jmx
    git-svn-id: http://svn.l2jfree.com/l2jfree@3822 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/L2_GameServer_CT1.5/src/main/java/net/sf/l2j/gameserver/model/L2World.java
+++ trunk/L2_GameServer_CT1.5/src/main/java/net/sf/l2j/gameserver/model/L2World.java
@@ -36,7 +36,7 @@ import org.apache.commons.logging.LogFactory;
  * 
  * @version $Revision: 1.21.2.5.2.7 $ $Date: 2005/03/27 15:29:32 $
  */
-public final class L2World implements L2WorldMBean
+public final class L2World
 {
     private final static Log _log = LogFactory.getLog(L2World.class.getName());
     
