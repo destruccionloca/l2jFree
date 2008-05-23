e692259b63cd7a586a7bed30b8927222570d609c
Noctarius <noctaria@gmail.com>
2008-05-23 18:44:40 +0000

    Refactoring Part 2
    git-svn-id: http://svn.l2jfree.com/l2jfree@3841 6ff00ba9-25cd-4355-987e-099c62bc2628

---
+++ trunk/l2j-commons/src/main/java/com/l2jfree/config/L2Config.java
@@ -0,0 +1,39 @@
+/*
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.l2jfree.config;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * @author evill33t
+ * 
+ */
+public class L2Config
+{
+	private final static Log _log = LogFactory.getLog(L2Config.class.getName());
+	
+	
+	private L2Config()
+	{
+	}
+}
