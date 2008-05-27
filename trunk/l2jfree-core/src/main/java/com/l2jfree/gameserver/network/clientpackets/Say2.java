9cb3910208949eee72cd4d6629e08aaa90bea39a
Michiru <nonexistent@l2jfree.com>
2008-05-27 07:12:32 +0000

    core: change for voicecmd to get .cmd + text in vcmd (thx G1ta0)
    git-svn-id: http://svn.l2jfree.com/l2jfree@3932 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/l2jfree-core/src/main/java/com/l2jfree/gameserver/network/clientpackets/Say2.java
+++ trunk/l2jfree-core/src/main/java/com/l2jfree/gameserver/network/clientpackets/Say2.java
@@ -15,8 +15,6 @@
 package com.l2jfree.gameserver.network.clientpackets;
 
 import java.nio.BufferUnderflowException;
-import java.util.StringTokenizer;
-
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -41,162 +39,165 @@ import com.l2jfree.gameserver.network.serverpackets.SystemMessage;
  */
 public class Say2 extends L2GameClientPacket
 {
-    private static final String _C__38_SAY2 = "[C] 38 Say2";
-    private final static Log _log = LogFactory.getLog(Say2.class.getName());
-    private static Log _logChat = LogFactory.getLog("chat");
+	private static final String	_C__38_SAY2	= "[C] 38 Say2";
+	private final static Log	_log		= LogFactory.getLog(Say2.class.getName());
+	private static Log			_logChat	= LogFactory.getLog("chat");
 
-    private String _text;
-    private SystemChatChannelId _type;
-    private String _target;
-    /**
-     * packet type id 0x38
-     * format:      cSd (S)
-     * @param decrypt
-     */
-    @Override
-    protected void readImpl()
-    {
-        _text = readS();
-        _text = _text.replaceAll("\\\\n","");
-        try
-        {
-            _type = SystemChatChannelId.getChatType(readD());
-        }
-        catch (BufferUnderflowException e) 
-        {
-            _type = SystemChatChannelId.Chat_None;
-        }
-        _target = _type == SystemChatChannelId.Chat_Tell ? readS() : null;
-    }
+	private String				_text;
+	private SystemChatChannelId	_type;
+	private String				_target;
 
-    @Override
-    protected void runImpl()
-    {
-        L2PcInstance activeChar = getClient().getActiveChar();
+	/**
+	 * packet type id 0x38
+	 * format:      cSd (S)
+	 * @param decrypt
+	 */
+	@Override
+	protected void readImpl()
+	{
+		_text = readS();
+		_text = _text.replaceAll("\\\\n", "");
+		try
+		{
+			_type = SystemChatChannelId.getChatType(readD());
+		}
+		catch (BufferUnderflowException e)
+		{
+			_type = SystemChatChannelId.Chat_None;
+		}
+		_target = _type == SystemChatChannelId.Chat_Tell ? readS() : null;
+	}
 
-        // If no channel is choosen - return
-        if (_type == SystemChatChannelId.Chat_None)
-        {
-            _log.warn("[Say2.java] Illegal chat channel was used.");
-            return;
-        }
+	@Override
+	protected void runImpl()
+	{
+		L2PcInstance activeChar = getClient().getActiveChar();
 
-        if (activeChar == null)
-        {
-            _log.warn("[Say2.java] Active Character is null.");
-            return;
-        }
- 
-        // If player is chat banned
-        if (ObjectRestrictions.getInstance()
-        		.checkRestriction(activeChar, AvailableRestriction.PlayerChat))
-        {
-            if (_type != SystemChatChannelId.Chat_User_Pet && _type !=SystemChatChannelId.Chat_Tell)
-            {
-                // [L2J_JP EDIT]
-                activeChar.sendPacket(new SystemMessage(SystemMessageId.CHATTING_IS_CURRENTLY_PROHIBITED));
-                return;
-            }
-        }
+		// If no channel is choosen - return
+		if (_type == SystemChatChannelId.Chat_None)
+		{
+			_log.warn("[Say2.java] Illegal chat channel was used.");
+			return;
+		}
 
-        if (activeChar.isCursedWeaponEquipped())
-        {
-            switch(_type)
-            {
-                case Chat_Shout:
-                case Chat_Market:
-                    SystemMessage sm = new SystemMessage(SystemMessageId.SHOUT_AND_TRADE_CHAT_CANNOT_BE_USED_WHILE_POSSESSING_CURSED_WEAPON);
-                    activeChar.sendPacket(sm);
-                    return;
-            }
-        }
+		if (activeChar == null)
+		{
+			_log.warn("[Say2.java] Active Character is null.");
+			return;
+		}
 
-        // If player is jailed
-        if ((activeChar.isInJail() || activeChar.isInsideZone(L2Zone.FLAG_JAIL)) && Config.JAIL_DISABLE_CHAT && !activeChar.isGM())
-        {
-            if (_type != SystemChatChannelId.Chat_User_Pet && _type != SystemChatChannelId.Chat_Normal)
-            {
-                activeChar.sendMessage("You can not chat with the outside of the jail.");
-                return;
-            }
-        }
-        
-        // If Petition and GM use GM_Petition Channel
-        if (_type == SystemChatChannelId.Chat_User_Pet && activeChar.isGM()) 
-            _type = SystemChatChannelId.Chat_GM_Pet;
-        
-        // Say Filter implementation
-        if(Config.USE_SAY_FILTER) 
-        {
-            for(String pattern : Config.FILTER_LIST)
-            {
-                _text = _text.replaceAll("(?i)" + pattern,"^_^");
-            }
-        }
+		// If player is chat banned
+		if (ObjectRestrictions.getInstance().checkRestriction(activeChar, AvailableRestriction.PlayerChat))
+		{
+			if (_type != SystemChatChannelId.Chat_User_Pet && _type != SystemChatChannelId.Chat_Tell)
+			{
+				// [L2J_JP EDIT]
+				activeChar.sendPacket(new SystemMessage(SystemMessageId.CHATTING_IS_CURRENTLY_PROHIBITED));
+				return;
+			}
+		}
 
-        if (_text.startsWith(".") && !_text.startsWith("..") &&
-            _type == SystemChatChannelId.Chat_Normal)
-        {
-            StringTokenizer st = new StringTokenizer(_text);
+		if (activeChar.isCursedWeaponEquipped())
+		{
+			switch (_type)
+			{
+			case Chat_Shout:
+			case Chat_Market:
+				SystemMessage sm = new SystemMessage(SystemMessageId.SHOUT_AND_TRADE_CHAT_CANNOT_BE_USED_WHILE_POSSESSING_CURSED_WEAPON);
+				activeChar.sendPacket(sm);
+				return;
+			}
+		}
 
-            if (st.countTokens()>=1)
-            {
-                String command = st.nextToken().substring(1);
-                String params = "";
-                if (st.countTokens()==0)
-                {
-                    if (activeChar.getTarget()!=null) params=activeChar.getTarget().getName();
-                }
-                else params=st.nextToken().trim();
+		// If player is jailed
+		if ((activeChar.isInJail() || activeChar.isInsideZone(L2Zone.FLAG_JAIL)) && Config.JAIL_DISABLE_CHAT && !activeChar.isGM())
+		{
+			if (_type != SystemChatChannelId.Chat_User_Pet && _type != SystemChatChannelId.Chat_Normal)
+			{
+				activeChar.sendMessage("You can not chat with the outside of the jail.");
+				return;
+			}
+		}
 
-                IVoicedCommandHandler vch = VoicedCommandHandler.getInstance().getVoicedCommandHandler(command);
+		// If Petition and GM use GM_Petition Channel
+		if (_type == SystemChatChannelId.Chat_User_Pet && activeChar.isGM())
+			_type = SystemChatChannelId.Chat_GM_Pet;
 
-                if (vch != null)
-                    vch.useVoicedCommand(command, activeChar, params);
-                else
-                    _log.warn("No handler registered for voice command '"+command+"'");
-            }
-            
-            return;
-        }
-        // Some custom implementation to show how to add channels
-        // (for me Chat_System is used for emotes - further informations
-        // in ChatSystem.java)
-        // else if (_text.startsWith("(")&&
-        //		_text.length() >= 5 &&
-        //		_type == SystemChatChannelId.Chat_Normal)
-        //{
-        //	_type = SystemChatChannelId.Chat_System;
-        //	
-        //	_text = _text.substring(1);
-        //	_text = "*" + _text + "*";
-        //}
-        
-        // Log chat to file
-        if (Config.LOG_CHAT) 
-        {
-            if (_type == SystemChatChannelId.Chat_Tell)
-                _logChat.info( _type.getName() + "[" + activeChar.getName() + " to "+_target+"] " + _text);
-            else
-                _logChat.info( _type.getName() + "[" + activeChar.getName() + "] " + _text);
-        }
-        
-        IChatHandler ich = ChatHandler.getInstance().getChatHandler(_type);
-        
-        if (ich != null)
-            ich.useChatHandler(activeChar, _target, _type, _text);
-    }
+		// Say Filter implementation
+		if (Config.USE_SAY_FILTER)
+		{
+			for (String pattern : Config.FILTER_LIST)
+			{
+				_text = _text.replaceAll("(?i)" + pattern, "^_^");
+			}
+		}
 
-    /* (non-Javadoc)
-     * @see com.l2jfree.gameserver.clientpackets.ClientBasePacket#getType()
-     */
-    @Override
-    public String getType()
-    {
-        return _C__38_SAY2;
-    }
+		if (_text.startsWith(".") && !_text.startsWith("..") && _type == SystemChatChannelId.Chat_Normal)
+		{
+			String[] _commandParams = _text.split(" ");
 
-    public void changeString(String newString) { _text = newString; }
-   
-    public String getSay() { return _text; }
+			String command = _commandParams[0].substring(1);
+			String params = "";
+
+			// if entered "command text"
+			if (_commandParams.length > 1)
+				params = _text.substring(1 + command.length()).trim(); // get all text
+			else if (activeChar.getTarget() != null)
+				params = activeChar.getTarget().getName();
+
+			IVoicedCommandHandler vch = VoicedCommandHandler.getInstance().getVoicedCommandHandler(command);
+
+			if (vch != null)
+				vch.useVoicedCommand(command, activeChar, params);
+			else
+				_log.warn("No handler registered for voice command '" + command + "'");
+
+			return;
+		}
+		// Some custom implementation to show how to add channels
+		// (for me Chat_System is used for emotes - further informations
+		// in ChatSystem.java)
+		// else if (_text.startsWith("(")&&
+		//		_text.length() >= 5 &&
+		//		_type == SystemChatChannelId.Chat_Normal)
+		//{
+		//	_type = SystemChatChannelId.Chat_System;
+		//	
+		//	_text = _text.substring(1);
+		//	_text = "*" + _text + "*";
+		//}
+
+		// Log chat to file
+		if (Config.LOG_CHAT)
+		{
+			if (_type == SystemChatChannelId.Chat_Tell)
+				_logChat.info(_type.getName() + "[" + activeChar.getName() + " to " + _target + "] " + _text);
+			else
+				_logChat.info(_type.getName() + "[" + activeChar.getName() + "] " + _text);
+		}
+
+		IChatHandler ich = ChatHandler.getInstance().getChatHandler(_type);
+
+		if (ich != null)
+			ich.useChatHandler(activeChar, _target, _type, _text);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.l2jfree.gameserver.clientpackets.ClientBasePacket#getType()
+	 */
+	@Override
+	public String getType()
+	{
+		return _C__38_SAY2;
+	}
+
+	public void changeString(String newString)
+	{
+		_text = newString;
+	}
+
+	public String getSay()
+	{
+		return _text;
+	}
 }
