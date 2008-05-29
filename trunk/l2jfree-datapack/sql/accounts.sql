2679c044cac14c15911eabeddf6a7e5f3613ab46
Michiru <nonexistent@l2jfree.com>
2008-05-29 15:13:09 +0000

    TRUNK DP: update for [3955]
    git-svn-id: http://svn.l2jfree.com/l2jfree@3956 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/l2jfree-datapack/sql/accounts.sql
+++ trunk/l2jfree-datapack/sql/accounts.sql
@@ -6,6 +6,7 @@ CREATE TABLE IF NOT EXISTS `accounts` (
   `password` VARCHAR(45) ,
   `lastactive` DECIMAL(20),
   `accessLevel` INT NOT NULL DEFAULT '0',
+  `lastServerId` INT NOT NULL DEFAULT '0',
   `lastIP` VARCHAR(20),
   PRIMARY KEY (`login`)
 ) DEFAULT CHARSET=utf8;
