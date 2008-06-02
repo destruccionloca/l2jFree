7c15f1df606308f4783345ce04a047299084246d
Noctarius <noctaria@gmail.com>
2008-06-02 19:29:04 +0000

    refactoring classpath to new com.l2jfree changes datapack version in pom.xml to new release 1.1.0
    git-svn-id: http://svn.l2jfree.com/l2jfree@4003 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/l2jfree-login/dist/RegisterGameServer.bat
+++ trunk/l2jfree-login/dist/RegisterGameServer.bat
@@ -2,7 +2,7 @@
 SET OLDCLASSPATH=%CLASSPATH%
 call setenv.bat
 
-@java -Djava.util.logging.config.file=console.cfg net.sf.l2j.gsregistering.GameServerRegister
+@java -Djava.util.logging.config.file=console.cfg com.l2jfree.gsregistering.GameServerRegister
 
 SET CLASSPATH=%OLDCLASSPATH%
 @pause
