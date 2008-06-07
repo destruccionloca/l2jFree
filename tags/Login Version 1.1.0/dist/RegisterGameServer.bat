b0b65533e01af71c34e8c3d8569b0116fb73a845
Noctarius <noctaria@gmail.com>
2008-06-07 17:50:07 +0000

    Removed file/folder
    git-svn-id: http://svn.l2jfree.com/l2jfree@4065 6ff00ba9-25cd-4355-987e-099c62bc2628

--- tags/Login Version 1.1.0/dist/RegisterGameServer.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-SET OLDCLASSPATH=%CLASSPATH%
-call setenv.bat
-
-@java -Djava.util.logging.config.file=console.cfg com.l2jfree.gsregistering.GameServerRegister
-
-SET CLASSPATH=%OLDCLASSPATH%
-@pause
