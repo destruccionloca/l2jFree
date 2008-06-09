416c7542c7549bf17ef75d6a20406ff47ccc65f8
hanwik <nonexistent@l2jfree.com>
2008-06-09 22:43:11 +0000

    final fix for http://www.l2jfree.com/index.php?topic=4170.0
    git-svn-id: http://svn.l2jfree.com/l2jfree@4100 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/l2jfree-core/src/main/java/com/l2jfree/gameserver/datatables/ItemTable.java
+++ trunk/l2jfree-core/src/main/java/com/l2jfree/gameserver/datatables/ItemTable.java
@@ -713,7 +713,7 @@ public class ItemTable
 		// Create and Init the L2ItemInstance corresponding to the Item Identifier
 		L2ItemInstance item = new L2ItemInstance(IdFactory.getInstance().getNextId(), itemId, itemId);
 
-		if (process.equalsIgnoreCase("loot") && !Config.AUTO_LOOT && (itemId != 57 && itemId != 5575 && itemId < 6360 && itemId > 6362 && Config.AUTO_LOOT_ADENA))
+		if (process.equalsIgnoreCase("loot") && ((!Config.AUTO_LOOT && itemId != 57 && itemId != 5575 && itemId < 6360 && itemId > 6362) || (!Config.AUTO_LOOT_ADENA && (itemId == 57 || itemId == 5575 || itemId >= 6360 && itemId <= 6362))))
 		{
 			ScheduledFuture<?> itemLootShedule;
 			long delay = 0;
