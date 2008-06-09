92114936b1950066818aecb771eed2f140e186c4
Noctarius <noctaria@gmail.com>
2008-06-09 18:34:20 +0000

    git-svn-id: http://svn.l2jfree.com/l2jfree@4094 6ff00ba9-25cd-4355-987e-099c62bc2628

---
+++ tags/Core Version 1.1.0A/src/main/java/com/l2jfree/gameserver/model/zone/L2DefenderSpawnZone.java
@@ -0,0 +1,38 @@
+/*
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.l2jfree.gameserver.model.zone;
+
+import com.l2jfree.gameserver.instancemanager.CastleManager;
+import com.l2jfree.gameserver.model.L2Character;
+
+public class L2DefenderSpawnZone extends EntityZone
+{
+	@Override
+	protected void register()
+	{
+		_entity = CastleManager.getInstance().getCastleById(_castleId);
+		if (_entity != null)
+			_entity.registerDefenderSpawn(this);
+		else
+			_log.warn("Invalid castleId: "+_castleId);
+	}
+
+	// They just define a respawn area
+	@Override
+	protected void onEnter(L2Character character){}
+	
+	@Override
+	protected void onExit(L2Character character){}
+}
