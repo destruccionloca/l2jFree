d86869330d1affad61cc3acc7d1b353709fa08ad
Michiru <nonexistent@l2jfree.com>
2008-06-16 05:27:37 +0000

    TRUNK DATAPACK: fix spawnlist thx drunkendave
    git-svn-id: http://svn.l2jfree.com/l2jfree@4133 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/l2jfree-datapack/sql/spawnlist.sql
+++ trunk/l2jfree-datapack/sql/spawnlist.sql
@@ -41516,10 +41516,10 @@ INSERT INTO `spawnlist` (`location`, `count`, `npc_templateid`, `locx`, `locy`, 
 ('', 1, 35966, 71436, -58182, -2904, 0, 0, 64664, 10, 0, 0),
 ('', 1, 35828, 159377, 52403, -3312, 0, 0, 64664, 10, 0, 0);
 
-INSERT INTO `spawnlist` (`location`, `count`, `npc_templateid`, `locx`, `locy`, `locz`, `randomx`, `randomy`, `heading`, `respawn_delay`, `loc_id`, `periodOfDay`) VALUES
-('',1,36478,145180,30482,-2460,0,0,55063,60,0,0),
-('',1,36478,43456,-49579,-796,0,0,3743,60,0,0);
+INSERT INTO `spawnlist` VALUES
+(NULL,'',1,36478,145180,30482,-2460,0,0,55063,60,0,0),
+(NULL,'',1,36478,43456,-49579,-796,0,0,3743,60,0,0);
 
 -- Fantasy Island Teleporter
-INSERT INTO `spawnlist` (`id`, `location`, `count`, `npc_templateid`, `locx`, `locy`, `locz`, `randomx`, `randomy`, `heading`, `respawn_delay`, `loc_id`, `periodOfDay`) VALUES
-('', '', 1, 32378, -59227, -56896, -2037, 0, 0, 40959, 60, 0, 0);
+INSERT INTO `spawnlist` VALUES
+(NULL,'',1,32378,-59227,-56896,-2037,0,0,40959,60,0,0);
