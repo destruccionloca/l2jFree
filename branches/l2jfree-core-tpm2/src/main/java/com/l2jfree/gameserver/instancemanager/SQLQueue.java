8191010ef9784ab5c54f9a9db1340a1bb8964685
nbali <nbalika@gmail.com>
2008-06-20 07:52:57 +0000

    Copied remotely
    git-svn-id: http://svn.l2jfree.com/l2jfree@4143 6ff00ba9-25cd-4355-987e-099c62bc2628

---
+++ branches/l2jfree-core-tpm2/src/main/java/com/l2jfree/gameserver/instancemanager/SQLQueue.java
@@ -0,0 +1,130 @@
+/*
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.l2jfree.gameserver.instancemanager;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.l2jfree.L2DatabaseFactory;
+import com.l2jfree.gameserver.ThreadPoolManager;
+
+/**
+ * This class manage queued sql's like INSERT/UPDATE/DELETE
+ * 
+ * @version $Revision: $ $Date: $
+ * @author  DiezelMax
+ */
+public class SQLQueue
+{
+	protected static Log	_log	= LogFactory.getLog(SQLQueue.class.getName());
+
+	private static SQLQueue	_instance;
+	protected List<String>	_queue1	= null;
+	protected List<String>	_queue2	= null;
+
+	private SQLQueue()
+	{
+		_queue1 = new ArrayList<String>();
+		_queue2 = new ArrayList<String>();
+		ThreadPoolManager.getInstance().scheduleGeneralAtFixedRate(new doQueue(), 60000, 60000);
+	}
+
+	public static final SQLQueue getInstance()
+	{
+		if (_instance == null)
+		{
+			_instance = new SQLQueue();
+		}
+		return _instance;
+	}
+
+	public void add(String sql)
+	{
+		synchronized (_queue1)
+		{
+			_queue1.add(sql);
+		}
+	}
+
+	protected class doQueue extends Thread
+	{
+		@Override
+		public void run()
+		{
+			flush();
+		}
+	}
+
+	public void flush()
+	{
+		synchronized (_queue2)
+		{
+			synchronized (_queue1)
+			{
+				if (!_queue1.isEmpty())
+				{
+					_queue2.addAll(_queue1);
+					_queue1.clear();
+				}
+			}
+
+			if (_queue2.isEmpty())
+				return;
+			Connection con = null;
+			PreparedStatement statement = null;
+			try
+			{
+				con = L2DatabaseFactory.getInstance().getConnection(con);
+				for (String sql : _queue2)
+				{
+					try
+					{
+						statement = con.prepareStatement(sql);
+						statement.executeUpdate();
+						statement.close();
+						sql = null;
+					}
+					catch (Exception e)
+					{
+						_log.fatal("error while porcessing sql queue " + e);
+					}
+				}
+				_queue2.clear();
+			}
+			catch (Exception e)
+			{
+				_log.fatal("error while porcessing sql queue " + e);
+			}
+			finally
+			{
+				try
+				{
+					con.close();
+				}
+				catch (Exception e)
+				{
+				}
+			}
+			if (_log.isDebugEnabled())
+				_log.warn("SQLQueue: " + _queue2.size() + " sql's processed");
+		}
+	}
+}
---
+++ branches/l2jfree-core-tpm2/src/main/java/com/l2jfree/gameserver/model/ForceBuff.java
@@ -0,0 +1,94 @@
+/*
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.l2jfree.gameserver.model;
+
+import java.util.concurrent.Future;
+
+import com.l2jfree.gameserver.GeoData;
+import com.l2jfree.gameserver.ThreadPoolManager;
+import com.l2jfree.gameserver.datatables.SkillTable;
+import com.l2jfree.gameserver.model.actor.instance.L2PcInstance;
+import com.l2jfree.gameserver.skills.effects.EffectForce;
+import com.l2jfree.gameserver.util.Util;
+
+
+/**
+ * @author kombat
+ *
+ */
+public final class ForceBuff
+{
+    final int _skillCastRange;
+    final int _forceId;
+    L2PcInstance _caster;
+    L2PcInstance _target;
+    Future<?> _geoCheckTask;
+
+    public L2PcInstance getCaster()
+    {
+        return _caster;
+    }
+
+    public L2PcInstance getTarget()
+    {
+        return _target;
+    }
+
+    public ForceBuff(L2PcInstance caster, L2PcInstance target, L2Skill skill)
+    {
+        _skillCastRange = skill.getCastRange();
+        _caster = caster;
+        _target = target;
+        _forceId = skill.getTriggeredId();
+
+        L2Effect effect = _target.getFirstEffect(_forceId);
+        if (effect != null)
+            ((EffectForce)effect).increaseForce();
+        else
+            SkillTable.getInstance().getInfo(_forceId, 1).getEffects(_caster, _target);
+        
+        _geoCheckTask = ThreadPoolManager.getInstance().scheduleGeneralAtFixedRate(new GeoCheckTask(), 1000, 1000);
+    }
+
+    public void delete()
+    {
+        _caster.setForceBuff(null);
+        L2Effect effect = _target.getFirstEffect(_forceId);
+        if (effect != null)
+            ((EffectForce)effect).decreaseForce();
+
+        _geoCheckTask.cancel(true);
+    }
+
+    private class GeoCheckTask implements Runnable
+    {
+        public void run()
+        {
+            try
+            {
+                if (!Util.checkIfInRange(_skillCastRange, _caster, _target, true))
+                    delete();
+
+                if (!GeoData.getInstance().canSeeTarget(_caster, _target))
+                    delete();
+            }
+            catch (Exception e)
+            {
+                // ignore
+            }
+        }
+    }
+}
