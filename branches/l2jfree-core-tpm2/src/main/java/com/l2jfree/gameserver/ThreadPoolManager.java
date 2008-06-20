0c48c3f871ad23f940497a5e13781b03bfc1d36e
nbali <nbalika@gmail.com>
2008-06-20 09:02:01 +0000

    git-svn-id: http://svn.l2jfree.com/l2jfree@4146 6ff00ba9-25cd-4355-987e-099c62bc2628

--- branches/l2jfree-core-tpm2/src/main/java/com/l2jfree/gameserver/ThreadPoolManager.java
+++ branches/l2jfree-core-tpm2/src/main/java/com/l2jfree/gameserver/ThreadPoolManager.java
@@ -33,59 +33,9 @@ import com.l2jfree.gameserver.threadmanager.L2ThreadFactory;
 import com.l2jfree.gameserver.threadmanager.RunnableStatsManager;
 
 /**
- * <p>
- * This class is made to handle all the ThreadPools used in L2j.
- * </p>
- * <p>
- * Scheduled Tasks can either be sent to a
- * {@link #_generalScheduledThreadPool "general"} or
- * {@link #_effectsScheduledThreadPool "effects"}
- * {@link ScheduledThreadPoolExecutor ScheduledThreadPool}: The "effects" one
- * is used for every effects (skills, hp/mp regen ...) while the "general" one
- * is used for everything else that needs to be scheduled.<br>
- * There also is an {@link #_aiScheduledThreadPool "ai"}
- * {@link ScheduledThreadPoolExecutor ScheduledThreadPool} used for AI Tasks.
- * </p>
- * <p>
- * Tasks can be sent to {@link ScheduledThreadPoolExecutor ScheduledThreadPool}
- * either with:
- * <ul>
- * <li>{@link #scheduleEffect(Runnable, long)} : for effects Tasks that needs
- * to be executed only once.</li>
- * <li>{@link #scheduleGeneral(Runnable, long)} : for scheduled Tasks that
- * needs to be executed once.</li>
- * <li>{@link #scheduleAi(Runnable, long)} : for AI Tasks that needs to be
- * executed once</li>
- * </ul>
- * or
- * <ul>
- * <li>{@link #scheduleEffectAtFixedRate(Runnable, long, long)(Runnable, long)} :
- * for effects Tasks that needs to be executed periodicaly.</li>
- * <li>{@link #scheduleGeneralAtFixedRate(Runnable, long, long)(Runnable, long)} :
- * for scheduled Tasks that needs to be executed periodicaly.</li>
- * <li>{@link #scheduleAiAtFixedRate(Runnable, long, long)(Runnable, long)} :
- * for AI Tasks that needs to be executed periodicaly</li>
- * </ul>
- * </p>
- * <p>
- * For all Tasks that should be executed with no delay asynchronously in a
- * ThreadPool there also are usual {@link ThreadPoolExecutor ThreadPools} that
- * can grow/shrink according to their load.:
- * <ul>
- * <li>{@link #_generalPacketsThreadPool GeneralPackets} where most packets
- * handler are executed.</li>
- * <li>{@link #_ioPacketsThreadPool I/O Packets} where all the i/o packets are
- * executed.</li>
- * <li>There will be an AI ThreadPool where AI events should be executed</li>
- * <li>A general ThreadPool where everything else that needs to run
- * asynchronously with no delay should be executed ({@link com.l2jfree.gameserver.model.actor.knownlist KnownList}
- * updates, SQL updates/inserts...)?</li>
- * </ul>
- * </p>
- * 
- * @author -Wooden-
+ * @author -Wooden-, NB4L1
  */
-public class ThreadPoolManager
+public final class ThreadPoolManager
 {
 	private final static Log _log = LogFactory.getLog(ThreadPoolManager.class);
 	
@@ -99,10 +49,24 @@ public class ThreadPoolManager
 		return _instance;
 	}
 	
+	/**
+	 * Current implementation of {@link ScheduledThreadPoolExecutor} uses
+	 * constant number of threads, so it have to be pretty big, to be sure it's
+	 * not lagging/stucked caused by various cases.
+	 */
 	private final ScheduledThreadPoolExecutor _scheduler =
 		new ScheduledThreadPoolExecutor(50, new L2ThreadFactory("Scheduler", Thread.NORM_PRIORITY),
 			new L2RejectedExecutionHandler());
 	
+	/**
+	 * If the inner queue of {@link ThreadPoolExecutor} is full, it creates a
+	 * new a {@link Thread}. Fine tuning has to be made for every single server
+	 * (depends on the number of sql queries, core number, online players etc).
+	 * (Previous implementatin can be imagined like an {@link Integer#MAX_VALUE}
+	 * big queue.
+	 * 
+	 * @link http://java.sun.com/javase/6/docs/api/java/util/concurrent/ThreadPoolExecutor.html
+	 */
 	private final ThreadPoolExecutor _executor =
 		new ThreadPoolExecutor(10, Integer.MAX_VALUE, 10L, TimeUnit.SECONDS,
 			new ArrayBlockingQueue<Runnable>(100), new L2ThreadFactory("Executor",
