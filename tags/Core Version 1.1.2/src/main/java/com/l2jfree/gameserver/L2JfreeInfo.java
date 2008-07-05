e506b86ae7d8cd24698c16d4f9a047e801894faa
Noctarius <noctaria@gmail.com>
2008-07-05 21:08:52 +0000

    Tagged Core Version 1.1.2
    git-svn-id: http://svn.l2jfree.com/l2jfree@4243 6ff00ba9-25cd-4355-987e-099c62bc2628

---
+++ tags/Core Version 1.1.2/src/main/java/com/l2jfree/gameserver/L2JfreeInfo.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.l2jfree.gameserver;
+
+/**
+ * @author noctarius
+ */
+public class L2JfreeInfo {
+	public static final void showStartupInfo() {
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println(" ___       ___");
+		System.out.println("/\\_ \\    /'___`\\   __  /'___\\");
+		System.out.println("\\//\\ \\  /\\_\\ /\\ \\ /\\_\\/\\ \\__/  _ __    __     __");
+		System.out.println("  \\ \\ \\ \\/_/// /__\\/\\ \\ \\ ,__\\/\\`'__\\/'__`\\ /'__`\\");
+		System.out.println("   \\_\\ \\_  // /_\\ \\\\ \\ \\ \\ \\_/\\ \\ \\//\\  __//\\  __/");
+		System.out.println("   /\\____\\/\\______/_\\ \\ \\ \\_\\  \\ \\_\\\\ \\____\\ \\____\\");
+		System.out.println("   \\/____/\\/_____//\\ \\_\\ \\/_/   \\/_/ \\/____/\\/____/");
+		System.out.println("                  \\ \\____/");
+		System.out.println("                   \\/___/  starting Version: "+GameServer.getVersionNumber());
+	}
+}
