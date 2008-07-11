844c90e2477f1e9e075624878270383a9404cd43
SarutrA <nonexistent@l2jfree.com>
2008-07-11 15:56:35 +0000

    Removing Agathion Seal Bracelets from custom_armor.sql -> we have them all on armor.sql
    git-svn-id: http://svn.l2jfree.com/l2jfree@4263 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/l2jfree-datapack/sql/custom/custom_armor.sql
+++ trunk/l2jfree-datapack/sql/custom/custom_armor.sql
@@ -25,26 +25,3 @@ CREATE TABLE IF NOT EXISTS `custom_armor` (
   `sex` INT (1) NOT NULL DEFAULT -1,
   PRIMARY KEY (`item_id`)
 ) DEFAULT CHARSET=utf8;
-
-
--- Agathion Seal Bracelet - Little Angel
-
-INSERT IGNORE custom_armor (`item_id`, `name`, `bodypart`, `crystallizable`, `armor_type`, `weight`, `material`, `crystal_type`, `avoid_modify`, `duration`, `p_def`, `m_def`, `mp_bonus`, `price`, `crystal_count`, `sellable`, `dropable`, `destroyable`, `tradeable`, `skills_item`, `races`, `classes`, `sex`) VALUES
-(10316, 'Agathion Seal Bracelet - Little Angel', 'lbracelet', 'true', 'none', 150, 'wood', 'none', 0, -1, 0, 0, 0, 0, 0, 'true', 'true', 'true', 'true', '3423-1;3267-1', '-1', '-1', -1),
-(10317, 'Agathion Seal Bracelet - Little Angel', 'lbracelet', 'true', 'none', 150, 'wood', 'none', 0, -1, 0, 0, 0, 0, 0, 'true', 'true', 'true', 'true', '3423-1;3267-1', '-1', '-1', -1),
-(10318, 'Agathion Seal Bracelet - Little Angel', 'lbracelet', 'true', 'none', 150, 'wood', 'none', 0, -1, 0, 0, 0, 0, 0, 'true', 'true', 'true', 'true', '3423-1;3267-1', '-1', '-1', -1),
-(10319, 'Agathion Seal Bracelet - Little Angel', 'lbracelet', 'true', 'none', 150, 'wood', 'none', 0, -1, 0, 0, 0, 0, 0, 'true', 'true', 'true', 'true', '3423-1;3267-1', '-1', '-1', -1),
-(10320, 'Agathion Seal Bracelet - Little Angel', 'lbracelet', 'true', 'none', 150, 'wood', 'none', 0, -1, 0, 0, 0, 0, 0, 'true', 'true', 'true', 'true', '3423-1;3267-1', '-1', '-1', -1);
-
--- Agathion Seal Bracelet - Little Devil
-
-INSERT IGNORE custom_armor (`item_id`, `name`, `bodypart`, `crystallizable`, `armor_type`, `weight`, `material`, `crystal_type`, `avoid_modify`, `duration`, `p_def`, `m_def`, `mp_bonus`, `price`, `crystal_count`, `sellable`, `dropable`, `destroyable`, `tradeable`, `skills_item`, `races`, `classes`, `sex`) VALUES
-(10322, 'Agathion Seal Bracelet - Little Devil', 'lbracelet', 'true', 'none', 150, 'wood', 'none', 0, -1, 0, 0, 0, 0, 0, 'true', 'true', 'true', 'true', '3424-1;3267-1', '-1', '-1', -1),
-(10323, 'Agathion Seal Bracelet - Little Devil', 'lbracelet', 'true', 'none', 150, 'wood', 'none', 0, -1, 0, 0, 0, 0, 0, 'true', 'true', 'true', 'true', '3424-1;3267-1', '-1', '-1', -1),
-(10324, 'Agathion Seal Bracelet - Little Devil', 'lbracelet', 'true', 'none', 150, 'wood', 'none', 0, -1, 0, 0, 0, 0, 0, 'true', 'true', 'true', 'true', '3424-1;3267-1', '-1', '-1', -1),
-(10325, 'Agathion Seal Bracelet - Little Devil', 'lbracelet', 'true', 'none', 150, 'wood', 'none', 0, -1, 0, 0, 0, 0, 0, 'true', 'true', 'true', 'true', '3424-1;3267-1', '-1', '-1', -1);
-
--- Agathion Seal Bracelet - Rudolph
-
-INSERT IGNORE custom_armor (`item_id`, `name`, `bodypart`, `crystallizable`, `armor_type`, `weight`, `material`, `crystal_type`, `avoid_modify`, `duration`, `p_def`, `m_def`, `mp_bonus`, `price`, `crystal_count`, `sellable`, `dropable`, `destroyable`, `tradeable`, `skills_item`, `races`, `classes`, `sex`) VALUES
-(10607, 'Agathion Seal Bracelet - Rudolph', 'lbracelet', 'true', 'none', 150, 'wood', 'none', 0, -1, 0, 0, 0, 0, 0, 'true', 'true', 'true', 'true', '3425-1;3267-1', '-1', '-1', -1);
