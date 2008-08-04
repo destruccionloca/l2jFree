ff6769e6ed9e67e15d99e5036ab5cf732a51c468
SarutrA <nonexistent@l2jfree.com>
2008-08-04 05:02:41 +0000

    Change version on setenv
    git-svn-id: http://svn.l2jfree.com/l2jfree@4353 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/l2jfree-core/dist/setenv.sh
+++ trunk/l2jfree-core/dist/setenv.sh
@@ -27,7 +27,7 @@ CLASSPATH=${CLASSPATH}:javolution-1.5.5.2.6.jar
 
 # main jar
 CLASSPATH=${CLASSPATH}:l2j-commons-1.1.7.jar
-CLASSPATH=${CLASSPATH}:l2jfree-core-1.1.4.jar
+CLASSPATH=${CLASSPATH}:l2jfree-core-1.1.5.jar
 
 CLASSPATH=${CLASSPATH}:core-3.3.0.jar
 CLASSPATH=${CLASSPATH}:java-engine-1.0.0.jar
