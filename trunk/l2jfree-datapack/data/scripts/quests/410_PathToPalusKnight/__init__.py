d8af1116e911add5b36375592a08add4b9334213
crion <medinos@gmx.de>
2008-08-19 21:47:26 +0000

    Forgot some imports
    git-svn-id: http://svn.l2jfree.com/l2jfree@4470 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/l2jfree-datapack/data/scripts/quests/410_PathToPalusKnight/__init__.py
+++ trunk/l2jfree-datapack/data/scripts/quests/410_PathToPalusKnight/__init__.py
@@ -6,6 +6,7 @@ from com.l2jfree import Config
 from com.l2jfree.gameserver.model.quest import State
 from com.l2jfree.gameserver.model.quest import QuestState
 from com.l2jfree.gameserver.model.quest.jython import QuestJython as JQuest
+from com.l2jfree.gameserver.network.serverpackets import SocialAction
 
 qn = "410_PathToPalusKnight"
 
