956989f6f84bba954797ceab30792022380b1f88
Respawner <respawneral@gmail.com>
2008-09-17 13:02:34 +0000

    Trunk datapack : Sync to L2JDP 	* 5184 - HTML,SQL: Support for L2J 2451 and big thanks to Gigii-kun for this work (need L2J 2451 sync) 	* 5185 - XML: Skill 1524: "Surrender to the Holy", thanks caramon81. 	* 5186 - SQL: Support for L2J 2457 by Aikimaniac (need L2J 2457 sync) 	* 5187 - Fix Black Leopard wrong stats by Kerberos 	* 5188 - Fix non talking NPC 31606 by Kerberos
    git-svn-id: http://svn.l2jfree.com/l2jfree@4570 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/l2jfree-datapack/sql/castle.sql
+++ trunk/l2jfree-datapack/sql/castle.sql
@@ -7,19 +7,19 @@ CREATE TABLE IF NOT EXISTS `castle` (
   `taxPercent` INT NOT NULL DEFAULT 15,
   `treasury` INT NOT NULL DEFAULT 0,
   `siegeDate` DECIMAL(20,0) NOT NULL DEFAULT 0,
-  `siegeDayOfWeek` INT NOT NULL DEFAULT 7,
-  `siegeHourOfDay` INT NOT NULL DEFAULT 20,
+  `regTimeOver` enum('true','false') DEFAULT 'true' NOT NULL,
+  `regTimeEnd` DECIMAL(20,0) NOT NULL default 0,
   PRIMARY KEY (`name`),
   KEY `id` (`id`)
 ) DEFAULT CHARSET=utf8;
 
 INSERT IGNORE INTO `castle` VALUES 
-(1,'Gludio',0,0,0,7,20),
-(2,'Dion',0,0,0,7,20),
-(3,'Giran',0,0,0,1,16),
-(4,'Oren',0,0,0,1,16),
-(5,'Aden',0,0,0,7,20),
-(6,'Innadril',0,0,0,1,16),
-(7,'Goddard',0,0,0,1,16),
-(8,'Rune',0,0,0,7,20),
-(9,'Schuttgart',0,0,0,7,20);
+(1,'Gludio',0,0,0,'true',0),
+(2,'Dion',0,0,0,'true',0),
+(3,'Giran',0,0,0,'true',0),
+(4,'Oren',0,0,0,'true',0),
+(5,'Aden',0,0,0,'true',0),
+(6,'Innadril',0,0,0,'true',0),
+(7,'Goddard',0,0,0,'true',0),
+(8,'Rune',0,0,0,'true',0),
+(9,'Schuttgart',0,0,0,'true',0);
