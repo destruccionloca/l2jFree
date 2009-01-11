32da158dfaa3213c153883c7a9d7ce5e4e522d3d
evill33t <ronny.stiftel@gmail.com>
2009-01-11 23:10:32 +0000

    l2jfree stable 1.2.9
    git-svn-id: http://svn.l2jfree.com/l2jfree@5007 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/l2jfree-core/dist/setenv.sh
+++ trunk/l2jfree-core/dist/setenv.sh
@@ -26,7 +26,7 @@ CLASSPATH=${CLASSPATH}:javolution-1.5.5.2.6.jar
 # main jar
 CLASSPATH=${CLASSPATH}:l2j-commons-1.1.8.jar
 CLASSPATH=${CLASSPATH}:l2jfree-geoserver-1.2.0.jar
-CLASSPATH=${CLASSPATH}:l2jfree-core-1.2.8.jar
+CLASSPATH=${CLASSPATH}:l2jfree-core-1.2.9.jar
 
 CLASSPATH=${CLASSPATH}:core-3.3.0.jar
 CLASSPATH=${CLASSPATH}:java-engine-1.0.2.jar
