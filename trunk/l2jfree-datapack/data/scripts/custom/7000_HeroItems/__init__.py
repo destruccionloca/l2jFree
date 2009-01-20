4a50248ac3152798a843ddc5f502ecabad87be47
Lizard <nonexistent@l2jfree.com>
2009-01-20 22:38:45 +0000

    /Trunk/Datapack
    7000_HeroItems.... 
    - Added usefull stuff to header and cleaned it a lot
    
    git-svn-id: http://svn.l2jfree.com/l2jfree@5064 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/l2jfree-datapack/data/scripts/custom/7000_HeroItems/__init__.py
+++ trunk/l2jfree-datapack/data/scripts/custom/7000_HeroItems/__init__.py
@@ -5,24 +5,61 @@ from com.l2jfree.gameserver.model.quest import QuestState
 from com.l2jfree.gameserver.model.quest.jython import QuestJython as JQuest
 from com.l2jfree.gameserver.datatables import ItemTable
 qn = "7000_HeroItems"
-MONUMENTS=[31690]+range(31769,31773)
+
+# NPCs : Monuments to heroes
+MON_HUMAN   = 31690
+MON_ELF     = 31769
+MON_DARKELF = 31770
+MON_ORC     = 31771
+MON_DWARF   = 31772
+
+MONUMENTS = [MON_HUMAN, MON_ELF, MON_DARKELF, MON_ORC, MON_DWARF]
+
+
+# HERO ITEMS
+INFINITY_BLADE    = 6611
+INFINITY_CLEAVER  = 6612
+INFINITY_AXE      = 6613
+INFINITY_ROD      = 6614
+INFINITY_CRUSHER  = 6615
+INFINITY_SCEPTER  = 6616
+INFINITY_STINGER  = 6617
+INFINITY_FANG     = 6618
+INFINITY_BOW      = 6619
+INFINITY_WING     = 6620
+INFINITY_SPEAR    = 6621
+INFINITY_RAPIER   = 9388
+INFINITY_SWORD    = 9389
+INFINITY_SHOOTER  = 9390
+
+WINGS_OF_DESTINY  = 6842
+
+HERO_WEAPONS = [
+				INFINITY_BLADE,		INFINITY_CLEAVER,	INFINITY_AXE
+				INFINITY_ROD,		INFINITY_CRUSHER,	INFINITY_SCEPTER,
+				INFINITY_STINGER,	INFINITY_FANG,		INFINITY_BOW,
+				INFINITY_WING,		INFINITY_SPEAR,		INFINITY_RAPIER,
+				INFINITY_SWORD,		INFINITY_SHOOTER
+				]
+
+
 
 HERO_ITEMS={
-6611:["weapon_the_sword_of_hero_i00","Infinity Blade","During a critical attack, decreases one's P. Def and increases de-buff casting ability, damage shield effect, Max HP, Max MP, Max CP, and shield defense power. Also enhances damage to target during PvP.","379/169","Sword"],
-6612:["weapon_the_two_handed_sword_of_hero_i00","Infinity Cleaver","Increases Max HP, Max CP, critical power and critical chance. Inflicts extra damage when a critical attack occurs and has possibility of reflecting the skill back on the player. Also enhances damage to target during PvP.","461/169","Double Handed Sword"],
-6613:["weapon_the_axe_of_hero_i00","Infinity Axe","During a critical attack, it bestows one the ability to cause internal conflict to one's opponent. Damage shield function, Max HP, Max MP, Max CP as well as one's shield defense rate are increased. It also enhances damage to one's opponent during PvP.","379/169","Blunt"],
-6614:["weapon_the_mace_of_hero_i00","Infinity Rod","When good magic is casted upon a target, increases MaxMP, MaxCP, Casting Spd, and MP regeneration rate. Also recovers HP 100% and enhances damage to target during PvP.","303/226","Blunt"],
-6615:["weapon_the_hammer_of_hero_i00","Infinity Crusher","Increases MaxHP, MaxCP, and Atk. Spd. Stuns a target when a critical attack occurs and has possibility of reflecting the skill back on the player. Also enhances damage to target during PvP.","461/169","Blunt"],
-6616:["weapon_the_staff_of_hero_i00","Infinity Scepter","When casting good magic, it can recover HP by 100% at a certain rate, increases MAX MP, MaxCP, M. Atk., lower MP Consumption, increases the Magic Critical rate, and reduce the Magic Cancel. Enhances damage to target during PvP.","369/226","Blunt"],
-6617:["weapon_the_dagger_of_hero_i00","Infinity Stinger","Increases MaxMP, MaxCP, Atk. Spd., MP regen rate, and the success rate of Mortal and Deadly Blow from the back of the target. Silences the target when a critical attack occurs and has Vampiric Rage effect. Also enhances damage to target during PvP.","332/169","Dagger"],
-6618:["weapon_the_fist_of_hero_i00","Infinity Fang","Increases MaxHP, MaxMP, MaxCP and evasion. Stuns a target when a critical attack occurs and has possibility of reflecting the skill back on the player at a certain probability rate. Also enhances damage to target during PvP.","461/169","Dual Fist"],
-6619:["weapon_the_bow_of_hero_i00","Infinity Bow","Increases MaxMP/MaxCP and decreases re-use delay of a bow. Slows target when a critical attack occurs and has Cheap Shot effect. Also enhances damage to target during PvP.","707/169","Bow"],
-6620:["weapon_the_dualsword_of_hero_i00","Infinity Wing","When a critical attack occurs, increases MaxHP, MaxMP, MaxCP and critical chance. Silences the target and has possibility of reflecting the skill back on the target. Also enhances damage to target during PvP.","461/169","Dual Sword"],
-6621:["weapon_the_pole_of_hero_i00","Infinity Spear","During a critical attack, increases MaxHP, Max CP, Atk. Spd. and Accuracy. Casts dispel on a target and has possibility of reflecting the skill back on the target. Also enhances damage to target during PvP.","379/169","Pole"],
-9388:["weapon_infinity_rapier_i00","Infinity Rapier","Decreases the target's P. Def and increases the de-buff casting ability, the damage shield ability, and the Max HP/Max MP/Max CP on a critical attack. Increases damage inflicted during PvP. A critical attack will have a chance to increase P. Atk., M. Atk., and healing power, and decrease MP consumption during skill use, for you and your party members.","344/169","Rapier"],
-9389:["weapon_infinity_sword_i00","Infinity Sword","Increases critical attack success rate/power, MaxHP, MaxCP, and damage inflicted during PvP. Also inflicts extra damage on critical attacks, and reflects debuff attacks back on enemies.","410/169","Ancient Sword"],
-9390:["weapon_infinity_shooter_i00","Infinity Shooter","Produces the following effects when a critical attack occurs: the target is slowed, decrease MP consumption for skill use, and increase Max MP/Max CP. Enhances damage done to the target during PvP.","405/169","Crossbow"],
-6842:["accessory_hero_cap_i00","Wings of Destiny Circlet","Hair accessory exclusively used by heroes.","0","Hair Accessory"]
+INFINITY_BLADE    :["weapon_the_sword_of_hero_i00","Infinity Blade","During a critical attack, decreases one's P. Def and increases de-buff casting ability, damage shield effect, Max HP, Max MP, Max CP, and shield defense power. Also enhances damage to target during PvP.","379/169","Sword"],
+INFINITY_CLEAVER  :["weapon_the_two_handed_sword_of_hero_i00","Infinity Cleaver","Increases Max HP, Max CP, critical power and critical chance. Inflicts extra damage when a critical attack occurs and has possibility of reflecting the skill back on the player. Also enhances damage to target during PvP.","461/169","Double Handed Sword"],
+INFINITY_AXE      :["weapon_the_axe_of_hero_i00","Infinity Axe","During a critical attack, it bestows one the ability to cause internal conflict to one's opponent. Damage shield function, Max HP, Max MP, Max CP as well as one's shield defense rate are increased. It also enhances damage to one's opponent during PvP.","379/169","Blunt"],
+INFINITY_ROD      :["weapon_the_mace_of_hero_i00","Infinity Rod","When good magic is casted upon a target, increases MaxMP, MaxCP, Casting Spd, and MP regeneration rate. Also recovers HP 100% and enhances damage to target during PvP.","303/226","Blunt"],
+INFINITY_CRUSHER  :["weapon_the_hammer_of_hero_i00","Infinity Crusher","Increases MaxHP, MaxCP, and Atk. Spd. Stuns a target when a critical attack occurs and has possibility of reflecting the skill back on the player. Also enhances damage to target during PvP.","461/169","Blunt"],
+INFINITY_SCEPTER  :["weapon_the_staff_of_hero_i00","Infinity Scepter","When casting good magic, it can recover HP by 100% at a certain rate, increases MAX MP, MaxCP, M. Atk., lower MP Consumption, increases the Magic Critical rate, and reduce the Magic Cancel. Enhances damage to target during PvP.","369/226","Blunt"],
+INFINITY_STINGER  :["weapon_the_dagger_of_hero_i00","Infinity Stinger","Increases MaxMP, MaxCP, Atk. Spd., MP regen rate, and the success rate of Mortal and Deadly Blow from the back of the target. Silences the target when a critical attack occurs and has Vampiric Rage effect. Also enhances damage to target during PvP.","332/169","Dagger"],
+INFINITY_FANG     :["weapon_the_fist_of_hero_i00","Infinity Fang","Increases MaxHP, MaxMP, MaxCP and evasion. Stuns a target when a critical attack occurs and has possibility of reflecting the skill back on the player at a certain probability rate. Also enhances damage to target during PvP.","461/169","Dual Fist"],
+INFINITY_BOW      :["weapon_the_bow_of_hero_i00","Infinity Bow","Increases MaxMP/MaxCP and decreases re-use delay of a bow. Slows target when a critical attack occurs and has Cheap Shot effect. Also enhances damage to target during PvP.","707/169","Bow"],
+INFINITY_WING     :["weapon_the_dualsword_of_hero_i00","Infinity Wing","When a critical attack occurs, increases MaxHP, MaxMP, MaxCP and critical chance. Silences the target and has possibility of reflecting the skill back on the target. Also enhances damage to target during PvP.","461/169","Dual Sword"],
+INFINITY_SPEAR    :["weapon_the_pole_of_hero_i00","Infinity Spear","During a critical attack, increases MaxHP, Max CP, Atk. Spd. and Accuracy. Casts dispel on a target and has possibility of reflecting the skill back on the target. Also enhances damage to target during PvP.","379/169","Pole"],
+INFINITY_RAPIER   :["weapon_infinity_rapier_i00","Infinity Rapier","Decreases the target's P. Def and increases the de-buff casting ability, the damage shield ability, and the Max HP/Max MP/Max CP on a critical attack. Increases damage inflicted during PvP. A critical attack will have a chance to increase P. Atk., M. Atk., and healing power, and decrease MP consumption during skill use, for you and your party members.","344/169","Rapier"],
+INFINITY_SWORD    :["weapon_infinity_sword_i00","Infinity Sword","Increases critical attack success rate/power, MaxHP, MaxCP, and damage inflicted during PvP. Also inflicts extra damage on critical attacks, and reflects debuff attacks back on enemies.","410/169","Ancient Sword"],
+INFINITY_SHOOTER  :["weapon_infinity_shooter_i00","Infinity Shooter","Produces the following effects when a critical attack occurs: the target is slowed, decrease MP consumption for skill use, and increase Max MP/Max CP. Enhances damage done to the target during PvP.","405/169","Crossbow"],
+WINGS_OF_DESTINY  :["accessory_hero_cap_i00","Wings of Destiny Circlet","Hair accessory exclusively used by heroes.","0","Hair Accessory"]
 }
 
 def render_list(mode,item) :
@@ -55,14 +92,14 @@ class Quest (JQuest) :
           htmltext=render_list("item",int(event))
        elif event.startswith("_") :
           item = int(event.split("_")[1])
-          if item == 6842:
-            if st.getQuestItemsCount(6842):
+          if item == WINGS_OF_DESTINY:
+            if st.getQuestItemsCount(WINGS_OF_DESTINY):
                htmltext = "You can't have more than one circlet and a weapon"
             else :
                st.giveItems(item,1)
                htmltext = "Enjoy your Wings of Destiny Circlet"
           else :
-             for i in range(6611,6622)+range(9388,9391):
+             for i in HERO_WEAPONS:
                 if st.getQuestItemsCount(i):
                    st.exitQuest(1)
                    return "You already have an "+HERO_ITEMS[i][1]
@@ -82,7 +119,6 @@ class Quest (JQuest) :
      return htmltext
 
 QUEST       = Quest(7000,qn,"Hero Items")
-
 
 
 for i in MONUMENTS:
