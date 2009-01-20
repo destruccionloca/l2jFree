fddddc4d64d1740025d9a7b2e1e60e68ef58b7b0
Lizard <nonexistent@l2jfree.com>
2009-01-20 23:31:51 +0000

    /Trunk/Datapack
    Last update on 5011_l2day but not least...
    - Improved readability in the whole script, and overall intelligibility aswell...
    
    git-svn-id: http://svn.l2jfree.com/l2jfree@5065 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/l2jfree-datapack/data/scripts/custom/5011_l2day/__init__.py
+++ trunk/l2jfree-datapack/data/scripts/custom/5011_l2day/__init__.py
@@ -1,6 +1,7 @@
 # ----------------------------------------------------------------------------------
 #	        Created by DJ_WEST. (C) Reallite Labs (www.reallite.cs2.ru)  1.11.2005
 #	        Official idea - NCSoft. Version: 1.0
+	        Beautified by Lizard
 # ----------------------------------------------------------------------------------
 
 # Settings
@@ -40,12 +41,17 @@ S  = 3886
 T  = 3887
 II = 3888
 
-# Items - Format [name, giveItemId, giveItemQty, giveItem1Id, giveItem1Qty, giveItem2Id, giveItem2Qty, giveItem3Id, giveItem3Qty, giveItem4Id, giveItem4Qty, takeItem1Id, takeItem1Qty, takeItem2Id, takeItem2Qty, takeItem3Id, takeItem3Qty, takeItem4Id, takeItem4Qty, takeItem5Id, takeItem5Qty, takeItem6Id, takeItem6Qty, takeItem7Id, takeItem7Qty, takeItem8Id, takeItem8Qty, takeItem9Id, takeItem9Qty]
+# Format [name, item1, qty1, item2, qty2, item3, qty3, item4, qty4, item5, qty5,
+#		 Letter1, Lqty1, Letter2, Lqty2, Letter3, Lqty3, Letter4, Lqty4, Letter5, Lqty5,
+#		 Letter6, Lqty6, Letter7, Lqty7, Letter8, Lqty8, Letter9, Lqty9]
+
 Items       = [
-["LINEAGEII", BRES, 3, BSOE, 3, GREATER_ACUMEN, 3, EMPOWER, 3, L, 1, I, 1, N, 1, E, 2, A, 1, G, 1, E, 1, II, 1, C, 0],
-["NCSOFT", BSOE, 1, BRES, 1, GUIDANCE, 1, DEATH_WHISPER, 1, N, 1, C, 1, S, 1, O, 1, F, 1, T, 1, A, 0, E, 0, G, 0],
-["CHRONICLE", WWALK, 2, BRES, 2, BSOE, 2, FOCUS, 2, C, 2, H, 1, R, 1, O, 1, N, 1, I, 1, C, 1, L, 1, E, 1]
+["LINEAGEII",	BRES,	3,	BSOE,	3,	GREATER_ACUMEN, 3,	EMPOWER,		3,		L,	1,	I,	1,	N,	1,	E,	2, A,	1,	G,	1,	E, 1,	II, 1,	C,	0],
+["NCSOFT",		BSOE,	1,	BRES,	1,	GUIDANCE,		1,	DEATH_WHISPER,	1,		N,	1,	C,	1,	S,	1,	O,	1, F,	1,	T,	1,	A, 0,	E,	0,	G,	0],
+["CHRONICLE",	WWALK,	2,	BRES,	2,	BSOE,			2,	FOCUS,			2,		C,	2,	H,	1,	R,	1,	O,	1, N,	1,	I,	1,	C,	1,	L,	1,	E,	1]
 ]
+
+
 
 import sys
 from com.l2jfree.gameserver.model.quest import State
@@ -53,21 +59,25 @@ from com.l2jfree.gameserver.model.quest import QuestState
 from com.l2jfree.gameserver.model.quest.jython import QuestJython as JQuest
 
 # doRequestedEvent
-def do_RequestedEvent(event, st, giveItem1Id, giveItem1Qty, giveItem2Id, giveItem2Qty, giveItem3Id, giveItem3Qty, giveItem4Id, giveItem4Qty, takeItem1Id, takeItem1Qty, takeItem2Id, takeItem2Qty, takeItem3Id, takeItem3Qty, takeItem4Id, takeItem4Qty, takeItem5Id, takeItem5Qty, takeItem6Id, takeItem6Qty, takeItem7Id, takeItem7Qty, takeItem8Id, takeItem8Qty, takeItem9Id, takeItem9Qty) :
-    if st.getQuestItemsCount(takeItem1Id) >= takeItem1Qty and st.getQuestItemsCount(takeItem2Id) >= takeItem2Qty and st.getQuestItemsCount(takeItem3Id) >= takeItem3Qty and st.getQuestItemsCount(takeItem4Id) >= takeItem4Qty and st.getQuestItemsCount(takeItem5Id) >= takeItem5Qty and st.getQuestItemsCount(takeItem6Id) >= takeItem6Qty and st.getQuestItemsCount(takeItem7Id) >= takeItem7Qty and st.getQuestItemsCount(takeItem8Id) >= takeItem8Qty and st.getQuestItemsCount(takeItem9Id) >= takeItem9Qty :
-        st.takeItems(takeItem1Id, takeItem1Qty)
-        st.takeItems(takeItem2Id, takeItem2Qty)
-        st.takeItems(takeItem3Id, takeItem3Qty)
-        st.takeItems(takeItem4Id, takeItem4Qty)
-        st.takeItems(takeItem5Id, takeItem5Qty)
-        st.takeItems(takeItem6Id, takeItem6Qty)
-        st.takeItems(takeItem7Id, takeItem7Qty)
-        st.takeItems(takeItem8Id, takeItem8Qty)
-        st.takeItems(takeItem9Id, takeItem9Qty)
-        st.giveItems(giveItem1Id, giveItem1Qty)
-        st.giveItems(giveItem2Id, giveItem2Qty)
-        st.giveItems(giveItem3Id, giveItem3Qty)
-        st.giveItems(giveItem4Id, giveItem4Qty)
+def do_RequestedEvent(event, st, item1, Iqty1, item2, Iqty2, item3, Iqty3, item4, Iqty4, Letter1, Lqty1, Letter2, Lqty2, Letter3, Lqty3, Letter4, Lqty4, Letter5, Lqty5, Letter6, Lqty6, Letter7, Lqty7, Letter8, Lqty8, Letter9, Lqty9) :
+    if  st.getQuestItemsCount(letter1) >= Lqty1 and st.getQuestItemsCount(letter2) >= Lqty2 and st.getQuestItemsCount(Letter3) >= Lqty3 \
+	and st.getQuestItemsCount(Letter4) >= Lqty4 and st.getQuestItemsCount(Letter5) >= Lqty5 and st.getQuestItemsCount(Letter6) >= Lqty6 \
+	and st.getQuestItemsCount(Letter7) >= Lqty7 and st.getQuestItemsCount(Letter8) >= Lqty8 and st.getQuestItemsCount(Letter9) >= Lqt9 :
+	
+        st.takeItems(Letter1, 	Lqty1)
+        st.takeItems(Letter2, 	Lqty2)
+        st.takeItems(Letter3, 	Lqty3)
+        st.takeItems(Letter4, 	Lqty4)
+        st.takeItems(Letter5, 	Lqty5)
+        st.takeItems(Letter6, 	Lqty6)
+        st.takeItems(Letter7, 	Lqty7)
+        st.takeItems(Letter8, 	Lqty8)
+        st.takeItems(Letter9,	Lqty9)
+		
+        st.giveItems(item1, 	Iqty1)
+        st.giveItems(item2, 	Iqty2)
+        st.giveItems(item3, 	Iqty3)
+        st.giveItems(item4, 	Iqty4)
         return "2.htm"
     else :
         return "You do not have enough materials."
@@ -86,7 +96,13 @@ class Quest (JQuest) :
 
     for item in Items:
         if event == str(item[1]):
-            htmltext = do_RequestedEvent(event, st, item[1], item[2], item[3], item[4], item[5], item[6], item[7], item[8], item[9], item[10], item[11], item[12], item[13], item[14], item[15], item[16], item[17], item[18], item[19], item[20], item[21], item[22], item[23], item[24], item[25], item[26])
+            htmltext = do_RequestedEvent(event, st,
+										item[1], item[2], item[3], item[4], item[5],
+										item[6], item[7], item[8], item[9], item[10],
+										item[11], item[12], item[13], item[14], item[15],
+										item[16], item[17], item[18], item[19], item[20],
+										item[21], item[22], item[23], item[24], item[25],
+										item[26])
     
     if htmltext != event:
       st.exitQuest(False) 
