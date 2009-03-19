7d2e93e4690ac1f33b2105f4673cb83fe186aa56
nbali <nbalika@gmail.com>
2009-03-19 19:13:33 +0000

    Autoban for walker users and some cleanup
    git-svn-id: http://svn.l2jfree.com/l2jfree@5343 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/l2jfree-core/src/main/java/com/l2jfree/gameserver/network/clientpackets/MoveBackwardToLocation.java
+++ trunk/l2jfree-core/src/main/java/com/l2jfree/gameserver/network/clientpackets/MoveBackwardToLocation.java
@@ -14,145 +14,121 @@
  */
 package com.l2jfree.gameserver.network.clientpackets;
 
-import java.nio.BufferUnderflowException;
-
 import com.l2jfree.Config;
-import com.l2jfree.gameserver.TaskPriority;
 import com.l2jfree.gameserver.ai.CtrlIntention;
 import com.l2jfree.gameserver.model.L2CharPosition;
 import com.l2jfree.gameserver.model.actor.instance.L2PcInstance;
 import com.l2jfree.gameserver.network.serverpackets.ActionFailed;
 import com.l2jfree.gameserver.network.serverpackets.PartyMemberPosition;
+import com.l2jfree.gameserver.util.IllegalPlayerAction;
+import com.l2jfree.gameserver.util.Util;
 
-
-/**
- * This class ...
- * 
- * @version $Revision: 1.11.2.4.2.4 $ $Date: 2005/03/27 15:29:30 $
- */
-public class MoveBackwardToLocation extends L2GameClientPacket
+public final class MoveBackwardToLocation extends L2GameClientPacket
 {
-    //private static Logger _log = Logger.getLogger(MoveBackwardToLocation.class.getName());
-    // cdddddd
-    private       int _targetX;
-    private       int _targetY;
-    private       int _targetZ;
-    @SuppressWarnings("unused")
-    private int _originX;
-    @SuppressWarnings("unused")
-    private int _originY;
-    @SuppressWarnings("unused")
-    private int _originZ;
-    private       int _moveMovement;
-    
-    //For geodata
-    private       int _curX;
-    private       int _curY;
-    @SuppressWarnings("unused")
-    private       int _curZ;
-    
-    public TaskPriority getPriority() { return TaskPriority.PR_HIGH; }
-
-    private static final String _C__01_MOVEBACKWARDTOLOC = "[C] 01 MoveBackwardToLoc";
-    /**
-     * packet type id 0x01
-     * @param decrypt
-     */
-    @Override
-    protected void readImpl()
-    {
-        _targetX  = readD();
-        _targetY  = readD();
-        _targetZ  = readD();
-        _originX  = readD();
-        _originY  = readD();
-        _originZ  = readD();
-		try
+	private static final String _C__01_MOVEBACKWARDTOLOC = "[C] 01 MoveBackwardToLoc";
+	
+	private int _targetX;
+	private int _targetY;
+	private int _targetZ;
+	//private int _originX;
+	//private int _originY;
+	//private int _originZ;
+	
+	private Integer _moveMovement;
+	
+	@Override
+	protected void readImpl()
+	{
+		_targetX = readD();
+		_targetY = readD();
+		_targetZ = readD();
+		/*_originX = */readD();
+		/*_originY = */readD();
+		/*_originZ = */readD();
+		
+		// walker noob
+		if (getByteBuffer().remaining() < 4)
+			return;
+		
+		_moveMovement = readD(); // is 0 if cursor keys are used  1 if mouse is used
+	}
+	
+	@Override
+	protected void runImpl()
+	{
+		L2PcInstance activeChar = getClient().getActiveChar();
+		if (activeChar == null)
+			return;
+		
+		if (_moveMovement == null)
 		{
-			_moveMovement = readD(); // is 0 if cursor keys are used  1 if mouse is used
+			Util.handleIllegalPlayerAction(activeChar, "Bot usage for movement by " + activeChar,
+				IllegalPlayerAction.PUNISH_KICKBAN);
+			return;
 		}
-		catch (BufferUnderflowException e)
+		
+		// Correcting targetZ from floor level to head level (?)
+		// Client is giving floor level as targetZ but that floor level doesn't
+		// match our current geodata and teleport coords as good as head level! 
+		// L2J uses floor, not head level as char coordinates. This is some 
+		// sort of incompatibility fix.
+		// Validate position packets sends head level.
+		_targetZ += activeChar.getTemplate().getCollisionHeight();
+		
+		int curX = activeChar.getX();
+		int curY = activeChar.getY();
+		//int curZ = activeChar.getZ();
+		
+		if (activeChar.isInBoat())
 		{
-			// ignore for now
+			activeChar.setInBoat(false);
 		}
-    }
-
-    
-    @Override
-    protected void runImpl()
-    {
-        L2PcInstance activeChar = getClient().getActiveChar();
-        if (activeChar == null)
-            return;
-
-        // Correcting targetZ from floor level to head level (?)
-        // Client is giving floor level as targetZ but that floor level doesn't
-        // match our current geodata and teleport coords as good as head level! 
-        // L2J uses floor, not head level as char coordinates. This is some 
-        // sort of incompatibility fix.
-        // Validate position packets sends head level.
-        _targetZ += activeChar.getTemplate().getCollisionHeight();
-
-        _curX = activeChar.getX();
-        _curY = activeChar.getY();
-        _curZ = activeChar.getZ();
-        
-        if(activeChar.isInBoat())
-        {
-            activeChar.setInBoat(false);
-        }
-        
-        if (activeChar.getTeleMode() > 0)
-        {
-            if (activeChar.getTeleMode() == 1)
-                activeChar.setTeleMode(0);
-            activeChar.sendPacket(ActionFailed.STATIC_PACKET);
-            activeChar.teleToLocation(_targetX, _targetY, _targetZ, false);
-            return;
-        }
-        
-        if (activeChar.isFakeDeath())
-        	return;
-
-        if (activeChar.isDead())
-        {
-        	/*Util.handleIllegalPlayerAction(activeChar, "Player " + activeChar.getName() + 
-        			" tried to move while being dead. L2Walker protection.", 
-        			Config.DEFAULT_PUNISH);*/
-        	activeChar.sendPacket(ActionFailed.STATIC_PACKET);
-        	return;
-        }
-        
-        if (_moveMovement == 0 && !Config.GEO_MOVE_PC) // cursor movement without geodata movement check is disabled
-        {
-            activeChar.sendPacket(ActionFailed.STATIC_PACKET);
-        }
-        else 
-        {
-            double dx = _targetX-_curX;
-            double dy = _targetY-_curY;
-            
-            // Can't move if character is confused, or trying to move a huge distance
-            if (activeChar.isOutOfControl() || ((dx*dx+dy*dy) > 98010000)) // 9900*9900
-            {
-                activeChar.sendPacket(ActionFailed.STATIC_PACKET);
-                return;
-            }
-            
-            activeChar.getAI().setIntention(CtrlIntention.AI_INTENTION_MOVE_TO,
-                    new L2CharPosition(_targetX, _targetY, _targetZ, 0));
-            
-            if(activeChar.getParty() != null)
-                activeChar.getParty().broadcastToPartyMembers(activeChar,new PartyMemberPosition(activeChar));
-        }       
-    }
-
-    /* (non-Javadoc)
-     * @see com.l2jfree.gameserver.clientpackets.ClientBasePacket#getType()
-     */
-    @Override
-    public String getType()
-    {
-        return _C__01_MOVEBACKWARDTOLOC;
-    }
+		
+		if (activeChar.getTeleMode() > 0)
+		{
+			if (activeChar.getTeleMode() == 1)
+				activeChar.setTeleMode(0);
+			activeChar.sendPacket(ActionFailed.STATIC_PACKET);
+			activeChar.teleToLocation(_targetX, _targetY, _targetZ, false);
+			return;
+		}
+		
+		if (activeChar.isFakeDeath())
+			return;
+		
+		if (activeChar.isDead())
+		{
+			activeChar.sendPacket(ActionFailed.STATIC_PACKET);
+			return;
+		}
+		
+		if (_moveMovement == 0 && !Config.GEO_MOVE_PC) // cursor movement without geodata movement check is disabled
+		{
+			activeChar.sendPacket(ActionFailed.STATIC_PACKET);
+		}
+		else
+		{
+			double dx = _targetX - curX;
+			double dy = _targetY - curY;
+			
+			// Can't move if character is confused, or trying to move a huge distance
+			if (activeChar.isOutOfControl() || ((dx * dx + dy * dy) > 98010000)) // 9900*9900
+			{
+				activeChar.sendPacket(ActionFailed.STATIC_PACKET);
+				return;
+			}
+			
+			activeChar.getAI().setIntention(CtrlIntention.AI_INTENTION_MOVE_TO,
+				new L2CharPosition(_targetX, _targetY, _targetZ, 0));
+			
+			if (activeChar.getParty() != null)
+				activeChar.getParty().broadcastToPartyMembers(activeChar, new PartyMemberPosition(activeChar));
+		}
+	}
+	
+	@Override
+	public String getType()
+	{
+		return _C__01_MOVEBACKWARDTOLOC;
+	}
 }
