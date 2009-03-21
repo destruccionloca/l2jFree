638769acf4c27f350025f251577fa953564e4ffd
crion <medinos@gmx.de>
2009-03-21 18:55:03 +0000

    Another mercenary fix
    git-svn-id: http://svn.l2jfree.com/l2jfree@5358 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/l2jfree-core/src/main/java/com/l2jfree/gameserver/instancemanager/MercTicketManager.java
+++ trunk/l2jfree-core/src/main/java/com/l2jfree/gameserver/instancemanager/MercTicketManager.java
@@ -1724,7 +1724,7 @@ public class MercTicketManager
 
 				// Create the ticket in the gameworld
 				L2ItemInstance dropticket = new L2ItemInstance(IdFactory.getInstance().getNextId(), itemId);
-				dropticket.setLocation(L2ItemInstance.ItemLocation.INVENTORY);
+				dropticket.setLocation(L2ItemInstance.ItemLocation.VOID);
 				dropticket.dropMe(null, x, y, z);
 				dropticket.setDropTime(0); // Avoids it from beeing removed by the auto item destroyer
 				L2World.getInstance().storeObject(dropticket); // Add to the world
