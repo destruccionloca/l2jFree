9e9057afc765d45bb7ef8c82e3ae391e07e952fa
nbali <nbalika@gmail.com>
2009-04-08 20:18:50 +0000

    FactionAggressionNotificationQueue  * fixed overlapping keys
    git-svn-id: http://svn.l2jfree.com/l2jfree@5449 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/l2jfree-core/src/main/java/com/l2jfree/gameserver/ai/FactionAggressionNotificationQueue.java
+++ trunk/l2jfree-core/src/main/java/com/l2jfree/gameserver/ai/FactionAggressionNotificationQueue.java
@@ -3,13 +3,10 @@
  */
 package com.l2jfree.gameserver.ai;
 
-import javolution.util.FastMap;
-import javolution.util.FastMap.Entry;
-
 import com.l2jfree.gameserver.model.L2Character;
 import com.l2jfree.gameserver.model.actor.instance.L2NpcInstance;
 import com.l2jfree.gameserver.threadmanager.FIFOExecutableQueue;
-import com.l2jfree.util.ObjectPool;
+import com.l2jfree.util.L2FastSet;
 
 /**
  * @author NB4L1
@@ -18,66 +15,40 @@ public final class FactionAggressionNotificationQueue extends FIFOExecutableQueu
 {
 	private static final class NotificationInfo
 	{
-		private static final ObjectPool<NotificationInfo> POOL = new ObjectPool<NotificationInfo>(1000) {
-			@Override
-			protected NotificationInfo create()
-			{
-				return new NotificationInfo();
-			}
-		};
+		private final L2NpcInstance _npc;
+		private final L2Character _target;
 		
-		private static NotificationInfo newInstance(Integer uid, L2NpcInstance npc, L2Character target)
+		private NotificationInfo(L2NpcInstance npc, L2Character target)
 		{
-			final NotificationInfo info = POOL.get();
-			
-			info._uid = uid;
-			info._npc = npc;
-			info._target = target;
-			
-			return info;
+			_npc = npc;
+			_target = target;
 		}
-		
-		private static void recycle(NotificationInfo info)
-		{
-			info._target = null;
-			info._npc = null;
-			info._uid = null;
-			
-			POOL.store(info);
-		}
-		
-		private Integer _uid;
-		private L2NpcInstance _npc;
-		private L2Character _target;
 		
 		@Override
 		public boolean equals(Object obj)
 		{
-			return obj instanceof NotificationInfo && ((NotificationInfo)obj)._uid == _uid;
+			if (!(obj instanceof NotificationInfo))
+				return false;
+			
+			final NotificationInfo ni = (NotificationInfo)obj;
+			
+			return _npc == ni._npc && _target == ni._target;
 		}
 		
 		@Override
 		public int hashCode()
 		{
-			return _uid;
-		}
-		
-		private static final int getUID(L2NpcInstance npc, L2Character target)
-		{
-			return (npc.getObjectId() << 2) + target.getObjectId();
+			return (_npc.getObjectId() << 2) + _target.getObjectId();
 		}
 	}
 	
-	private final FastMap<Integer, NotificationInfo> _map = new FastMap<Integer, NotificationInfo>();
+	private final L2FastSet<NotificationInfo> _set = new L2FastSet<NotificationInfo>();
 	
 	public void add(L2NpcInstance npc, L2Character target)
 	{
-		final Integer uid = NotificationInfo.getUID(npc, target);
-		
-		synchronized (_map)
+		synchronized (_set)
 		{
-			if (!_map.containsKey(uid))
-				_map.put(uid, NotificationInfo.newInstance(uid, npc, target));
+			_set.add(new NotificationInfo(npc, target));
 		}
 		
 		execute();
@@ -86,37 +57,27 @@ public final class FactionAggressionNotificationQueue extends FIFOExecutableQueu
 	@Override
 	protected boolean isEmpty()
 	{
-		synchronized (_map)
+		synchronized (_set)
 		{
-			return _map.isEmpty();
+			return _set.isEmpty();
 		}
 	}
 	
 	@Override
 	protected void removeAndExecuteFirst()
 	{
-		L2NpcInstance npc = null;
-		L2Character target = null;
+		NotificationInfo ni = null;
 		
-		synchronized (_map)
+		synchronized (_set)
 		{
-			Entry<Integer, NotificationInfo> first = _map.head().getNext();
-			
-			NotificationInfo info = first.getValue();
-			
-			npc = info._npc;
-			target = info._target;
-			
-			_map.remove(first.getKey());
-			
-			NotificationInfo.recycle(info);
+			ni = _set.removeFirst();
 		}
 		
-		switch (npc.getAI().getIntention())
+		switch (ni._npc.getAI().getIntention())
 		{
 			case AI_INTENTION_IDLE:
 			case AI_INTENTION_ACTIVE:
-				npc.getAI().notifyEvent(CtrlEvent.EVT_AGGRESSION, target, 1);
+				ni._npc.getAI().notifyEvent(CtrlEvent.EVT_AGGRESSION, ni._target, 1);
 		}
 	}
 }
