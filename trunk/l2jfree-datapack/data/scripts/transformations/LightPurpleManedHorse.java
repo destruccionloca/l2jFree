5f108b203240ec8871e31c4323486f2ecc48fa4c
Droppo <Droppo@gmail.com>
2009-04-15 20:16:46 +0000

    Making horse work again
    git-svn-id: http://svn.l2jfree.com/l2jfree@5524 6ff00ba9-25cd-4355-987e-099c62bc2628

---
+++ trunk/l2jfree-datapack/data/scripts/transformations/LightPurpleManedHorse.java
@@ -0,0 +1,27 @@
+﻿package transformations;
+
+import com.l2jfree.gameserver.instancemanager.TransformationManager;
+import com.l2jfree.gameserver.model.L2DefaultTransformation;
+import com.l2jfree.gameserver.model.actor.instance.L2PcInstance;
+
+public class LightPurpleManedHorse extends L2DefaultTransformation
+{
+	public LightPurpleManedHorse()
+	{
+		// id, duration (secs), colRadius, colHeight
+		super(106, 1800, 12.0, 32.0);
+	}
+
+	public void transformedSkills(L2PcInstance player)
+	{
+	}
+
+	public void removeSkills(L2PcInstance player)
+	{
+	}
+
+	public static void main(String[] args)
+	{
+		TransformationManager.getInstance().registerTransformation(new LightPurpleManedHorse());
+	}
+}
