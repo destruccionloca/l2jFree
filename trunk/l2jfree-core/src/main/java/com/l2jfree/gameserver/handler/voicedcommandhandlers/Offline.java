3f448cc5aa9bedc23c2fd9b4d0409b4a653a27e1
evill33t <ronny.stiftel@gmail.com>
2009-05-31 18:25:42 +0000

    auction/mail/offline shop/bug report(wip) handlers added
    git-svn-id: http://svn.l2jfree.com/l2jfree@5990 6ff00ba9-25cd-4355-987e-099c62bc2628

---
+++ trunk/l2jfree-core/src/main/java/com/l2jfree/gameserver/handler/voicedcommandhandlers/Offline.java
@@ -0,0 +1,41 @@
+package com.l2jfree.gameserver.handler.voicedcommandhandlers;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.l2jfree.Config;
+import com.l2jfree.gameserver.handler.IVoicedCommandHandler;
+import com.l2jfree.gameserver.model.actor.instance.L2PcInstance;
+import com.l2jfree.gameserver.model.zone.L2Zone;
+
+public class Offline implements IVoicedCommandHandler
+{
+	protected static Log			_log			= LogFactory.getLog(Offline.class);
+	private static final String[]	VOICED_COMMANDS	= { "offline" };
+
+	public boolean useVoicedCommand(String command, L2PcInstance activeChar, String target)
+	{
+		if ((activeChar.getPrivateStoreType() == 1 || activeChar.getPrivateStoreType() == 3 || (activeChar.getPrivateStoreType() == 5 && Config.ALLOW_OFFLINE_TRADE_CRAFT)))
+		{
+			if (activeChar.isInsideZone(L2Zone.FLAG_PEACE) || activeChar.isGM())
+			{
+				return activeChar.setOffline();
+			}
+			else
+			{
+				activeChar.sendMessage("You must be in a peace zone to use offline mode");
+				return false;
+			}
+		}
+		else
+		{
+			activeChar.sendMessage("You must be in a peace zone to use offline mode");
+			return false;
+		}
+	}
+
+	public String[] getVoicedCommandList()
+	{
+		return VOICED_COMMANDS;
+	}
+}
