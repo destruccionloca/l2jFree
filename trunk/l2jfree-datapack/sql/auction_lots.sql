0ac311ba8c657f7ba7e49e5a9abcd969e82b94c6
evill33t <ronny.stiftel@gmail.com>
2009-05-31 19:36:42 +0000

    auction and changelog sql
    git-svn-id: http://svn.l2jfree.com/l2jfree@6034 6ff00ba9-25cd-4355-987e-099c62bc2628

---
+++ trunk/l2jfree-datapack/sql/auction_lots.sql
@@ -0,0 +1,20 @@
+-- ----------------------------
+-- Table structure for auction_lots
+-- ----------------------------
+DROP TABLE IF EXISTS `auction_lots`;
+CREATE TABLE `auction_lots` (
+  `lotId` int(10) NOT NULL AUTO_INCREMENT,
+  `ownerId` int(10) NOT NULL,
+  `itemId` int(6) NOT NULL,
+  `objectId` int(10) NOT NULL,
+  `count` bigint(20) NOT NULL,
+  `enchantLevel` int(6) NOT NULL,
+  `currency` int(11) NOT NULL,
+  `startingBid` bigint(20) NOT NULL,
+  `bidIncrement` bigint(20) NOT NULL,
+  `buyNow` bigint(20) NOT NULL,
+  `endDate` bigint(20) NOT NULL,
+  `processed` varchar(5) NOT NULL DEFAULT 'false',
+  PRIMARY KEY (`lotId`),
+  KEY `lotId` (`ownerId`)
+) ENGINE=InnoDB AUTO_INCREMENT=3816 DEFAULT CHARSET=utf8;
---
+++ trunk/l2jfree-datapack/sql/changelog.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS `changelog`;
+CREATE TABLE `changelog` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `udate` date NOT NULL,
+  `introduction` varchar(255) NOT NULL,
+  `text` varchar(4500) NOT NULL,
+  `author` varchar(25) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
