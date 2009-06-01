c4ebf76ef9cae393917d1684397dcea8532149f5
evill33t <ronny.stiftel@gmail.com>
2009-06-01 14:17:11 +0000

    fixing import
    git-svn-id: http://svn.l2jfree.com/l2jfree@6078 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/l2jfree-core/src/main/java/com/l2jfree/gameserver/templates/item/L2Equip.java
+++ trunk/l2jfree-core/src/main/java/com/l2jfree/gameserver/templates/item/L2Equip.java
@@ -22,7 +22,7 @@ import org.apache.commons.logging.LogFactory;
 import com.l2jfree.gameserver.datatables.SkillTable;
 import com.l2jfree.gameserver.model.L2Effect;
 import com.l2jfree.gameserver.model.L2Skill;
-import com.l2jfree.gameserver.skills.effects.EffectTemplate;
+import com.l2jfree.gameserver.templates.effects.EffectTemplate;
 import com.l2jfree.gameserver.skills.funcs.Func;
 import com.l2jfree.gameserver.skills.funcs.FuncTemplate;
 import com.l2jfree.gameserver.templates.StatsSet;
