7cb9793d7c4f1587a2789a0dbed5eb3622ae6902
nbali <nbalika@gmail.com>
2009-06-06 18:30:54 +0000

    GPL license
    git-svn-id: http://svn.l2jfree.com/l2jfree@6128 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/l2jfree-core/src/main/java/com/l2jfree/gameserver/model/entity/FortUpdater.java
+++ trunk/l2jfree-core/src/main/java/com/l2jfree/gameserver/model/entity/FortUpdater.java
@@ -1,4 +1,5 @@
-/* This program is free software: you can redistribute it and/or modify it under
+/*
+ * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
@@ -11,7 +12,6 @@
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 package com.l2jfree.gameserver.model.entity;
 
 import org.apache.commons.logging.Log;
