6f84141ce4aa15b110a31fc1460d9c8f6cb199ef
evill33t <ronny.stiftel@gmail.com>
2009-06-07 16:52:17 +0000

    missing operate type
    git-svn-id: http://svn.l2jfree.com/l2jfree@6173 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/l2jfree-core/src/main/java/com/l2jfree/gameserver/model/L2Skill.java
+++ trunk/l2jfree-core/src/main/java/com/l2jfree/gameserver/model/L2Skill.java
@@ -101,7 +101,7 @@ public class L2Skill implements FuncOwner, IChanceSkillTrigger
 
 	public static enum SkillOpType
 	{
-		OP_PASSIVE, OP_ACTIVE, OP_TOGGLE
+		OP_PASSIVE, OP_ACTIVE, OP_TOGGLE, OP_CHANCE
 	}
 
 	/** Target types of skills : SELF, PARTY, CLAN, PET... */
