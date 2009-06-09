7d1e77faa92f24e22dc0eed9c85b8682ce152ddd
nbali <nbalika@gmail.com>
2009-06-09 11:20:16 +0000

    http://www.l2jfree-users.com/l2jfreeusers/posts/list/303.page
    git-svn-id: http://svn.l2jfree.com/l2jfree@6221 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/l2jfree-datapack/data/scripts/instances/Kamaloka/__init__.py
+++ trunk/l2jfree-datapack/data/scripts/instances/Kamaloka/__init__.py
@@ -10,7 +10,7 @@ from com.l2jfree import L2DatabaseFactory
 from com.l2jfree.gameserver.instancemanager        import InstanceManager
 from com.l2jfree.gameserver.model.entity           import Instance
 from com.l2jfree.gameserver.model.actor            import L2Character
-from com.l2jfree.gameserver.model                  import L2Summon
+from com.l2jfree.gameserver.model.actor            import L2Summon
 from com.l2jfree.gameserver.model                  import L2World
 from com.l2jfree.gameserver.model.quest            import State
 from com.l2jfree.gameserver.model.quest            import QuestState
