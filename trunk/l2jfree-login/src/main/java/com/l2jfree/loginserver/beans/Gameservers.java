487005d6008d81040e595ef07ae4f77c2eb034a9
nbali <nbalika@gmail.com>
2009-07-12 16:12:56 +0000

    GPL3 again
    git-svn-id: http://svn.l2jfree.com/l2jfree@6578 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/l2jfree-login/src/main/java/com/l2jfree/loginserver/beans/Gameservers.java
+++ trunk/l2jfree-login/src/main/java/com/l2jfree/loginserver/beans/Gameservers.java
@@ -1,3 +1,17 @@
+/*
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.l2jfree.loginserver.beans;
 
 // Generated 16 dec. 2006 13:48:45 by Hibernate Tools 3.2.0.beta8
