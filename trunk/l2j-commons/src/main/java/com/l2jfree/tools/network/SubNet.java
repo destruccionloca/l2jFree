c6c39f9baceda09b43bfa9837c30e0ea21ae668b
nbali <nbalika@gmail.com>
2009-07-22 12:17:13 +0000

    hashcode() should be overriden too if equals() is overriden
    git-svn-id: http://svn.l2jfree.com/l2jfree@6686 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/l2j-commons/src/main/java/com/l2jfree/tools/network/SubNet.java
+++ trunk/l2j-commons/src/main/java/com/l2jfree/tools/network/SubNet.java
@@ -23,27 +23,27 @@ import java.util.StringTokenizer;
 public class SubNet {
 	private final int _net;
 	private final int _mask;
-
+	
 	public SubNet(int net, int mask) {
 		_net = net;
 		_mask = mask;
 	}
-
+	
 	public SubNet(String net, int mask) {
 		_net = ipToInt(net);
 		_mask = mask;
 	}
-
+	
 	public SubNet(String net, String mask) {
 		_net = ipToInt(net);
 		_mask = ipToInt(mask);
 	}
-
+	
 	public SubNet(int net, String mask) {
 		_net = net;
 		_mask = ipToInt(mask);
 	}
-
+	
 	public SubNet(String netMask) {
 		StringTokenizer st = new StringTokenizer(netMask.trim(), "/");
 		_net = ipToInt(st.nextToken());
@@ -52,45 +52,52 @@ public class SubNet {
 		else
 			_mask = 0xFFFFFFFF;
 	}
-
+	
 	public int getNet() {
 		return _net;
 	}
-
+	
 	public int getMask() {
 		return _mask;
 	}
-
+	
 	public boolean isInSubnet(int ip) {
-
+		
 		return ((ip & _mask) == _net);
 	}
-
+	
 	public boolean isInSubnet(String ip) {
 		int _ip = ipToInt(ip);
 		return isInSubnet(_ip);
 	}
-
+	
 	@Override
-	public boolean equals(Object obj) {
+	public int hashCode()
+	{
+		return _net + _mask;
+	}
+	
+	@Override
+	public boolean equals(Object obj)
+	{
 		if (obj instanceof SubNet)
-			return (((SubNet) obj).getNet() == _net && ((SubNet) obj).getMask() == _mask);
-
+			return (((SubNet)obj).getNet() == _net && ((SubNet)obj).getMask() == _mask);
+		
 		return false;
 	}
-
+	
 	private int ipToInt(String ip) {
 		int _ip = 0;
-
+		
 		StringTokenizer st = new StringTokenizer(ip.trim(), ".");
-
+		
 		int _dots = st.countTokens();
-
+		
 		if (_dots == 1) {
 			_ip = 0xFFFFFFFF;
 			try {
 				int _bitmask = Integer.parseInt(st.nextToken());
-
+				
 				if (_bitmask > 0) {
 					if (_bitmask < 32)
 						_ip = (_ip << (32 - _bitmask));
@@ -105,7 +112,7 @@ public class SubNet {
 				} catch (NumberFormatException e) {
 				}
 			}
-
+		
 		return _ip;
 	}
 }
