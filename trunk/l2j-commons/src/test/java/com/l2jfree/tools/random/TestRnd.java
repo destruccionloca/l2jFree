cbd0b5b6bcacdc767541de5ae6b836508cf978a7
nbali <nbalika@gmail.com>
2009-08-14 16:47:10 +0000

    Deprecated unit test
    git-svn-id: http://svn.l2jfree.com/l2jfree@6793 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/l2j-commons/src/test/java/com/l2jfree/tools/random/TestRnd.java
+++ trunk/l2j-commons/src/test/java/com/l2jfree/tools/random/TestRnd.java
@@ -29,32 +29,4 @@ public class TestRnd extends TestCase {
 		assertTrue(Rnd.nextInt(-600) <= 0);
 		assertTrue(Rnd.get(-60, 50) <= 50 && Rnd.get(-60, 50) >= -60);
 	}
-
-	public void testSpeed() {
-		long timeInt = 0;
-		long timeDbl = 0;
-
-		@SuppressWarnings("unused")
-		int res = 0;
-
-		for (int j = 0; j < 2; j++) {
-			timeInt = System.currentTimeMillis();
-
-			for (int i = 0; i < 1000000; i++) {
-				res = Rnd.get(-100000, 100000);
-			}
-
-			timeInt = System.currentTimeMillis() - timeInt;
-
-			timeDbl = System.currentTimeMillis();
-
-			for (int i = 0; i < 1000000; i++) {
-				res = -100000 + (int) (Rnd.nextDouble() * 100000);
-			}
-
-			timeDbl = System.currentTimeMillis() - timeDbl;
-		}
-
-		assertTrue(timeDbl > timeInt);
-	}
 }
