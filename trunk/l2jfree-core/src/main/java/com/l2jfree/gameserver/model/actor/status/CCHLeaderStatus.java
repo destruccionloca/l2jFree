076ce5d5d0fdc5a4df1579f43043b8b18033f804
nbali <nbalika@gmail.com>
2009-08-20 00:58:27 +0000

    CCHLeaderStatus  * I guess this is the was it supposed to be
    git-svn-id: http://svn.l2jfree.com/l2jfree@6855 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/l2jfree-core/src/main/java/com/l2jfree/gameserver/model/actor/status/CCHLeaderStatus.java
+++ trunk/l2jfree-core/src/main/java/com/l2jfree/gameserver/model/actor/status/CCHLeaderStatus.java
@@ -1,36 +1,44 @@
 package com.l2jfree.gameserver.model.actor.status;
 
 import com.l2jfree.gameserver.instancemanager.CCHManager;
+import com.l2jfree.gameserver.model.L2Object;
 import com.l2jfree.gameserver.model.actor.L2Character;
 import com.l2jfree.gameserver.model.actor.instance.L2CCHBossInstance;
 import com.l2jfree.gameserver.model.actor.instance.L2PcInstance;
 import com.l2jfree.gameserver.model.entity.CCHSiege;
 
+/**
+ * @author savormix
+ */
 public final class CCHLeaderStatus extends AttackableStatus
 {
 	public CCHLeaderStatus(L2CCHBossInstance activeChar)
 	{
 		super(activeChar);
 	}
-
+	
 	@Override
-	public final void reduceHp0(double value, L2Character attacker, boolean awake, boolean isDOT,
-			boolean isConsume)
+	void reduceHp0(double value, L2Character attacker, boolean awake, boolean isDOT, boolean isConsume)
 	{
-		if (!canReduceHp(value, attacker, awake, isDOT, isConsume))
-			return;
 		super.reduceHp0(value, attacker, awake, isDOT, isConsume);
-		L2PcInstance player = attacker.getActingPlayer();
+		
+		final L2PcInstance player = L2Object.getActingPlayer(attacker);
 		if (player == null)
 			return;
+		
 		CCHSiege siege = CCHManager.getInstance().getSiege(player.getClan());
 		if (siege != null && siege.equals(getActiveChar().getSiege()))
-			getActiveChar().getDamageTable().put(player.getClanId(), (int) value);
+		{
+			Integer previousValue = getActiveChar().getDamageTable().get(player.getClanId());
+			int newValue = (previousValue == null ? 0 : previousValue.intValue()) + (int)value;
+			
+			getActiveChar().getDamageTable().put(player.getClanId(), newValue);
+		}
 	}
-
+	
 	@Override
-	public final L2CCHBossInstance getActiveChar()
+	public L2CCHBossInstance getActiveChar()
 	{
-		return (L2CCHBossInstance) super.getActiveChar();
+		return (L2CCHBossInstance)_activeChar;
 	}
 }
