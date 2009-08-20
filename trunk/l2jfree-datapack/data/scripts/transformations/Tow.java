8363d2340bad6645094fed69a1f132827eed3b10
Psycho <laurentchervet@bluewin.ch>
2009-08-20 19:23:20 +0000

    /trunk/datapack: - A lot of missing transformations, should sync L2j's ones, but a few more here. Without skills
    git-svn-id: http://svn.l2jfree.com/l2jfree@6894 6ff00ba9-25cd-4355-987e-099c62bc2628

---
+++ trunk/l2jfree-datapack/data/scripts/transformations/Tow.java
@@ -0,0 +1,55 @@
+/*
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package transformations;
+
+import com.l2jfree.gameserver.instancemanager.TransformationManager;
+import com.l2jfree.gameserver.model.L2DefaultTransformation;
+import com.l2jfree.gameserver.model.actor.instance.L2PcInstance;
+
+/**
+ * Description: <br>
+ * This will handle the transformation, giving the skills, and removing them, when the player logs out and is transformed these skills
+ * do not save.
+ * When the player logs back in, there will be a call from the enterworld packet that will add all their skills.
+ * The enterworld packet will transform a player.
+ *
+ * @author Psychokiller1888
+ *
+ */
+public class Tow extends L2DefaultTransformation
+{
+	public Tow()
+	{
+		// id, colRadius, colHeight
+		super(117, 8.0, 8.0);
+	}
+
+	@Override
+	public void transformedSkills(L2PcInstance player)
+	{
+		addSkill(player,8248, 1); // Transfrom Dispel
+	}
+
+	@Override
+	public void removeSkills(L2PcInstance player)
+	{
+		removeSkill(player,8248); // Transfrom Dispel
+	}
+
+	public static void main(String[] args)
+	{
+		TransformationManager.getInstance().registerTransformation(new Tow());
+	}
+}
