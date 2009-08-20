049b70f55c87175dbb0e517d523a66d874f640b8
Droppo <Droppo@gmail.com>
2009-08-20 03:02:45 +0000

    Sync to L2JDP 6466: SQL: DP part for [http://www.l2jserver.com/trac/changeset/3369] by _DS_ (UPDATE REQUIRED!).
    git-svn-id: http://svn.l2jfree.com/l2jfree@6865 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/l2jfree-datapack/sql/character_skills.sql
+++ trunk/l2jfree-datapack/sql/character_skills.sql
@@ -2,7 +2,6 @@ CREATE TABLE IF NOT EXISTS `character_skills` (
   `charId` INT UNSIGNED NOT NULL,
   `skill_id` SMALLINT UNSIGNED NOT NULL,
   `skill_level` SMALLINT(3) NOT NULL,
-  `skill_name` VARCHAR(40),
   `class_index` TINYINT UNSIGNED NOT NULL,
   PRIMARY KEY (`charId`,`skill_id`,`class_index`)
 ) DEFAULT CHARSET=utf8;
