567e4983b3784105047d439dc10a7dab3c26f9c8
evill33t <ronny.stiftel@gmail.com>
2009-09-08 20:10:35 +0000

    sync l2j 3421 Warehouse manager check fix, thanks tgs.
    git-svn-id: http://svn.l2jfree.com/l2jfree@7035 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/l2jfree-core/src/main/java/com/l2jfree/gameserver/network/clientpackets/RequestPackageSend.java
+++ trunk/l2jfree-core/src/main/java/com/l2jfree/gameserver/network/clientpackets/RequestPackageSend.java
@@ -3,12 +3,12 @@
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -37,7 +37,7 @@ import com.l2jfree.gameserver.network.serverpackets.SystemMessage;
 public final class RequestPackageSend extends L2GameClientPacket
 {
 	private static final String	_C_9F_REQUESTPACKAGESEND	= "[C] 9F RequestPackageSend";
-	
+
 	private int					_objectID;
 
 	private static final int BATCH_LENGTH = 8; // length of the one item
@@ -95,7 +95,7 @@ public final class RequestPackageSend extends L2GameClientPacket
 
 		L2Npc manager = player.getLastFolkNPC();
 		if ((manager == null
-				|| !(manager instanceof L2WarehouseInstance)
+				|| !manager.isWarehouse()
 				|| !manager.canInteract(player)) && !player.isGM())
 			return;
 
