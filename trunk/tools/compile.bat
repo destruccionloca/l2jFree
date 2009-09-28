2cf901df2c3376fbcaa29e40e355399ff3228436
nbali <nbalika@gmail.com>
2009-09-28 15:47:48 +0000

    Small addition to the script
    git-svn-id: http://svn.l2jfree.com/l2jfree@7186 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/tools/compile.bat
+++ trunk/tools/compile.bat
@@ -1,9 +1,12 @@
 @echo off
 
 REM #############################################
+REM # Configure this, if you don't have 'svn' in the path!
+SET PATH=%PATH%;C:\Program Files\Subversion\bin
+
 set MAVEN_OPTS=-Xms64m -Xmx256m
 
-REM # Configure this, if you don't have 'mvn' as an environment variable!
+REM # Configure this, if you don't have 'mvn' in the path!
 set MAVEN=mvn
 REM #############################################
 
--- trunk/tools/compile.sh
+++ trunk/tools/compile.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 ##############################################
+# Configure this, if you don't have 'svn' in the path!
+PATH=${PATH}:/usr/bin
+
 MAVEN_OPTS="-Xms64m -Xmx256m"
 
-# Configure this, if you don't have 'mvn' as an environment variable!
+# Configure this, if you don't have 'mvn' in the path!
 MAVEN="mvn"
 ##############################################
 
