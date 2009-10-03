1f8f010ed4c1cb571a03e450ec900b43ddae60c7
nbali <nbalika@gmail.com>
2009-10-03 10:29:46 +0000

    git-svn-id: http://svn.l2jfree.com/l2jfree@7209 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/l2jfree-core/dist/setenv.bat
+++ trunk/l2jfree-core/dist/setenv.bat
@@ -1,42 +1,43 @@
 SET VERSION=1.3.0
 
-SET CLASSPATH=%CLASSPATH%;./lib/l2j-mmocore-%VERSION%.jar
+REM SET CLASSPATH=%CLASSPATH%;./lib/l2j-mmocore-%VERSION%.jar
 
 REM jython
-SET CLASSPATH=%CLASSPATH%;./lib/jython-2.2.1.jar
-SET CLASSPATH=%CLASSPATH%;./lib/jython-engine-1.0.1.jar
+REM SET CLASSPATH=%CLASSPATH%;./lib/jython-2.2.1.jar
+REM SET CLASSPATH=%CLASSPATH%;./lib/jython-engine-1.0.1.jar
 
-SET CLASSPATH=%CLASSPATH%;./lib/bsf-2.0.jar
-SET CLASSPATH=%CLASSPATH%;./lib/bsh-2.0b5.jar
-SET CLASSPATH=%CLASSPATH%;./lib/bsh-engine-1.0.1.jar
-SET CLASSPATH=%CLASSPATH%;./lib/core-3.3.0.jar
-SET CLASSPATH=%CLASSPATH%;./lib/java-engine-1.0.2.jar
+REM SET CLASSPATH=%CLASSPATH%;./lib/bsf-2.0.jar
+REM SET CLASSPATH=%CLASSPATH%;./lib/bsh-2.0b5.jar
+REM SET CLASSPATH=%CLASSPATH%;./lib/bsh-engine-1.0.1.jar
+REM SET CLASSPATH=%CLASSPATH%;./lib/core-3.3.0.jar
+REM SET CLASSPATH=%CLASSPATH%;./lib/java-engine-1.0.2.jar
 
-SET CLASSPATH=%CLASSPATH%;./lib/commons-lang-2.4.jar
+REM SET CLASSPATH=%CLASSPATH%;./lib/commons-lang-2.4.jar
 
 REM For connection pool
-SET CLASSPATH=%CLASSPATH%;./lib/c3p0-0.9.1.2.jar
+REM SET CLASSPATH=%CLASSPATH%;./lib/c3p0-0.9.1.2.jar
 
 REM for logging usage
-SET CLASSPATH=%CLASSPATH%;./lib/commons-logging-1.1.1.jar
+REM SET CLASSPATH=%CLASSPATH%;./lib/commons-logging-1.1.1.jar
 
 REM for common input output 
-SET CLASSPATH=%CLASSPATH%;./lib/commons-io-1.4.jar
+REM SET CLASSPATH=%CLASSPATH%;./lib/commons-io-1.4.jar
 
 REM for performance usage
-SET CLASSPATH=%CLASSPATH%;./lib/javolution-5.3.1.jar
+REM SET CLASSPATH=%CLASSPATH%;./lib/javolution-5.3.1.jar
 
 REM main jar
-SET CLASSPATH=%CLASSPATH%;./lib/l2j-commons-%VERSION%.jar
+REM SET CLASSPATH=%CLASSPATH%;./lib/l2j-commons-%VERSION%.jar
 SET CLASSPATH=%CLASSPATH%;l2jfree-core-%VERSION%.jar
+SET CLASSPATH=%CLASSPATH%;./lib/
 
 REM For SQL use
-SET CLASSPATH=%CLASSPATH%;./lib/mysql-connector-java-5.1.6.jar
+REM SET CLASSPATH=%CLASSPATH%;./lib/mysql-connector-java-5.1.6.jar
 
 REM For IRC use
-SET CLASSPATH=%CLASSPATH%;./lib/irclib-1.10.jar
+REM SET CLASSPATH=%CLASSPATH%;./lib/irclib-1.10.jar
 
-SET CLASSPATH=%CLASSPATH%;./lib/trove-2.1.1.jar
+REM SET CLASSPATH=%CLASSPATH%;./lib/trove-2.1.1.jar
 
 REM for configuration
 SET CLASSPATH=%CLASSPATH%;./config/
--- trunk/l2jfree-login/dist/setenv.bat
+++ trunk/l2jfree-login/dist/setenv.bat
@@ -1,46 +1,47 @@
 SET VERSION=1.3.0
 
 REM Hibernate and associated jars
-SET CLASSPATH=%CLASSPATH%;./lib/antlr-2.7.6.jar
-SET CLASSPATH=%CLASSPATH%;./lib/asm-1.5.3.jar
-SET CLASSPATH=%CLASSPATH%;./lib/asm-attrs-1.5.3.jar
-SET CLASSPATH=%CLASSPATH%;./lib/cglib-2.1_3.jar
-SET CLASSPATH=%CLASSPATH%;./lib/hibernate-3.2.2.ga.jar
-SET CLASSPATH=%CLASSPATH%;./lib/jta-1.0.1B.jar
-SET CLASSPATH=%CLASSPATH%;./lib/commons-collections-2.1.1.jar
+REM SET CLASSPATH=%CLASSPATH%;./lib/antlr-2.7.6.jar
+REM SET CLASSPATH=%CLASSPATH%;./lib/asm-1.5.3.jar
+REM SET CLASSPATH=%CLASSPATH%;./lib/asm-attrs-1.5.3.jar
+REM SET CLASSPATH=%CLASSPATH%;./lib/cglib-2.1_3.jar
+REM SET CLASSPATH=%CLASSPATH%;./lib/hibernate-3.2.2.ga.jar
+REM SET CLASSPATH=%CLASSPATH%;./lib/jta-1.0.1B.jar
+REM SET CLASSPATH=%CLASSPATH%;./lib/commons-collections-2.1.1.jar
 
 REM for second level cache (hibernate)
-SET CLASSPATH=%CLASSPATH%;./lib/ehcache-1.2.3.jar
+REM SET CLASSPATH=%CLASSPATH%;./lib/ehcache-1.2.3.jar
 
-SET CLASSPATH=%CLASSPATH%;./lib/l2j-mmocore-%VERSION%.jar
+REM SET CLASSPATH=%CLASSPATH%;./lib/l2j-mmocore-%VERSION%.jar
 
-SET CLASSPATH=%CLASSPATH%;./lib/commons-lang-2.4.jar
+REM SET CLASSPATH=%CLASSPATH%;./lib/commons-lang-2.4.jar
 
 REM For connection pool
-SET CLASSPATH=%CLASSPATH%;./lib/c3p0-0.9.1.2.jar
+REM SET CLASSPATH=%CLASSPATH%;./lib/c3p0-0.9.1.2.jar
 
 REM for logging usage
-SET CLASSPATH=%CLASSPATH%;./lib/commons-logging-1.1.1.jar
+REM SET CLASSPATH=%CLASSPATH%;./lib/commons-logging-1.1.1.jar
 
 REM for common input output 
-SET CLASSPATH=%CLASSPATH%;./lib/commons-io-1.4.jar
+REM SET CLASSPATH=%CLASSPATH%;./lib/commons-io-1.4.jar
 
 REM for dom 
-SET CLASSPATH=%CLASSPATH%;./lib/dom4j-1.6.1.jar
+REM SET CLASSPATH=%CLASSPATH%;./lib/dom4j-1.6.1.jar
 
 REM for performance usage
-SET CLASSPATH=%CLASSPATH%;./lib/javolution-5.3.1.jar
+REM SET CLASSPATH=%CLASSPATH%;./lib/javolution-5.3.1.jar
 
 REM main jar
-SET CLASSPATH=%CLASSPATH%;./lib/l2j-commons-%VERSION%.jar
+REM SET CLASSPATH=%CLASSPATH%;./lib/l2j-commons-%VERSION%.jar
 SET CLASSPATH=%CLASSPATH%;l2jfree-login-%VERSION%.jar
+SET CLASSPATH=%CLASSPATH%;./lib/
 
 REM spring 
-SET CLASSPATH=%CLASSPATH%;./lib/spring-2.0.2.jar
-SET CLASSPATH=%CLASSPATH%;./lib/spring-mock-2.0.2.jar
+REM SET CLASSPATH=%CLASSPATH%;./lib/spring-2.0.2.jar
+REM SET CLASSPATH=%CLASSPATH%;./lib/spring-mock-2.0.2.jar
 
 REM For SQL use
-SET CLASSPATH=%CLASSPATH%;./lib/mysql-connector-java-5.1.6.jar
+REM SET CLASSPATH=%CLASSPATH%;./lib/mysql-connector-java-5.1.6.jar
 
 REM for configuration
 SET CLASSPATH=%CLASSPATH%;./config/
