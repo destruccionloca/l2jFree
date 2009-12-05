880482a066083079003bd2c5731d96dfa11305ed
nbali <nbalika@gmail.com>
2009-12-05 09:40:37 +0000

    FlyToLocation  * fix for client glitch
    (the flying character's coordinates were changed just after creation of the packet, but before it was written by the mmocore, so the packet sent the new coordinates for the flying character -> resulted in client sided problems..)
    
    git-svn-id: http://svn.l2jfree.com/l2jfree@7618 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/l2jfree-core/src/main/java/com/l2jfree/gameserver/network/serverpackets/FlyToLocation.java
+++ trunk/l2jfree-core/src/main/java/com/l2jfree/gameserver/network/serverpackets/FlyToLocation.java
@@ -17,55 +17,57 @@ package com.l2jfree.gameserver.network.serverpackets;
 import com.l2jfree.gameserver.model.actor.L2Character;
 
 /**
- *
- * @author  KenM
+ * @author KenM
  */
 public final class FlyToLocation extends L2GameServerPacket
 {
-    private final L2Character _cha;
-    private final int _destX, _destY, _destZ;
-    private final FlyType _type;
-
-    public enum FlyType
-    {
-        THROW_UP,
-        THROW_HORIZONTAL,
-        DUMMY, // no effect
-        CHARGE
-    }
-
-    public FlyToLocation(L2Character cha, int destX, int destY, int destZ, FlyType type)
-    {
-        _cha = cha;
-        _destX = destX;
-        _destY = destY;
-        _destZ = destZ;
-        _type = type;
-    }
-
-    public FlyToLocation(L2Character cha, L2Character dest, FlyType type)
-    {
-        this(cha, dest.getX(), dest.getY(), dest.getZ(), type);
-    }
-
-    @Override
-    public String getType()
-    {
-        return "[S] 0xd4 FlyToLocation";
-    }
-
-    @Override
-    protected void writeImpl()
-    {
-        writeC(0xd4);
-        writeD(_cha.getObjectId());
-        writeD(_destX);
-        writeD(_destY);
-        writeD(_destZ);
-        writeD(_cha.getX());
-        writeD(_cha.getY());
-        writeD(_cha.getZ());
-        writeD(_type.ordinal());
-    }
-
+	private final int _objId;
+	private final int _x, _y, _z;
+	private final int _destX, _destY, _destZ;
+	private final FlyType _type;
+	
+	public enum FlyType
+	{
+		THROW_UP,
+		THROW_HORIZONTAL,
+		DUMMY, // no effect
+		CHARGE;
+	}
+	
+	public FlyToLocation(L2Character cha, int destX, int destY, int destZ, FlyType type)
+	{
+		_objId = cha.getObjectId();
+		_x = cha.getX();
+		_y = cha.getY();
+		_z = cha.getZ();
+		_destX = destX;
+		_destY = destY;
+		_destZ = destZ;
+		_type = type;
+	}
+	
+	public FlyToLocation(L2Character cha, L2Character dest, FlyType type)
+	{
+		this(cha, dest.getX(), dest.getY(), dest.getZ(), type);
+	}
+	
+	@Override
+	public String getType()
+	{
+		return "[S] 0xd4 FlyToLocation";
+	}
+	
+	@Override
+	protected void writeImpl()
+	{
+		writeC(0xd4);
+		writeD(_objId);
+		writeD(_destX);
+		writeD(_destY);
+		writeD(_destZ);
+		writeD(_x);
+		writeD(_y);
+		writeD(_z);
+		writeD(_type.ordinal());
+	}
 }
