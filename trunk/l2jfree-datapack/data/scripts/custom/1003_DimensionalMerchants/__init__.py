cf727cbe723f699a4608bc889a7dd88dc91c079d
Psycho <laurentchervet@bluewin.ch>
2009-12-19 13:17:16 +0000

    /trunk/datapack: - Dimensional Merchants exchange function
    git-svn-id: http://svn.l2jfree.com/l2jfree@7690 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/l2jfree-datapack/data/scripts/custom/1003_DimensionalMerchants/__init__.py
+++ trunk/l2jfree-datapack/data/scripts/custom/1003_DimensionalMerchants/__init__.py
@@ -1,11 +1,10 @@
-# Script by Psychokiller1888 / L2jFree
+# Script by Psycho(killer1888) / L2jFree
 
 import sys
 from com.l2jfree                               import Config
 from com.l2jfree.gameserver.model.quest        import State
 from com.l2jfree.gameserver.model.quest        import QuestState
 from com.l2jfree.gameserver.model.quest.jython import QuestJython as JQuest
-
 
 qn = "1003_DimensionalMerchants"
 
@@ -15,7 +14,35 @@ class Quest (JQuest) :
         JQuest.__init__(self,id,name,descr)
 
     def onAdvEvent(self,event,npc,player):
+        st = player.getQuestState(qn)
+        if not st: return
         htmltext = event
+        if event == "13017" or event == "13018" or event == "13019" or event == "13020":
+            # Player can either have an event coupon or a normal coupon, so first check for the normal one
+            normalItem = st.getQuestItemsCount(13273)
+            eventItem = st.getQuestItemsCount(13383)
+            if normalItem >= 1:
+                st.takeItems(13273,1)
+                st.giveItems(int(event),1)
+                st.exitQuest(1)
+                return
+            elif eventItem >= 1:
+                event = int(event) + 286
+                st.takeItems(13383,1)
+                st.giveItems(int(event),1)
+                st.exitQuest(1)
+                return
+            else:
+                htmltext = "32478-11.htm"
+        elif event == "13548" or event == "13549" or event == "13550" or event == "13551":
+            if st.getQuestItemsCount(14065) >= 1:
+                st.takeItems(14065,1)
+                st.giveItems(int(event),1)
+                st.exitQuest(1)
+                return
+            else:
+                htmltext = "32478-11.htm"
+                st.exitQuest(1)
         return htmltext
 
     def onFirstTalk(self,npc,player):
