62a168a1074e0310a786ecfd1c30c020e844befe
savormix <cossacking@gmail.com>
2009-12-27 12:38:46 +0000

    /trunk/core: - Critical issues fixed * Cannot use 2.5.x or newer jython with an outdated jython-engine * Removing entries in direct iterations guarantees the subsequent call to entry.getNext() return null, thus invalidating the whole iteration after first removal
    git-svn-id: http://svn.l2jfree.com/l2jfree@7729 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/l2jfree-core/src/main/java/com/l2jfree/gameserver/taskmanager/MovementController.java
+++ trunk/l2jfree-core/src/main/java/com/l2jfree/gameserver/taskmanager/MovementController.java
@@ -14,6 +14,8 @@
  */
 package com.l2jfree.gameserver.taskmanager;
 
+import java.util.ArrayList;
+
 import javolution.util.FastMap;
 import javolution.util.FastMap.Entry;
 
@@ -72,6 +74,8 @@ public final class MovementController extends AbstractPeriodicTaskManager
 	@Override
 	public void run()
 	{
+		// see my messages on MSN
+		ArrayList<L2Character> dirtyFix = new ArrayList<L2Character>();
 		for (Entry<L2Character, TickRange> entry = _movingChars.head(), end = _movingChars.tail();
 				(entry = entry.getNext()) != end;)
 		{
@@ -85,9 +89,12 @@ public final class MovementController extends AbstractPeriodicTaskManager
 			if (!cha.updatePosition(GameTimeController.getGameTicks()))
 				continue;
 			
-			_movingChars.remove(cha);
+			dirtyFix.add(cha);
+			//_movingChars.remove(cha);
 			_evtArrivedManager.execute(cha);
 		}
+		for (L2Character cha : dirtyFix)
+			_movingChars.remove(cha);
 	}
 	
 	private final class EvtArrivedManager extends FIFOSimpleExecutableQueue<L2Character>
