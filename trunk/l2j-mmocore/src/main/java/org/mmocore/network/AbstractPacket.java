955f773368e9d499dfff8dc77915d46fb9642188
nbali <nbalika@gmail.com>
2010-01-08 01:20:43 +0000

    so again
    git-svn-id: http://svn.l2jfree.com/l2jfree@7856 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/l2j-mmocore/src/main/java/org/mmocore/network/AbstractPacket.java
+++ trunk/l2j-mmocore/src/main/java/org/mmocore/network/AbstractPacket.java
@@ -12,7 +12,7 @@
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package com.l2jfree.mmocore.network;
+package org.mmocore.network;
 
 import java.nio.ByteBuffer;
 
