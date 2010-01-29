6ac56b19598bbc98e11ee025b8eaf5251a6a4b74
savormix <cossacking@gmail.com>
2010-01-29 07:51:32 +0000

    /trunk/datapack: - Most probably a fix to 60_GoodWorkReward (3) - Weapon grade penalty now affects magic success rate
    git-svn-id: http://svn.l2jfree.com/l2jfree@8012 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/l2jfree-datapack/data/scripts/quests/60_GoodWorkReward/__init__.py
+++ trunk/l2jfree-datapack/data/scripts/quests/60_GoodWorkReward/__init__.py
@@ -78,7 +78,7 @@ class Quest (JQuest) :
       self.isNpcSpawned = 0
       return
     st = player.getQuestState(qn)
-    if st.isCompleted() : return QUEST_DONE
+    if st.isCompleted() : return "<html><body>This quest has already been completed.</body></html>"
     htmltext = event
     if event == "31435-03.htm" :
       st.set("cond","1")
