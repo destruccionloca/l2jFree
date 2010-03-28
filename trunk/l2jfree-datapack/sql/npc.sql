efcd4f8eb8b28180388e68adbea94964979bb6ef
hex1r0 <hex1r0@gmail.com>
2010-03-28 14:11:52 +0000

    /trunk/datapack: * QA dp side
    git-svn-id: http://svn.l2jfree.com/l2jfree@8119 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/l2jfree-datapack/sql/npc.sql
+++ trunk/l2jfree-datapack/sql/npc.sql
@@ -10083,3 +10083,8 @@ UPDATE `npc` SET `type` = 'L2Npc' WHERE `id` = 18466;
 
 -- Body Destroyer has special AI (hex1r0)
 UPDATE `npc` SET `type` =  'SCBodyDestroyer' WHERE `id` = 22363;
+
+-- Queen Ant and & family :) (hex1r0)
+UPDATE `npc` SET `type` = 'QueenAntLarva' WHERE `id` = 29002;
+UPDATE `npc` SET `type` = 'QueenAnt' WHERE `id` = 29001;
+UPDATE `npc` SET `type` = 'QueenAntNurse' WHERE `id` = 29003;
