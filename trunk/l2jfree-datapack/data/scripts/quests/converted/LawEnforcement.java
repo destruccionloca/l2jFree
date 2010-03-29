a6abb5af73fa63bb97776cde3c1addbc5da1700c
savormix <cossacking@gmail.com>
2010-03-29 05:23:09 +0000

    /trunk/datapack: - Minor Law Enforcement quest cleanup - Updating template references for optional multisells
    git-svn-id: http://svn.l2jfree.com/l2jfree@8128 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/l2jfree-datapack/data/scripts/quests/converted/LawEnforcement.java
+++ trunk/l2jfree-datapack/data/scripts/quests/converted/LawEnforcement.java
@@ -2,14 +2,15 @@ package quests.converted;
 
 import com.l2jfree.gameserver.model.actor.L2Npc;
 import com.l2jfree.gameserver.model.actor.instance.L2PcInstance;
+import com.l2jfree.gameserver.model.base.ClassId;
 import com.l2jfree.gameserver.model.base.Race;
+import com.l2jfree.gameserver.model.itemcontainer.PcInventory;
 import com.l2jfree.gameserver.model.quest.QuestState;
 import com.l2jfree.gameserver.model.quest.State;
 import com.l2jfree.gameserver.model.quest.jython.QuestJython;
 
 /**
- * Quest for Inspector to become Judicator
- * 
+ * 3rd class transfer quest for Inspector.
  * @author hex1r0
  */
 public final class LawEnforcement extends QuestJython
@@ -21,7 +22,7 @@ public final class LawEnforcement extends QuestJython
 	private static final int KEKROPUS = 32138;
 	private static final int EINDBURGH = 32469;
 
-	public LawEnforcement(int questId, String name, String descr)
+	private LawEnforcement(int questId, String name, String descr)
 	{
 		super(questId, name, descr);
 		addStartNpc(LIANE);
@@ -34,28 +35,29 @@ public final class LawEnforcement extends QuestJython
 	public String onAdvEvent(String event, L2Npc npc, L2PcInstance player)
 	{
 		QuestState qs = player.getQuestState(LAW_ENFORCEMENT);
-
 		if (qs == null)
 			return NO_QUEST;
+		else if (qs.isCompleted())
+			return QUEST_DONE;
 
 		if (event.equals("32222-05.htm"))
 		{
 			qs.set(CONDITION, 1);
 			qs.setState(State.STARTED);
-			qs.playSound("ItemSound.quest_accept");
+			player.sendPacket(SND_ACCEPT);
 		}
 		else if (event.equals("32138-09.htm"))
 		{
-			qs.set("cond", "2");
-			qs.playSound("ItemSound.quest_middle");
+			qs.set(CONDITION, 2);
+			player.sendPacket(SND_MIDDLE);
 		}
 		else if (event.equals("32469-08.htm") || event.equals("32469-09.htm"))
 		{
-			player.setClassId(136);
+			player.setClassId(ClassId.Judicator.getId());
 			player.broadcastUserInfo();
-			qs.rewardItems(57, 26000);
+			qs.rewardItems(PcInventory.ADENA_ID, 26000);
 			qs.exitQuest(false);
-			qs.playSound("ItemSound.quest_finish");
+			player.sendPacket(SND_FINISH);
 		}
 		return event;
 	}
@@ -64,12 +66,10 @@ public final class LawEnforcement extends QuestJython
 	public String onTalk(L2Npc npc, L2PcInstance talker)
 	{
 		QuestState qs = talker.getQuestState(LAW_ENFORCEMENT);
-
 		if (qs == null)
 			return NO_QUEST;
 
 		int npcId = npc.getNpcId();
-
 		int cond = qs.getInt(CONDITION);
 
 		if (npcId == LIANE)
@@ -78,44 +78,27 @@ public final class LawEnforcement extends QuestJython
 			{
 				if (talker.getRace() == Race.Kamael)
 				{
-					if (talker.getClassId().ordinal() == 135 && talker.getLevel() >= 76)
-					{
+					if (talker.getClassId() == ClassId.Inspector && talker.getLevel() >= 76)
 						return "32222-01.htm";
-					}
 					else
-					{
 						return "32222-02.htm";
-					}
 				}
 				else
-				{
 					return "32222-03.htm";
-				}
 			}
 			else if (cond == 1)
-			{
 				return "32222-06.htm";
-			}
 		}
 		else if (npcId == KEKROPUS)
 		{
 			if (cond == 1)
-			{
 				return "32138-01.htm";
-			}
 			else if (cond == 2)
-			{
 				return "32138-10.htm";
-			}
 		}
-		else if (npcId == EINDBURGH)
-		{
-			if (cond == 2)
-			{
-				return "32469-01.htm";
-			}
-		}
-		return null;
+		else if (npcId == EINDBURGH && cond == 2)
+			return "32469-01.htm";
+		return NO_QUEST;
 	}
 
 	public static void main(String[] args)
