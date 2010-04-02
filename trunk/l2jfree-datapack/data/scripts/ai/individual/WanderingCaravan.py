243e1694c717445def8f695879ce40602537cf84
Psycho <laurentchervet@bluewin.ch>
2010-04-02 22:52:45 +0000

    /trunk/datapack: - Some cleanup, preparations for coming instances as well
    git-svn-id: http://svn.l2jfree.com/l2jfree@8161 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/l2jfree-datapack/data/scripts/ai/individual/WanderingCaravan.py
+++ trunk/l2jfree-datapack/data/scripts/ai/individual/WanderingCaravan.py
@@ -1,14 +1,9 @@
-# By Psychokiller1888
+# By Psycho(killer1888) / L2jFree
 
 import sys
 from com.l2jfree.gameserver.model.quest           import State
 from com.l2jfree.gameserver.model.quest           import QuestState
 from com.l2jfree.gameserver.model.quest.jython    import QuestJython as JQuest
-from com.l2jfree.gameserver.model.itemcontainer   import PcInventory
-from com.l2jfree.gameserver.model                 import L2ItemInstance
-from com.l2jfree.gameserver.network.serverpackets import InventoryUpdate
-from com.l2jfree.gameserver.network.serverpackets import SystemMessage
-from com.l2jfree.gameserver.network               import SystemMessageId
 
 WANDERING_CARAVAN = 22339
 BASIC_CERTIFICATE = 9850
@@ -25,15 +20,8 @@ class WanderingCaravan(JQuest):
 			bcertificate = player.getInventory().getItemByItemId(BASIC_CERTIFICATE)
 			scertificate = player.getInventory().getItemByItemId(STANDARD_CERTIFICATE)
 			if bcertificate and not scertificate:
-				item = player.getInventory().addItem("Quest", MARK_BETRAYAL, 1, player, None)
-				iu = InventoryUpdate()
-				iu.addItem(item)
-				player.sendPacket(iu);
-				sm = SystemMessage(SystemMessageId.YOU_PICKED_UP_S1_S2)
-				sm.addItemName(item)
-				sm.addNumber(1)
-				player.sendPacket(sm)
+				player.addItem("Wandering Caravan", MARK_BETRAYAL, 1, player, True, True)
 		return
 	
-QUEST = WanderingCaravan(-1, "WanderingCaravan", "custom")
+QUEST = WanderingCaravan(-1, "WanderingCaravan", "ai")
 QUEST.addKillId(WANDERING_CARAVAN)
