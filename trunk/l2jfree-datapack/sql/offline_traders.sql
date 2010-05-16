773f99a34925ecdc62a4b0a00e7aa39efaa542c1
savormix <cossacking@gmail.com>
2010-05-16 15:09:29 +0000

    /trunk/l2jfree-datapack: - Minor corrections
    git-svn-id: http://svn.l2jfree.com/l2jfree@8447 6ff00ba9-25cd-4355-987e-099c62bc2628

--- trunk/l2jfree-datapack/sql/offline_traders.sql
+++ trunk/l2jfree-datapack/sql/offline_traders.sql
@@ -1,7 +1,6 @@
 -- Table to keep offline traders
 -- author hex1r0
-DROP TABLE IF EXISTS `offline_traders`;
-CREATE TABLE `offline_traders` (
+CREATE TABLE IF NOT EXISTS `offline_traders` (
   `char_id` INT NOT NULL ,
   `mode` VARCHAR( 1 ) NOT NULL ,
   `msg` VARCHAR( 255 ) NULL ,
--- trunk/l2jfree-datapack/sql/offline_traders_items.sql
+++ trunk/l2jfree-datapack/sql/offline_traders_items.sql
@@ -1,7 +1,6 @@
 -- Table to keep items of offline traders
 -- author hex1r0
-DROP TABLE IF EXISTS `offline_traders_items`;
-CREATE TABLE `offline_traders_items` (
+CREATE TABLE IF NOT EXISTS `offline_traders_items` (
   `char_id` INT NOT NULL ,
   `item_obj_id` INT NOT NULL ,
   `count` BIGINT NOT NULL ,
