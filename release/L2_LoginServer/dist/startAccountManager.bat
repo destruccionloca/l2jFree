362abd2b1c5ea490b7f770de9abdc7daf8a4a87f
nbali <nbalika@gmail.com>
2010-06-05 15:13:07 +0000

    git-svn-id: http://svn.l2jfree.com/l2jfree@8556 6ff00ba9-25cd-4355-987e-099c62bc2628

--- release/L2_LoginServer/dist/startAccountManager.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-SET OLDCLASSPATH=%CLASSPATH%
-call setenv.bat
-
-@java -Djava.util.logging.config.file=console.cfg com.l2jfree.accountmanager.AccountManager
-
-SET CLASSPATH=%OLDCLASSPATH%
-@pause
--- release/L2_LoginServer/dist/startAccountManager.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-. ./setenv.sh
-java -Djava.util.logging.config.file=console.cfg com.l2jfree.accountmanager.AccountManager
